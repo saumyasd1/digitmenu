c43c0e69a3717d2b77e09e3664911e5a69d4e2e8
Sakshi <Sakshi@ADLP028_Sakshi>
2015-11-23 18:34:31 +0530

    First commit

---
+++ WebContent/avery/properties_en.js
@@ -0,0 +1,6 @@
+var addImage='avery/resources/images/add.png',
+menuIcon='avery/resources/images/MenuImage.png',
+editIcon='avery/resources/images/edit.png',
+BackIcon='avery/resources/images/backButton.png',backText='Back',
+advSearchIcon='/adeptia/icons/Advancesearch.png',advSearchText='adv search',centerStyleDiv='<div align="center"><b><font color=#006FDD>',
+clearSearchIcon='/adeptia/icons/Error.png';
