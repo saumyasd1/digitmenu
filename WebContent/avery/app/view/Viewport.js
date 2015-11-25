fd1ad1d64a7ef0706d45df5ef799140ec95c7560
shaifali <shaifali@ADLP046_Shaifa>
2015-11-25 19:34:55 +0530

    Added AocLogin view in card layout

--- WebContent/avery/app/view/Viewport.js
+++ WebContent/avery/app/view/Viewport.js
@@ -6,60 +6,71 @@
 Ext.define('AOC.view.Viewport', {
 	extend : 'Ext.container.Viewport',
 	itemId : 'viewportitemid',
-	requires : ['AOC.view.AOCHeader','AOC.view.ToolbarView','AOC.view.orderqueue.OrderQueueView','AOC.view.partner.PartnerManagement','AOC.view.address.AddressManage','AOC.view.webform.WebOrderView'],
+	requires : ['AOC.view.AOCLogin','AOC.view.AOCHeader','AOC.view.ToolbarView','AOC.view.orderqueue.OrderQueueView','AOC.view.partner.PartnerManagement','AOC.view.address.AddressManage','AOC.view.webform.WebOrderView'],
 	stores:[ 'PartnerManagementStore','AddressManageStore'],
 	initComponent : function(){
 		Ext.apply(this, {
 			layout : {
-				type : 'border'
-			},
-			items : this.buildItems()
+				type : 'card',
+			     deferredRender : true
+			    },
+			items : [
+			         {
+			             xtype : 'aocLogin'
+			         },
+			         {
+			            xtype     : 'container',
+			         	layout : {
+							type : 'border'
+						},
+			             items     : [
+					{
+						region : 'north',
+						xtype : 'aocheader'
+					},
+					{
+						region : 'north',
+						xtype : 'toolbarview',
+						height:30,
+					    itemId:'toolbarviewitemid'
+					},
+					{
+					 	region : 'center',
+					 	xtype:'container',
+					 	layout : {
+					 		type : 'card',
+					 		deferredRender : true
+					  	},
+						activeItem : 0,
+						itemId:'AOCContainer',
+						items:[
+						       {
+						    	   xtype : 'orderqueueview' ,
+						    	   cls : 'adeptia-home-entity',
+						    	   margins : '12 12 12 12'
+						       },
+							   {
+							       xtype : 'partnermanagement',
+							    	cls : 'adeptia-home-entity',
+							    	 margins : '12 12 12 12'
+							   },
+							   {
+							       xtype : 'addressmanage',
+							    	cls : 'adeptia-home-entity',
+							    	 margins : '12 12 12 12'
+							   },
+							   {
+							       xtype : 'weborderview',
+							    	cls : 'adeptia-home-entity',
+							    	 margins : '12 12 12 12'
+							   }
+						       ]
+					}
+								                     ]
+								                 }
+								             ]
+								         })
+								     	this.callParent(arguments);
+	}
 		});
-		this.callParent(arguments);
-	},
 	
-	buildItems : function()  {
-		return [{
-			region : 'north',
-			xtype : 'aocheader'
-		},
-		{
-			region : 'north',
-			xtype : 'toolbarview',
-			height:30,
-	        itemId:'toolbarviewitemid'
-		},
-		{
-		 	region : 'center',
-		 	xtype:'container',
-		 	layout : {
-		 		type : 'card',
-		 		deferredRender : true
-		  	},
-			activeItem : 0,
-			itemId:'AOCContainer',
-			items:[
-			       {
-			    	   xtype : 'orderqueueview' ,
-			    	   cls : 'adeptia-home-entity',
-			    	   margins : '12 12 12 12'
-			       },
-				   {
-				       xtype : 'partnermanagement',
-				    	cls : 'adeptia-home-entity',
-				    	 margins : '12 12 12 12'
-				   },
-				   {
-				       xtype : 'addressmanage',
-				    	cls : 'adeptia-home-entity',
-				    	 margins : '12 12 12 12'
-				   },
-				   {
-				       xtype : 'weborderview',
-				    	cls : 'adeptia-home-entity',
-				    	 margins : '12 12 12 12'
-				   }
-			       ]
-		}
-	]}
-});
