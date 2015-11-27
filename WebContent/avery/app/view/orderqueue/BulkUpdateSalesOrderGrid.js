a041fdb09f56dd6721dfbe5dd59f7e284314fa2e
shaifali <shaifali@ADLP046_Shaifa>
2015-11-27 09:56:18 +0530

    Added Bulk update function in js

--- WebContent/avery/app/view/orderqueue/BulkUpdateSalesOrderGrid.js
+++ WebContent/avery/app/view/orderqueue/BulkUpdateSalesOrderGrid.js
@@ -3,9 +3,6 @@ Ext.define('AOC.view.orderqueue.BulkUpdateSalesOrderGrid', {
     alias : 'widget.bulkupdatesalesordergrid',
     itemId:'BulkUpdateSalesOrderGrid',
     controller:'salesorder',
-	//store:'SalesOrderStore',
-//	requires : ['AOC.controller.orderqueue.SalesOrderViewController'
-//    ],
 	emptyText:'<div align=center> No content type(s) to display.</div>',
 	runTime : AOC.config.Runtime,
     initComponent : function(){
@@ -25,15 +22,20 @@ Ext.define('AOC.view.orderqueue.BulkUpdateSalesOrderGrid', {
 			        
 			    },
 			    listeners:{
-		        	'selectionchange':function( grid, selection, eOpts ){
+			    	'selectionchange':function( grid, selection, eOpts ){
                         if(selection.startCell)
 							var store=grid.store;
                                 var intialCell=selection.startCell;
-                            var dataindex=intialCell.column.dataIndex;
+                                var dataindex=intialCell.column.dataIndex;
                                 var value=intialCell.record.get(dataindex);
                                 var initialrowIdx=intialCell.rowIdx;
                                 var lastrowIdx=selection.endCell.rowIdx;
-                                for(var i=(initialrowIdx+1);i<=lastrowIdx;i++){
+                                var start=initialrowIdx,end=lastrowIdx;
+                                if(lastrowIdx<initialrowIdx){
+                                	start=lastrowIdx;
+                                	end=initialrowIdx;
+                                }
+                                for(var i=(start+1);i<=end;i++){
                                     store.getAt(i).set(dataindex,value);
                                 }
 		        	}
@@ -44,52 +46,53 @@ Ext.define('AOC.view.orderqueue.BulkUpdateSalesOrderGrid', {
     },
     buildColumns : function(){
     	var me=this;
-        return [{ text: 'Division',  dataIndex: 'Division',editor:'textfield' },
-{ text: 'OrderSource', dataIndex: 'OrderSource', editor:'textfield' },
-{ text: 'SOLD TO RBO Number', dataIndex: 'SOLDTORBONumber',editor:'textfield' },
-{ text: 'Oracle Bill to Site Number', dataIndex: 'OracleBilltoSiteNumber',editor:'textfield' },
-{ text: 'Oracle Ship to Site Number', dataIndex: 'OracleShiptoSiteNumber',editor:'textfield' },
-{ text: 'Shipping method', dataIndex: 'Shippingmethod',editor:{xtype:'combo',store:[['ACS','ACS'],['ADP GLOBAL EXPRESS','ADP GLOBAL EXPRESS'],['AIRTRANS LOG - BY AIR','AIRTRANS LOG - BY AIR'],['A-LINK','A-LINK']]} },
-{ text: 'Customer PO Number', dataIndex: 'CustomerPONumber',editor:'textfield' },
-{ text: 'Retailer poCustomer job', dataIndex: 'RetailerpoCustomerjob',editor:'textfield' },
-{ text: 'Oracle Item Number', dataIndex: 'OracleItemNumber',editor:'textfield' },
-{ text: 'Customer Item Number', dataIndex: 'CustomerItemNumber',editor:'textfield' },
-{ text: 'Item Description', dataIndex: 'ItemDescription',editor:'textfield' },
-{ text: 'Orderded Qty', dataIndex: 'OrderdedQty',editor:'textfield' },
-{ text: 'Date Ordered', dataIndex: 'DateOrdered',editor:'textfield' },
-{ text: 'Request date', dataIndex: 'Requestdate',editor:'textfield' },
-{ text: 'promise date', dataIndex: 'promisedate',editor:'textfield' },
-{ text: 'Freight term', dataIndex: 'Freightterm',editor:'textfield' },
-{ text: 'CSR', dataIndex: 'CSR',editor:'textfield' },
+        return [
+{ text: 'Division',  dataIndex: 'division',editor:'textfield' },
+{ text: 'OrderSource', dataIndex: 'orderSource', editor:'textfield' },
+{ text: 'SOLD TO RBO Number', dataIndex: 'soldTORBONumber',editor:'textfield' },
+{ text: 'Oracle Bill to Site Number', dataIndex: 'oracleBilltoSiteNumber',editor:'textfield' },
+{ text: 'Oracle Ship to Site Number', dataIndex: 'oracleShiptoSiteNumber',editor:'textfield' },
+{ text: 'Shipping method', dataIndex: 'shippingMethod',editor:{xtype:'combo',store:[['ACS','ACS'],['ADP GLOBAL EXPRESS','ADP GLOBAL EXPRESS'],['AIRTRANS LOG - BY AIR','AIRTRANS LOG - BY AIR'],['A-LINK','A-LINK']]} },
+{ text: 'Customer PO Number', dataIndex: 'customerPONumber',editor:'textfield' },
+{ text: 'Retailer poCustomer job', dataIndex: 'retailerPO_CustomerJob',editor:'textfield' },
+{ text: 'Oracle Item Number', dataIndex: 'oracleItemNumber',editor:'textfield' },
+{ text: 'Customer Item Number', dataIndex: 'customerItemNumber',editor:'textfield' },
+{ text: 'Item Description', dataIndex: 'itemDescription',editor:'textfield' },
+{ text: 'Orderded Qty', dataIndex: 'orderdedQty',editor:'textfield' },
+{ text: 'Date Ordered', dataIndex: 'dateOrdered',editor:'textfield' },
+{ text: 'Request date', dataIndex: 'ustomerRequestDate',editor:'textfield' },
+{ text: 'promise date', dataIndex: 'promiseDate',editor:'textfield' },
+{ text: 'Freight term', dataIndex: 'freightTerms',editor:'textfield' },
+{ text: 'CSR', dataIndex: 'csr',editor:'textfield' },
 { text: 'packing instruction', dataIndex: 'packinginstruction',editor:'textfield' },
-{ text: 'Shipping instruction', dataIndex: 'Shippinginstruction',editor:'textfield' },
-{ text: 'Invoice line instruction', dataIndex: 'Invoicelineinstruction',editor:'textfield' },
-{ text: 'Division for Interface ERP ORG', dataIndex: 'DivisionforInterfaceERPORG',editor:'textfield' },
-{ text: 'Bill to contact', dataIndex: 'Billtocontact',editor:'textfield' },
-{ text: 'Bill to tel', dataIndex: 'Billtotel',editor:'textfield' },
-{ text: 'Bill to EMAIL', dataIndex: 'BilltoEMAIL',editor:'textfield' },
-{ text: 'Ship to contact', dataIndex: 'Ship to contact',editor:'textfield' },
-{ text: 'Ship to tel', dataIndex: 'Shiptotel',editor:'textfield' },
-{ text: 'Ship to FAX', dataIndex: 'ShiptoFAX',editor:'textfield' },
-{ text: 'Ship to EMAIL', dataIndex: 'Ship to EMAIL',editor:'textfield' },
-{ text: 'Artwork hold', dataIndex: 'Artwork hold',editor:'checkbox' },
-{ text: 'Artwork work attachment', dataIndex: 'Artwork for Reference',editor:'checkbox' },
-{ text: 'Variable data Breakdown', dataIndex: 'Variable data Breakdown',editor:'textfield' },
-{ text: 'Manu. Note', dataIndex: 'ManuNote',editor:'textfield' },
-{ text: 'Order type', dataIndex: 'Ordertype',editor:'textfield' },
-{ text: 'Order by', dataIndex: 'Orderby',editor:'textfield' },
-{ text: 'End customer', dataIndex: 'Endcustomer',editor:'textfield' },
-{ text: 'Shipping only note', dataIndex: 'Shippingonlynote',editor:'textfield' },
-{ text: 'Bank charge', dataIndex: 'Bankcharge',editor:'textfield' },
-{ text: 'Freight charge', dataIndex: 'Freightcharge',editor:'textfield' },
-{ text: 'Shipping hold', dataIndex: 'Shippinghold',editor:'checkbox' },
-{ text: 'Production hold', dataIndex: 'Productionhold',editor:'checkbox' },
-{ text: 'Split ship set by', dataIndex: 'Splitshipsetby',editor:'textfield' },
-{ text: 'Agreement', dataIndex: 'Agreement',editor:'textfield' },
-{ text: 'Model serial Number', dataIndex: 'ModelserialNumber',editor:'textfield' },
-{ text: 'Waive MOQ', dataIndex: 'WaiveMOQ',editor:'checkbox' },
-{ text: 'APO type', dataIndex: 'APOtype',editor:'textfield' }
-		];
+{ text: 'Shipping instruction', dataIndex: 'shippingInstructions',editor:'textfield' },
+{ text: 'Invoice line instruction', dataIndex: 'invoicelineInstruction',editor:'textfield' },
+{ text: 'Division for Interface ERP ORG', dataIndex: 'divisionforInterfaceERPORG',editor:'textfield' },
+{ text: 'Bill to contact', dataIndex: 'billToContact',editor:'textfield' },
+{ text: 'Bill to tel', dataIndex: 'billToTEL',editor:'textfield' },
+{ text: 'Bill to EMAIL', dataIndex: 'billToEMAIL',editor:'textfield' },
+{ text: 'Ship to contact', dataIndex: 'shipTOContact',editor:'textfield' },
+{ text: 'Ship to tel', dataIndex: 'shipTOTEL',editor:'textfield' },
+{ text: 'Ship to FAX', dataIndex: 'shipTOFAX',editor:'textfield' },
+{ text: 'Ship to EMAIL', dataIndex: 'shipTOEMAIL',editor:'textfield' },
+{ text: 'Artwork hold', dataIndex: 'artworkhold',editor:'checkbox' },
+{ text: 'Artwork work attachment', dataIndex: 'artworkworkattachment',editor:'checkbox' },
+{ text: 'Variable data Breakdown', dataIndex: 'variableDataBreakdown',editor:'textfield' },
+{ text: 'Manu. Note', dataIndex: 'manufacturingnotes',editor:'textfield' },
+{ text: 'Order type', dataIndex: 'ordertype',editor:'textfield' },
+{ text: 'Order by', dataIndex: 'orderby',editor:'textfield' },
+{ text: 'End customer', dataIndex: 'endcustomer',editor:'textfield' },
+{ text: 'Shipping only note', dataIndex: 'shippingonlynotes',editor:'textfield' },
+{ text: 'Bank charge', dataIndex: 'bankCharge',editor:'textfield' },
+{ text: 'Freight charge', dataIndex: 'freightCharge',editor:'textfield' },
+{ text: 'Shipping hold', dataIndex: 'shippinghold',editor:'checkbox' },
+{ text: 'Production hold', dataIndex: 'productionhold',editor:'checkbox' },
+{ text: 'Split ship set by', dataIndex: 'splitshipset',editor:'textfield' },
+{ text: 'Agreement', dataIndex: 'agreement',editor:'textfield' },
+{ text: 'Model serial Number', dataIndex: 'modelSerialNumber',editor:'textfield' },
+{ text: 'Waive MOQ', dataIndex: 'waiveMOQ',editor:'checkbox' },
+{ text: 'APO type', dataIndex: 'apoType',editor:'textfield' }
+    ];
     },
     tbar: { 
 		height: 50,
@@ -98,7 +101,7 @@ Ext.define('AOC.view.orderqueue.BulkUpdateSalesOrderGrid', {
 			 {
 	              xtype:'button',
 				  text:'Save',
-				  handler:'backButton'
+				  handler:'saveSalesOrder'
 	         },
 	         {
 	              xtype:'button',
