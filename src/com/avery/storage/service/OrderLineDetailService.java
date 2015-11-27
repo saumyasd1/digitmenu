289adba934821ed54f985a4babb75c983778867e
Sakshi <Sakshi@ADLP028_Sakshi>
2015-11-27 13:44:26 +0530

    Changing the returb type of readByVariableName

--- src/com/avery/storage/service/OrderLineDetailService.java
+++ src/com/avery/storage/service/OrderLineDetailService.java
@@ -1,13 +1,13 @@
 package com.avery.storage.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.avery.storage.dao.impl.OrderLineDetailDao;
-import com.avery.storage.entities.OrderLine;
 import com.avery.storage.entities.OrderLineDetail;
 
 @Component
@@ -32,7 +32,7 @@ public class OrderLineDetailService extends GenericEntityService<OrderLineDetail
 		
 	} 
 	
-	public List<OrderLineDetail> readByVariableName(Long entityId,String variablfieldename){
+	public Map readByVariableName(Long entityId,String variablfieldename){
 		
 		return getOrderLineDetailDao().readByVariableName(entityId,variablfieldename);
 	}
