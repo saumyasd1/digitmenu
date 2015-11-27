2f915bbf64b10e2386358630f74673af3322f3cd
shaifali <shaifali@ADLP046_Shaifa>
2015-11-27 11:15:58 +0530

    changed the form size

--- WebContent/avery/app/view/AOCLogin.js
+++ WebContent/avery/app/view/AOCLogin.js
@@ -3,8 +3,8 @@ Ext.define('AOC.view.AOCLogin',{
 	alias:'widget.aocLogin',
 	itemId:'aocLoginItemId',
 	bodyPadding: 5,
-	width: 700,
-	border:false,
+	width: 600,
+	border:true,
     modal:true,
     initComponent : function(){
     	var me=this;
@@ -16,36 +16,10 @@ Ext.define('AOC.view.AOCLogin',{
                 	align: 'center',
                 	pack: 'center'
                 	},
-                title: '<B>Welcome to Accelerated Order Capture Login</B>'
+                title: '<B><font size=4><center>Welcome to Accelerated Order Capture</center></font></B>'
             });
             this.callParent(arguments);
 
-        },
-        buildButtons : function(){
-        	var me=this;
-            return [{
-            	text : 'Login',
-            	handler: function() 
-            	{
-            		valueObj=me.form.getValues(false,true,false,true);
-            		var UserName=valueObj.userName;
-            		var password=valueObj.password;
-            		if(UserName=="aoc_user" && password=="indigo1")
-            		{
-            			var activeCard=Ext.ComponentQuery.query("#viewportitemid")[0];
-            			activeCard.getLayout().setActiveItem(1);
-            			var menuStore=Ext.create('AOC.store.MenuStore');
-                    	var menuController=myAppGlobal.getController('MenuController');
-                    	menuStore.on({
-                    		load : menuController.onLoadMenuBar
-                    	});
-            		}else{
-            			var Msg=me.down('#messageFieldItemId');
-            			Msg.setValue('Either UserName or Password is not correct');
-            		}
-            	}
-            }
-           ];
         },
         buildItem:function(){
         	var me=this;
@@ -60,18 +34,22 @@ Ext.define('AOC.view.AOCLogin',{
 					      {
         		            xtype: 'form',        
         		            frame: true,         
-        		            bodyPadding: 50,
+        		            bodyPadding:30,
+        		            border:true,
+        		            title:'<B>LOGIN</B>',
         		            defaults: {             
         		                xtype: 'textfield', 
-        		                labelWidth : 100,
-        			            width : 500,
+        		                labelWidth : 65,
+        			            width : 350,
+        			            height:10,
         			            allowBlank:false
         		            },
         		            items: [
         		                {
         		                    name: 'userName',    
         		                    fieldLabel: "<B>UserName</B>",
-        		                    blankText : 'UserName Field is required'
+        		                    blankText : 'UserName Field is required',
+        		                    emptyText:'USERNAME'
         		                },
         		            	{
         		                	xtype :'tbspacer',
@@ -81,12 +59,43 @@ Ext.define('AOC.view.AOCLogin',{
         		                    name: 'password',
         		                    fieldLabel: "<B>Password</B>",
         		                    inputType: 'password',
-        		                    blankText : 'Password Field is required'
+        		                    blankText : 'Password Field is required',
+        		                    emptyText:'PASSWORD'
+        		                },
+        		                {
+        		                	xtype :'tbspacer',
+        		                	width :20,
+        		                	height:10
+        		        		},
+        		                {
+        		                	xtype :'button',
+        		                	text : 'LOGIN',
+        		                	labelWidth : 65,
+             			            width : 350,
+             			            height:30,
+        		                	handler: function() 
+        		                	{
+        		                		valueObj=me.form.getValues(false,true,false,true);
+        		                		var UserName=valueObj.userName;
+        		                		var password=valueObj.password;
+        		                		if(UserName=="aoc_user" && password=="indigo1")
+        		                		{
+        		                			var activeCard=Ext.ComponentQuery.query("#viewportitemid")[0];
+        		                			activeCard.getLayout().setActiveItem(1);
+        		                			var menuStore=Ext.create('AOC.store.MenuStore');
+        		                        	var menuController=myAppGlobal.getController('MenuController');
+        		                        	menuStore.on({
+        		                        		load : menuController.onLoadMenuBar
+        		                        	});
+        		                		}else{
+        		                			var Msg=me.down('#messageFieldItemId');
+        		                			Msg.setValue('Either UserName or Password is not correct');
+        		                		}
+        		                	}
         		                }
         		            ]
         		        }
         		    ],
-        		buttons:this.buildButtons()
         	}]
         },
         notifyByMessage : function(config){
