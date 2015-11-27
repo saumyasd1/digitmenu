a25bb24d156964e8e2041945bfb0a780ed433f91
shaifali <shaifali@ADLP046_Shaifa>
2015-11-27 09:46:02 +0530

    implementation of bulkupdate method

--- src/com/avery/storage/dao/impl/SalesOrderDaoImpl.java
+++ src/com/avery/storage/dao/impl/SalesOrderDaoImpl.java
@@ -1,9 +1,12 @@
 package com.avery.storage.dao.impl;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -15,7 +18,14 @@ import org.springframework.stereotype.Repository;
 
 import com.avery.logging.AppLogger;
 import com.avery.storage.dao.GenericDaoImpl;
+import com.avery.storage.entities.Address;
+import com.avery.storage.entities.OrderLine;
+import com.avery.storage.entities.Partner;
 import com.avery.storage.entities.SalesOrder;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Repository
 public class SalesOrderDaoImpl extends GenericDaoImpl<SalesOrder, Long> implements
@@ -38,6 +48,68 @@ public class SalesOrderDaoImpl extends GenericDaoImpl<SalesOrder, Long> implemen
 		} catch (Exception e) {
 			AppLogger.getSystemLogger().error(
 					"Error in fetching sales order for order queue id " + orderID, e);
+			throw new WebApplicationException(Response
+					.status(Status.INTERNAL_SERVER_ERROR)
+					.entity(ExceptionUtils.getRootCauseMessage(e))
+					.type(MediaType.TEXT_PLAIN_TYPE).build());
+		}
+		
+	}
+
+
+	@Override
+	public Map getAllEntitiesWithCriteria(MultivaluedMap queryMap)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Long create(SalesOrder newInstance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public SalesOrder read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void bulkUpdate(String data) {
+		ObjectMapper mapper = new ObjectMapper();
+		Long currentObjId=0L;
+		ObjectReader updater=null;
+		String[] objArray=data.split("@@@");
+		Session session = null;
+		try{
+			mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+			mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+					false);
+			session = getSessionFactory().getCurrentSession();
+			String t=null;
+			for(int i=0;i<objArray.length;i++){
+				t=objArray[i];
+				SalesOrder salesOrder = mapper.readValue(t,SalesOrder.class);
+				currentObjId=salesOrder.getId();
+				salesOrder=(SalesOrder) session.get(SalesOrder.class,currentObjId);
+				updater = mapper.readerForUpdating(salesOrder);
+				salesOrder = updater.readValue(t);
+				salesOrder.preUpdateOp();
+				session.update(salesOrder);
+				salesOrder.postUpdateOp();
+			}
+		}catch (WebApplicationException ex) {
+			AppLogger.getSystemLogger().error(
+					"Error while Performing bulk update ", ex);
+			throw ex;
+		} catch (Exception e) {
+			AppLogger.getSystemLogger().error(
+					"Error while Performing bulk update ", e);
 			throw new WebApplicationException(Response
 					.status(Status.INTERNAL_SERVER_ERROR)
 					.entity(ExceptionUtils.getRootCauseMessage(e))
