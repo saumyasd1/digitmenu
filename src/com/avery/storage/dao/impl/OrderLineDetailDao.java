d5a474ed25506ce7527c4d4fad9445d74c77b904
Sakshi <Sakshi@ADLP028_Sakshi>
2015-11-27 13:40:51 +0530

    Changing return type of method to readByVariableName

--- src/com/avery/storage/dao/impl/OrderLineDetailDao.java
+++ src/com/avery/storage/dao/impl/OrderLineDetailDao.java
@@ -1,16 +1,16 @@
 package com.avery.storage.dao.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import com.avery.storage.dao.GenericDao;
-import com.avery.storage.entities.OrderLine;
 import com.avery.storage.entities.OrderLineDetail;
 
 public interface OrderLineDetailDao extends GenericDao<OrderLineDetail, Long>{
 	
 	public List<OrderLineDetail> readAllVariableByOrderID(Long orderID);
 	
-	public List<OrderLineDetail> readByVariableName(Long orderID,String variablfieldename);
+	public Map readByVariableName(Long orderID,String variablfieldename);
 	
 
 }
