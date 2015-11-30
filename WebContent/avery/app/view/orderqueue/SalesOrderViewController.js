cebd0af16e7a818d0f2b034222940c345ae10c6a
shaifali <shaifali@ADLP046_Shaifa>
2015-11-30 13:28:07 +0530

    Added store

--- WebContent/avery/app/view/orderqueue/SalesOrderViewController.js
+++ WebContent/avery/app/view/orderqueue/SalesOrderViewController.js
@@ -1,14 +1,35 @@
 Ext.define('AOC.view.orderqueue.SalesOrderViewController', {
 	extend: 'Ext.app.ViewController',
     alias: 'controller.salesorder',
-    requires : ['AOC.view.orderqueue.BulkUpdateSalesOrderGrid'],
+    requires : ['AOC.view.orderqueue.BulkUpdateSalesOrderGrid','AOC.view.orderqueue.BulkUpdateSalesVariableHeaderGrid','AOC.model.VariableHeaderModel'],
     runTime : AOC.config.Runtime,
     getUpdateScreen:function(){
     	 var viwport=Ext.ComponentQuery.query('#viewportitemid')[0];
       	 var height=viwport.getHeight()-20;
       	 var width=viwport.getWidth()-20;
       	 var id=this.runTime.getOrderQueueId();
-      	var store=Ext.create('AOC.store.SalesOrderStore', {
+     	 var radioGroupValue=this.lookupReference('radioGroup').getValue().rb,store,win,innerGridType,comboValue='';
+      	 if(radioGroupValue=='2'){
+      		var comboField=this.lookupReference('variableFieldCombo');
+      		comboValue=comboField.getValue();
+      		if(comboValue=='' || comboValue==null){
+      			Ext.Msg.alert('', 'Please select a value from the drop down before drop down.');
+      			return false;
+      		}
+      		innerGridType='bulkUpdateSalesVariableHeaderGrid';
+      		store=Ext.create('AOC.store.SalesOrderStore', {
+      			model:'AOC.model.VariableHeaderModel',
+    			proxy : {
+    				type : 'rest',
+    				url : applicationContext+'/rest/salesorderdetails/order/'+id+'/'+comboValue,
+    				reader:{
+    			        type:'json', 
+    			        rootProperty: 'SalesOrderDetail'
+    			    }
+    			}
+    		}); 
+      	 }else{
+      store=Ext.create('AOC.store.SalesOrderStore', {
       		extend : 'Ext.data.Store',
       		model:'AOC.model.SalesOrderModel',
       		autoLoad : true,
@@ -22,6 +43,8 @@ Ext.define('AOC.view.orderqueue.SalesOrderViewController', {
       		    }
       	}
       	});
+      	innerGridType='bulkupdatesalesordergrid';
+      }
 		   var win=Ext.create('Ext.window.Window',{
 			 	height:height,
 				width:width,
@@ -30,12 +53,13 @@ Ext.define('AOC.view.orderqueue.SalesOrderViewController', {
 				draggable: false,
 				modal:true,
 				items:[{
-					xtype:'bulkupdatesalesordergrid',
-					store:store
+					xtype:innerGridType,
+					store:store,
+					variableColumnName:comboValue
 				}]
 		   });
 		   win.show();
-    },
+      	 },
     backButton:function(){
     	 var bulkUpdate=Ext.ComponentQuery.query('#bulkUpdateItemId')[0];
     	  bulkUpdate.setText('<b>Order Queue</b>');
@@ -43,8 +67,38 @@ Ext.define('AOC.view.orderqueue.SalesOrderViewController', {
         panel.getLayout().setActiveItem(0);
         this.getView().destroy();
     },
-    radioButtonClick:function(){
-    	
+    radioButtonClick:function(obj,newValue,oldValue){
+    	var comboField=this.lookupReference('variableFieldCombo');
+    	if(newValue.rb=='2'){
+    		Ext.getBody().mask('Loading..');
+    		var id= this.runTime.getOrderQueueId();
+    		Ext.Ajax.request( {
+    			method:'GET',
+    			url : applicationContext+'/rest/salesorderdetails/order/'+id,
+		        success : function(response, opts) {
+		        	var jsonValue=Ext.decode(response.responseText);
+		        	var serviceStoreData = [];
+		        	if(jsonValue.length>0){
+		        	jsonValue.forEach(function(item){
+                		var service = [item];
+                		serviceStoreData.push(service);
+                	});
+		        	var serviceStore =  Ext.create('Ext.data.ArrayStore',{
+                 	   		fields : ['variableFieldName'],	
+            	            data : serviceStoreData
+                    });
+		        	comboField.bindStore(serviceStore);
+		        	}
+		        	comboField.setVisible(true);
+		        	Ext.getBody().unmask();
+		        },
+		        failure: function(response, opts) {
+		        	Ext.getBody().unmask();
+	          }
+	  	});
+    	}else{
+    		comboField.setVisible(false);
+    	}
     },
     saveSalesOrder:function(){
     	       var grid=this.getView();
@@ -75,6 +129,35 @@ Ext.define('AOC.view.orderqueue.SalesOrderViewController', {
     	             }); 
     	       
     	   
+    },
+    saveSalesOrderDetails:function(){
+    	debugger;
+     	var grid=this.getView(),me=this;
+    	var store=grid.store,
+    	parms ='';
+    	var updatedRecords=store.getModifiedRecords();
+    	Ext.each(updatedRecords,function(record){
+    		var obj=record.getChanges( ) ;
+    		obj.id=record.id;
+    		if(parms=='')
+    			parms=parms+Ext.encode(obj);
+    		else 
+    			parms=parms+'@@@'+Ext.encode(obj);
+    		  
+    		 });
+    	Ext.Ajax.request({
+    		method:'PUT',
+	        jsonData:parms,
+    		   url : applicationContext+'/rest/salesorderdetails/variablebulkupdate',
+		        success : function(response, opts) {
+			  		Ext.Msg.alert('Sales Order successfully updated');
+			  		Ext.getBody().unmask();
+			  		me.getView().store.load();
+		        },
+		        failure: function(response, opts) {
+		        	Ext.getBody().unmask();
+	          }
+    		  }); 
     },
     cancelChanges:function(){
     	var me=this;
