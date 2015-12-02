53f4973722c1cee7e7dcf283beaabb5f18eaa79a
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-02 10:01:14 +0530

    Adding Cancel Order Message in property file

--- WebContent/avery/properties_en.js
+++ WebContent/avery/properties_en.js
@@ -2,5 +2,6 @@ var addImage='avery/resources/images/add.png',
 menuIcon='avery/resources/images/MenuImage.png',
 editIcon='avery/resources/images/edit.png',
 BackIcon='avery/resources/images/backButton.png',backText='Back',
-advSearchIcon='avery/resources/images/search.png',advSearchText='adv search',centerStyleDiv='<div align="center"><b><font color=#006FDD>',
-clearSearchIcon='avery/resources/images/invalid_field.jpg';
+advSearchIcon='avery/resources/images/search.png',advSearchText='Adv. Search',centerStyleDiv='<div align="center"><b><font color=#006FDD>',
+clearSearchIcon='avery/resources/images/invalid_field.jpg',cancelOrderText='<b>Are you sure you want to cancel the Order? If yes, please enter the reason for futher reference:</b> ',
+advancedSearchWindowTitle='Advanced Search',cancelStatus='Cancelled';
