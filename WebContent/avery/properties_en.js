8619d8973bc50ce1e4b1a17e24f644e710c75bca
shaifali <shaifali@ADLP046_Shaifa>
2015-12-02 10:23:11 +0530

    added menuList image

--- WebContent/avery/properties_en.js
+++ WebContent/avery/properties_en.js
@@ -1,7 +1,8 @@
 var addImage='avery/resources/images/add.png',
-menuIcon='avery/resources/images/MenuImage.png',
+menuIcon='avery/resources/images/menuList.png',
 editIcon='avery/resources/images/edit.png',
 BackIcon='avery/resources/images/backButton.png',backText='Back',
 advSearchIcon='avery/resources/images/search.png',advSearchText='Adv. Search',centerStyleDiv='<div align="center"><b><font color=#006FDD>',
 clearSearchIcon='avery/resources/images/invalid_field.jpg',cancelOrderText='<b>Are you sure you want to cancel the Order? If yes, please enter the reason for futher reference:</b> ',
 advancedSearchWindowTitle='Advanced Search',cancelStatus='Cancelled';
+
