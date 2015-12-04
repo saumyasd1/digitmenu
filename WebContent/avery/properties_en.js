664bdda28b61189fe7e5e42dc35dc5084f6d2d87
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-04 17:43:47 +0530

    Adding names of button in property file

--- WebContent/avery/properties_en.js
+++ WebContent/avery/properties_en.js
@@ -4,5 +4,10 @@ editIcon='avery/resources/images/edit.png',
 BackIcon='avery/resources/images/backButton.png',backText='Back',
 advSearchIcon='avery/resources/images/search.png',advSearchText='Adv. Search',centerStyleDiv='<div align="center"><b><font color=#006FDD>',
 clearSearchIcon='avery/resources/images/invalid_field.jpg',cancelOrderText='<b>Are you sure you want to cancel the Order? If yes, please enter the reason for futher reference:</b> ',
-advancedSearchWindowTitle='Advanced Search',cancelStatus='Cancelled';
-
+advancedSearchWindowTitle='Advanced Search',cancelStatus='Cancelled'
+createSalesOrderBtnText='<b>Create Sales Order</b>',
+viewSalesOrderBtnText='<b>View Sales Order</b>',
+cancelText='<b>cancel</b>',
+salesOrdersumbitText='Submit to System',
+submitToOracleStatus='Submitted to Oracle'
+;
