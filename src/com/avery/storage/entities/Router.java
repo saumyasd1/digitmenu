08699df908be7dea4d2e5d2c112b440355acd06d
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-04 17:37:15 +0530

    Added functionality to delete sales order

--- src/com/avery/storage/entities/Router.java
+++ src/com/avery/storage/entities/Router.java
@@ -120,5 +120,52 @@ public class Router {
 		return rb.build();
 
 	}
+	
+	@GET
+	@Path("/cancelsalesorder/{id:[0-9]+}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response cancelSalesOrder(@Context UriInfo ui,@Context HttpHeaders hh,@PathParam("id") String id) {
+		Response.ResponseBuilder rb = null;
+		Integer retryCount = null;
+		Integer retryInterval = null;
+		try {
+			Long orderId = Long.parseLong(id);
+			StringWriter writer = new StringWriter();
+			String adeptiaServerURL=PropertiesConfig
+					.getString(PropertiesConstants.ADEPTIA_SERVER_URL);
+			String userName=PropertiesConfig
+					.getString(PropertiesConstants.ADEPTIA_SERVER_USERNAME);
+			String password=PropertiesConfig
+					.getString(PropertiesConstants.ADEPTIA_SERVER_PASSWORD);
+			//http://APDVADC283.na.averydennison.net:80/adeptia/publishProviderByRest/AV_Test_SalesOrder_Process/profile?FileQueueID=3
+			String url = adeptiaServerURL+"/adeptia/publishProviderByRest/AV_Test_SalesOrderDelete/profile?FileQueueID="+orderId.intValue();
+			Response response = RestClient.invoke(
+					url,
+					MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON,
+					"get", null, null, null, null, userName, password,1,1,AppLogger.getSystemLogger());
+			
+			
+			/*Response response = invoke(
+					"http://www.espncricinfo.com",
+					MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON,
+					GET_OPERATION, null, null, null, null, null, null);*/
+//			System.out.println(response.readEntity(String.class));
+			
+		//	mapper.writeValue(writer, String.class);
+			rb = Response.ok(response.readEntity(String.class));
+		} catch (WebApplicationException ex) {
+			AppLogger.getSystemLogger().error(
+					"Error in calling validation flow with order queue id " + id, ex);
+			throw ex;
+		} catch (Exception e) {
+			AppLogger.getSystemLogger().error(
+					"Error in calling validation flow with order queue id " + id, e);
+			throw new WebApplicationException(Response
+					.status(Status.INTERNAL_SERVER_ERROR)
+					.entity(ExceptionUtils.getRootCauseMessage(e))
+					.type(MediaType.TEXT_PLAIN_TYPE).build());
+		}
+		return rb.build();
 
+	}
 }
