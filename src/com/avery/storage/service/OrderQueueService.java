c13b846a69cb3e7e2d9e39888f7b1c9236492eec
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-08 14:43:03 +0530

    Added methods to cancel order and submitting order to system

--- src/com/avery/storage/service/OrderQueueService.java
+++ src/com/avery/storage/service/OrderQueueService.java
@@ -1,12 +1,36 @@
 package com.avery.storage.service;
 
-import org.springframework.stereotype.Component;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.avery.storage.dao.impl.OrderQueueDao;
 import com.avery.storage.entities.OrderQueue;
+import com.avery.storage.entities.SalesOrder;
 
 @Component
 public class OrderQueueService extends GenericEntityService<OrderQueue, Long>{
 	
-	
+	private OrderQueueDao orderQueueDao;
 
+	public OrderQueueDao getOrderQueueDao() {
+		return orderQueueDao;
+	}
+
+	@Autowired
+	public void setOrderQueueDao(OrderQueueDao orderQueueDao) {
+		this.orderQueueDao = orderQueueDao;
+	}
+	
+	@Transactional
+	public void submitOrderToSystem(String data,Long entityId){
+		getOrderQueueDao().submitOrderToSystem(data,entityId);
+	}
+	
+	@Transactional
+	public void cancelOrder(String data,Long entityId){
+		getOrderQueueDao().cancelOrder(data,entityId);
+	}
 }
