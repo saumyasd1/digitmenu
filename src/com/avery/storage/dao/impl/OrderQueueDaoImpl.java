9b799792799cf09984057e3a96d77774125b88e0
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-08 14:37:13 +0530

    Added methods to cancel order nad submitting order to system

--- src/com/avery/storage/dao/impl/OrderQueueDaoImpl.java
+++ src/com/avery/storage/dao/impl/OrderQueueDaoImpl.java
@@ -4,19 +4,29 @@ import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
+import com.avery.logging.AppLogger;
 import com.avery.storage.dao.GenericDaoImpl;
 import com.avery.storage.entities.OrderQueue;
-import com.avery.storage.entities.Partner;
 import com.avery.utils.ApplicationUtils;
 import com.avery.utils.HibernateUtils;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Repository
 public class OrderQueueDaoImpl extends GenericDaoImpl<OrderQueue, Long> implements
@@ -79,5 +89,91 @@ public class OrderQueueDaoImpl extends GenericDaoImpl<OrderQueue, Long> implemen
         entitiesMap.put("totalCount", totalCount);
         entitiesMap.put("orders", new LinkedHashSet(criteria.list()));
 		return entitiesMap;
+	}
+
+	@Override
+	public void submitOrderToSystem(String data, Long entityId) {
+		ObjectMapper mapper = new ObjectMapper();
+		Long currentObjId=0L;
+		ObjectReader updater=null;
+		Session session = null;
+		try{
+			mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+			mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+					false);
+			session = getSessionFactory().getCurrentSession();
+			OrderQueue orderQueueObj=null;
+			orderQueueObj=(OrderQueue) session.get(OrderQueue.class,entityId);
+			updater = mapper.readerForUpdating(orderQueueObj);
+			orderQueueObj = updater.readValue(data);
+			orderQueueObj.preUpdateOp();
+			session.update(orderQueueObj);
+			orderQueueObj.postUpdateOp();
+			String status=orderQueueObj.getStatus();
+			String s = "update SalesOrder set status=:value where orderQueueID=:id";
+			Query q = session.createQuery(s);
+			q.setString("value",status);
+			q.setLong("id",entityId);
+			q.executeUpdate();
+		}catch (WebApplicationException ex) {
+			AppLogger.getSystemLogger().error(
+					"Error while Performing bulk update ", ex);
+			throw ex;
+		} catch (Exception e) {
+			AppLogger.getSystemLogger().error(
+					"Error while Performing bulk update ", e);
+			throw new WebApplicationException(Response
+					.status(Status.INTERNAL_SERVER_ERROR)
+					.entity(ExceptionUtils.getRootCauseMessage(e))
+					.type(MediaType.TEXT_PLAIN_TYPE).build());
+		}
+		
+	}
+	
+	@Override
+	public void cancelOrder(String data, Long entityId) {
+		ObjectMapper mapper = new ObjectMapper();
+		Long currentObjId=0L;
+		ObjectReader updater=null;
+		Session session = null;
+		String commentString="";
+		try{
+			mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+			mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+					false);
+			session = getSessionFactory().getCurrentSession();
+			OrderQueue orderQueueObj=null;
+			orderQueueObj=(OrderQueue) session.get(OrderQueue.class,entityId);
+			updater = mapper.readerForUpdating(orderQueueObj);
+			orderQueueObj = updater.readValue(data);
+			orderQueueObj.preUpdateOp();
+			session.update(orderQueueObj);
+			orderQueueObj.postUpdateOp();
+			String status=orderQueueObj.getStatus();
+			String comment=orderQueueObj.getComment();
+			if(!"".equals(comment)){
+				commentString=",comment=:comment ";
+			}
+			String s = "update OrderLine set status=:value "+commentString+" where orderQueueID=:id";
+			Query q = session.createQuery(s);
+			q.setString("value",status);
+			if(!"".equals(comment)){
+				q.setString("comment",comment);
+			}
+			q.setLong("id",entityId);
+			q.executeUpdate();
+		}catch (WebApplicationException ex) {
+			AppLogger.getSystemLogger().error(
+					"Error while Performing bulk update ", ex);
+			throw ex;
+		} catch (Exception e) {
+			AppLogger.getSystemLogger().error(
+					"Error while Performing bulk update ", e);
+			throw new WebApplicationException(Response
+					.status(Status.INTERNAL_SERVER_ERROR)
+					.entity(ExceptionUtils.getRootCauseMessage(e))
+					.type(MediaType.TEXT_PLAIN_TYPE).build());
+		}
+		
 	}
 }
