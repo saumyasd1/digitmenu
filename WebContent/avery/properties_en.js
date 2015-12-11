5de6a46a1881d6a7e7909f6ee8e0792ce0f03c2f
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-11 12:46:12 +0530

    Adding deleteImage path in properties file

--- WebContent/avery/properties_en.js
+++ WebContent/avery/properties_en.js
@@ -12,14 +12,20 @@ clearSearchIcon='avery/resources/images/invalid_field.jpg',
 errorIcon='avery/resources/images/invalid_field.jpg',
 cancelOrderText='<b>Are you sure you want to cancel the Order? If yes, please enter the reason for futher reference:</b> ',
 advancedSearchWindowTitle='Advanced Search',
-cancelStatus='8'
 createSalesOrderBtnText='<b>Create Sales Order</b>',
 viewSalesOrderBtnText='<b>View Sales Order</b>',
 cancelText='<b>cancel</b>',
 cancelSalesOrderText='<b>Cancel Sales Order</b>',
 salesOrdersumbitText='<b>Submit to System</b>',
-submitToOracleStatus='6',
 dateFormat='Y-m-d',
-bulkUpdateButtonText='<b>Bulk Update</b>'
+bulkUpdateButtonText='<b>Bulk Update</b>',
+attacheImageSrc='avery/resources/images/attachmentImage.png',
+deleteImageSrc='avery/resources/images/delete.png',
+//Status Code
+orderReceivedStatus='1',
+orderError='2',
+orderPreProcessedStatus='3',
+submitToOracleStatus='6',	
+cancelStatus='8'
 ;
 
