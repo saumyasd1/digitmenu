7cff68122c5fc51ee169bc0ea054bacf553df18f
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-17 18:46:05 +0530

    First Commit

---
+++ WebContent/avery/alert_message.js
@@ -0,0 +1,9 @@
+var webSubmissionSuccesFulMsg='The form was successfully sumbitted. Please go to Order Queue to see your order.',
+salesOrderCreationMsg='Sales Order was successfully created',
+uploadOnlyExcelFileAlert='Please attach only xls or xlxs type of files',
+uploadOnlyPdfFileAlert='Please attach only pdf type of files',
+orderCancelSuccessAlert='Order cancelled successfully',
+orderLineMandatoryFieldMissingAlt='One of the mandatory fields is empty. Please ensure that all mandatory fields are filled before proceeding.',
+orderLineMandatoryValidationFieldMissingAlt='One of the mandatory validation has failed. Please resolve it before proceeding.'
+;
+
