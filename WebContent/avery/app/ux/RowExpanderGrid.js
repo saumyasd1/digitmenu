0f92f585334b39bdc19e5b0394904546a0b1de7b
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-17 18:38:34 +0530

    Changing the implementation of inner grid

--- WebContent/avery/app/ux/RowExpanderGrid.js
+++ WebContent/avery/app/ux/RowExpanderGrid.js
@@ -1,111 +1,177 @@
-Ext.define('AOC.overrides.view.Table', {
-    override: 'Ext.view.Table',
-    checkThatContextIsParentGridView: function(e){
-        var target = Ext.get(e.target);
-        var parentGridView = target.up('.x-grid-view');
-        if (this.el.getId() != parentGridView.getId()) {
-            return false;
-        } else {
-            return true;
-        }
-    },
-    processItemEvent: function(record, row, rowIndex, e) {
-        if (e.target && !this.checkThatContextIsParentGridView(e)) {
-            return false;
-        } else {
-            return this.callParent([record, row, rowIndex, e]);
-        }
-    }
-});
+
 Ext.define('AOC.ux.RowExpanderGrid', {
-    extend: 'Ext.grid.plugin.RowExpander',
-    requires: [
-        'Ext.grid.feature.RowBody',
-        'Ext.grid.plugin.RowExpander',
-        'Ext.grid.Panel'
-    ],
-    alias: 'plugin.rowexpandergrid',
-     gridConfig:null,
-     expandOnDblClick:false,
-     expandOnEnter:false,
-     setCmp: function(outerGrid) {
-		var me = this;
-		this.rowBodyTpl=new Ext.XTemplate('<div class="detailData"></div>');
-		 me.callParent(arguments);
-        if (!me.gridConfig) {
-            Ext.Error.raise("The 'gridConfig' config is required and is not defined.");
-        }
-     },
-    init:function(outerGrid){
-    	var me = this;
-    	this.callParent(arguments);
-    	outerGrid.getView().on('expandbody',me.addInnerGridOnExpand,me);
-    },
-      
-    addInnerGridOnExpand : function (rowNode, record, expandRow, eOpts) {
-    	 var me=this;
-        if( Ext.fly(rowNode).down('.x-grid-view')){
-            return;
-        }
-         me.recordsExpanded[record.internalId] = false;
-    	 var detailData = Ext.DomQuery.select("div.detailData", expandRow);
-    	 var data=record.get(me.gridConfig.nestedGridRefrence);
-    	 var store = Ext.create('Ext.data.Store', {
-    		    autoLoad: true,
-    		    model: Ext.create(me.gridConfig.modal),
-    		    data : data,
-    		    proxy: {
-    		        type: 'memory'
-    		    }
-    		});
-    	 me.gridConfig.store=store;
-		 var innerGrid=Ext.create('Ext.grid.Panel',me.gridConfig);
-		 innerGrid.render(detailData[0]);
-    },
-    getHeaderConfig: function() {
-        var me = this,
-            lockable = me.grid.ownerLockable;
+	/*
+	 * Inspired by : http://www.rahulsingla.com/blog/2010/04/extjs-preserving-rowexpander-markup-across-view-refreshes
+	 * Reappend element to DOM : http://stackoverflow.com/questions/20143082/does-extjs-automatically-garbage-collect-components
+	 */
 
-        return {
-            width: me.headerWidth,
-            lockable: false,
-            autoLock: true,
-            sortable: false,
-            resizable: false,
-            draggable: false,
-            hideable: false,
-            menuDisabled: true,
-            tdCls: Ext.baseCSSPrefix + 'grid-cell-special',
-            innerCls: Ext.baseCSSPrefix + 'grid-cell-inner-row-expander',
-            renderer: function(type, config, cell, rowIndex, cellIndex, e, view) {
-            	var me=this;
-            	var record=config.record;
-            	var nestedGridRefrence=me.features[0].rowExpander.gridConfig.nestedGridRefrence;
-            	var length=record.get(nestedGridRefrence).length;
-            	if(length!=0)
-            		return '<div class="' + Ext.baseCSSPrefix + 'grid-row-expander" role="presentation"></div>';
-            	else
-            		return '<div class="hiderowexpander" role="presentation"></div>';
-            },
-            processEvent: function(type, view, cell, rowIndex, cellIndex, e, record) {
-                if (e.getTarget('.' + Ext.baseCSSPrefix + 'grid-row-expander')) {
-                    if (type === "click") {
-                        me.toggleRow(rowIndex, record);
-                        return me.selectRowOnExpand;
-                    }
-                }
-            },
+	    extend: 'Ext.ux.RowExpander',
 
-            // This column always migrates to the locked side if the locked side is visible.
-            // It has to report this correctly so that editors can position things correctly
-            isLocked: function() {
-                return lockable && (lockable.lockedGrid.isVisible() || this.locked);
-            },
+	    alias: 'plugin.cmprowexpander',
 
-            // In an editor, this shows nothing.
-            editRenderer: function() {
-                return '&#160;';
-            }
-        };
-    }
-});
+	    rowBodyTpl : ['<div></div>'],
+	    expandOnDblClick:false,
+	     expandOnEnter:false,
+	    obj_recordId_componentId: {},
+	     
+	    init: function(grid) {
+	        this.callParent(arguments) ;
+	        
+	        var view = grid.getView() ;
+	        view.on('refresh', this.onRefresh, this);
+	        view.on('expandbody', this.onExpand, this);
+	        
+	        grid.on('destroy', this.onDestroyGrid, this) ;
+	        grid.headerCt.on('columnresize', this.onColumnResize, this) ;
+	        
+	        this.obj_recordId_componentId = {} ;
+	    },
+	    
+	    getRecordKey: function(record) {
+	        return (record.internalId);
+	    },
+	    
+	    createComponent: function(view, record, rowNode, rowIndex) {
+	        return Ext.create('Ext.Component') ;
+	    },
+	    
+	    onExpand: function(rowNode, record, expandRow) {
+	        var recordId = this.getRecordKey(record) ;
+	        if( Ext.isEmpty( this.obj_recordId_componentId[recordId] ) ) {
+	            var view = this.grid.getView(),
+	                newComponent = this.createComponent(view, record, rowNode, view.indexOf(rowNode)),
+	                targetRowbody = Ext.DomQuery.selectNode('div.x-grid-rowbody', expandRow) ;
+	            
+	            while (targetRowbody.hasChildNodes()) {
+	                targetRowbody.removeChild(targetRowbody.lastChild);
+	            }
+	            newComponent.render( targetRowbody ) ;
+	            newComponent.getEl().swallowEvent([
+	                                        'mousedown', 'mouseup', 'click',
+	                                        'contextmenu', 'mouseover', 'mouseout',
+	                                        'dblclick', 'mousemove'
+	                                    ]);
+	            this.obj_recordId_componentId[recordId] = newComponent.getId() ;
+	        }
+	    },
+	    
+	    onRefresh: function(view) {
+	        var reusedCmpIds = [] ;
+	        Ext.Array.each( view.getNodes(), function(node) {
+	            var record = view.getRecord(node),
+	                recordId = this.getRecordKey(record) ;
+	                
+	            if( !Ext.isEmpty(this.obj_recordId_componentId[recordId]) ) {
+	                var cmpId = this.obj_recordId_componentId[recordId] ;
+	                
+	                reusedCmpIds.push(cmpId) ;
+	                var reusedComponent = Ext.getCmp(this.obj_recordId_componentId[recordId]),
+	                    targetRowbody = Ext.DomQuery.selectNode('div.x-grid-rowbody', node);
+	                while (targetRowbody.hasChildNodes()) {
+	                    targetRowbody.removeChild(targetRowbody.lastChild);
+	                }
+	                
+	                targetRowbody.appendChild( reusedComponent.getEl().dom );
+	                reusedComponent.doLayout() ;
+	            }
+	        },this) ;
+	        
+	        
+	        // Do Garbage collection
+	        // Method 1 ( http://skirtlesden.com/static/ux/download/component-column/1.1/Component.js )
+	        var keysToDelete = [] ;
+	        Ext.Object.each( this.obj_recordId_componentId, function( recordId, testCmpId ) {
+	            comp = Ext.getCmp(testCmpId);
+	            el = comp && comp.getEl();
+
+	            if (!el || (true && (!el.dom || Ext.getDom(Ext.id(el)) !== el.dom))) {
+	                // The component is no longer in the DOM
+	                if (comp && !comp.isDestroyed) {
+	                    comp.destroy();
+	                    keysToDelete.push(recordId) ;
+	                }
+	            }
+	        }) ;
+	        
+	        // Method 2
+	        /*
+	        Ext.Object.each( this.obj_recordId_componentId, function( recordId, testCmpId ) {
+	            if( !Ext.Array.contains( reusedCmpIds, testCmpId ) ) {
+	                comp = Ext.getCmp(testCmpId);
+	                comp.destroy();
+	                keysToDelete.push(recordId) ;
+	            }
+	        }) ;
+	        */
+	        
+	        // Clean map
+	        Ext.Array.each( keysToDelete, function(mkey) {
+	            delete this.obj_recordId_componentId[mkey] ;
+	        },this);
+	    },
+	    
+	    onColumnResize: function() {
+	        Ext.Object.each( this.obj_recordId_componentId, function( recordId, cmpId ) {
+	            Ext.getCmp(cmpId).doLayout();
+	        }) ;
+	    },
+	     
+	    onDestroyGrid: function() {
+	        Ext.Object.each( this.obj_recordId_componentId, function(recordId, cmpId) {
+	            Ext.getCmp(cmpId).destroy() ;
+	        }) ;
+	     },
+	     getHeaderConfig: function() {
+	         var me = this,
+	             lockable = me.grid.ownerLockable;
+
+	         return {
+	             width: me.headerWidth,
+	             lockable: false,
+	             autoLock: true,
+	             sortable: false,
+	             resizable: false,
+	             draggable: false,
+	             hideable: false,
+	             menuDisabled: true,
+	             tdCls: Ext.baseCSSPrefix + 'grid-cell-special',
+	             innerCls: Ext.baseCSSPrefix + 'grid-cell-inner-row-expander',
+	             renderer: function(type, config, cell, rowIndex, cellIndex, e, view) {
+	             	var me=this;
+	             	var record=config.record;
+	             	var nestedGridRefrence=me.nestedGridRefrence;
+	             	var length=record.get(nestedGridRefrence).length;
+	             	if(me.showMandatoryValidationField){
+	             		var mandatoryVariableDataFieldFlag=record.data.mandatoryVariableDataFieldFlag;
+		        		var checkvalue=mandatoryVariableDataFieldFlag.trim();
+		    			if(checkvalue.substr(0,1)!='S' && record.get('status')==waitingForCSRStatus){
+		    				config.tdCls='invalid';
+		    			}
+	             	}
+	             	if(length!=0)
+	             		return '<div class="' + Ext.baseCSSPrefix + 'grid-row-expander" role="presentation"></div>';
+	             	else
+	             		return '<div class="hiderowexpander" role="presentation"></div>';
+	             },
+	             processEvent: function(type, view, cell, rowIndex, cellIndex, e, record) {
+	                 if (e.getTarget('.' + Ext.baseCSSPrefix + 'grid-row-expander')) {
+	                     if (type === "click") {
+	                         me.toggleRow(rowIndex, record);
+	                         return me.selectRowOnExpand;
+	                     }
+	                 }
+	             },
+
+	             // This column always migrates to the locked side if the locked side is visible.
+	             // It has to report this correctly so that editors can position things correctly
+	             isLocked: function() {
+	                 return lockable && (lockable.lockedGrid.isVisible() || this.locked);
+	             },
+
+	             // In an editor, this shows nothing.
+	             editRenderer: function() {
+	                 return '&#160;';
+	             }
+	         };
+	     }
+	});
