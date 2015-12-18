c07b186c1f42b90232a050611f2243c2817dcb1b
Shaifali <Shaifali@ADLP046>
2015-12-18 16:28:24 +0530

    added valid image

--- WebContent/avery/app/view/address/AddAddress.js
+++ WebContent/avery/app/view/address/AddAddress.js
@@ -262,7 +262,7 @@ Ext.define('AOC.view.address.AddAddress',{
 		   },
 		   notifyByImage : function(config){
 		    	 if(config.isValid())
-		    		   config.setFieldStyle('background-image:url(avery/resources/images/valid_field.png);background-repeat:no-repeat;background-position:right;');
+		    		   config.setFieldStyle('background-image:url(avery/resources/images/valid_field.jpg);background-repeat:no-repeat;background-position:right;');
 					else
 					   config.setFieldStyle('background-image:url(avery/resources/images/invalid_field.jpg);background-repeat:no-repeat;background-position:right;');
 		     }
