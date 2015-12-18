129e6ee768fbb392a21442cea0ff38052a5cda76
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-18 17:23:07 +0530

    Adding "validateFieldFailedConfirmatonMsg" variable

--- WebContent/avery/alert_message.js
+++ WebContent/avery/alert_message.js
@@ -4,6 +4,7 @@ uploadOnlyExcelFileAlert='Please attach only xls or xlxs type of files',
 uploadOnlyPdfFileAlert='Please attach only pdf type of files',
 orderCancelSuccessAlert='Order cancelled successfully',
 orderLineMandatoryFieldMissingAlt='One of the mandatory fields is empty. Please ensure that all mandatory fields are filled before proceeding.',
-orderLineMandatoryValidationFieldMissingAlt='One of the mandatory validation has failed. Please resolve it before proceeding.'
+orderLineMandatoryValidationFieldMissingAlt='One of the mandatory validation has failed. Please resolve it before proceeding.',
+validateFieldFailedConfirmatonMsg='Some of the validations has failed. Are you sure you want to continue?'
 ;
 
