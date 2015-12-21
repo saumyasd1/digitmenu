e4bf2335d67a4f63f331e4066ee4bb580a98a3b1
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-21 16:28:00 +0530

    Modifing so that extra JSONObject object is not created every time this method is called

--- src/com/avery/utils/ApplicationUtils.java
+++ src/com/avery/utils/ApplicationUtils.java
@@ -5,28 +5,22 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
-import org.json.JSONException;
-import org.json.JSONObject;
 public class ApplicationUtils {
 
 	public static DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
     
-	public static HashMap<String,String> convertJSONtoMaps(String jsonStr) throws JsonParseException, JsonMappingException, IOException {
+	public static HashMap<String,String> convertJSONtoMaps(String jsonStr) throws  IOException,Exception {
 		HashMap<String,String> map = new HashMap<String,String>();
-		JSONObject jsonObj = null;
 		try {
 			if(jsonStr != null)
 			{
-				jsonObj= new JSONObject(jsonStr);
 				ObjectMapper mapper = new ObjectMapper();
-				map = mapper.readValue(jsonObj.toString(), 
+				map = mapper.readValue(jsonStr, 
 					    new TypeReference<HashMap<String,String>>(){});
 			}
-		} catch (JSONException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return map;
