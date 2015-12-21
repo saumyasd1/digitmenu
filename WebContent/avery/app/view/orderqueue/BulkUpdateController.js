cacfac29760ba2715a118080241e0a7ee5936978
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-21 19:12:10 +0530

    [#AOC-38] Change in GUI for Bulk Update Page

--- WebContent/avery/app/view/orderqueue/BulkUpdateController.js
+++ WebContent/avery/app/view/orderqueue/BulkUpdateController.js
@@ -11,39 +11,55 @@ Ext.define('AOC.view.orderqueue.BulkUpdateController', {
     	parms ='';
     	var i=0, insertShipAddress=false,insertBillAddress=false;
     	var updatedRecords=store.getModifiedRecords();
-    	Ext.each(updatedRecords,function(currentRecord){
-    		i=store.find('id',currentRecord.id);
-    		if(i==0){
-    			if(currentRecord.isModified('oracleBilltoSiteNumber') &&  currentRecord.get('oracleBilltoSiteNumber')!=null && currentRecord.get('oracleBilltoSiteNumber')!=''){
-    	  			insertBillAddress=true;
-    	  	  }
-    	  		if(currentRecord.isModified('oracleShiptoSiteNumber') &&  currentRecord.get('oracleShiptoSiteNumber')!=null && currentRecord.get('oracleShiptoSiteNumber')!=''){
-    	  			insertShipAddress=true;
-    	  		}
+    	if(updatedRecords.length==0){
+    		Ext.Msg.alert('',noRecordsToUpdateMessage);
+    		Ext.getBody().unmask();
+    		return false;
+    	}else{
+    		Ext.getBody().unmask();
+    		Ext.Msg.confirm('',confirmBulkUpdateSave,function(btn){
+    			if(btn=='yes'){
+    				var mask=Ext.getBody().mask('Saving....');
+    				mask.dom.style.zIndex = '99999';
+    				Ext.each(updatedRecords,function(currentRecord){
+    		    		i=store.find('id',currentRecord.id);
+    		    		if(i==0){
+    		    			if(currentRecord.isModified('oracleBilltoSiteNumber') &&  currentRecord.get('oracleBilltoSiteNumber')!=null && currentRecord.get('oracleBilltoSiteNumber')!=''){
+    		    	  			insertBillAddress=true;
+    		    	  	  }
+    		    	  		if(currentRecord.isModified('oracleShiptoSiteNumber') &&  currentRecord.get('oracleShiptoSiteNumber')!=null && currentRecord.get('oracleShiptoSiteNumber')!=''){
+    		    	  			insertShipAddress=true;
+    		    	  		}
+    		    			}
+    		    			
+    		    		var obj=currentRecord.getChanges( ) ;
+    		    		obj.id=currentRecord.id;
+    		    		if(parms=='')
+    		    			parms=parms+Ext.encode(obj);
+    		    		else 
+    		    			parms=parms+'@@@'+Ext.encode(obj);
+    		    		  
+    		    		 });
+    		    	var obj='{"insertBillAddress":'+insertBillAddress+',"insertShipAddress":'+insertShipAddress+',"data":'+Ext.encode(parms)+',"orderQueueId":"'+me.runTime.getOrderQueueId()+'"}';
+    		    	Ext.Ajax.request({
+    		    		method:'PUT',
+    			        jsonData:obj,
+    		    		   url : applicationContext+'/rest/orderLines/bulkupdate',
+    				        success : function(response, opts) {
+    					  		Ext.Msg.alert('','Order line successfully updated');
+    					  		Ext.getBody().unmask();
+    					  		me.getView().store.load();
+    				        },
+    				        failure: function(response, opts) {
+    				        	Ext.getBody().unmask();
+    			          }
+    		    		  });
+    			}else{
+    				Ext.getBody().unmask();
     			}
-    			
-    		var obj=currentRecord.getChanges( ) ;
-    		obj.id=currentRecord.id;
-    		if(parms=='')
-    			parms=parms+Ext.encode(obj);
-    		else 
-    			parms=parms+'@@@'+Ext.encode(obj);
-    		  
-    		 });
-    	var obj='{"insertBillAddress":'+insertBillAddress+',"insertShipAddress":'+insertShipAddress+',"data":'+Ext.encode(parms)+',"orderQueueId":"'+me.runTime.getOrderQueueId()+'"}';
-    	Ext.Ajax.request({
-    		method:'PUT',
-	        jsonData:obj,
-    		   url : applicationContext+'/rest/orderLines/bulkupdate',
-		        success : function(response, opts) {
-			  		Ext.Msg.alert('','Order line successfully updated');
-			  		Ext.getBody().unmask();
-			  		me.getView().store.load();
-		        },
-		        failure: function(response, opts) {
-		        	Ext.getBody().unmask();
-	          }
-    		  }); 
+    		});
+    	}
+     
     },
     cancelChanges:function(){
     	var me=this;
@@ -58,6 +74,16 @@ Ext.define('AOC.view.orderqueue.BulkUpdateController', {
     	var store=grid.store,
     	parms ='';
     	var updatedRecords=store.getModifiedRecords();
+    	if(updatedRecords.length==0){
+    		Ext.Msg.alert('',noRecordsToUpdateMessage);
+    		Ext.getBody().unmask();
+    		return false;
+    	}else{
+    		Ext.getBody().unmask();
+    		Ext.Msg.confirm('',confirmBulkUpdateSave,function(btn){
+    			if(btn=='yes'){
+    				var mask=Ext.getBody().mask('Saving....');
+    				mask.dom.style.zIndex = '99999';
     	Ext.each(updatedRecords,function(record){
     		var obj=record.getChanges( ) ;
     		obj.id=record.id;
@@ -81,6 +107,9 @@ Ext.define('AOC.view.orderqueue.BulkUpdateController', {
 		        	Ext.getBody().unmask();
 	          }
     		  }); 
-    	
+    	}
+    		
+    	});
+    	}
     }
 })
