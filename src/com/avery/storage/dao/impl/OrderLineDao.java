8e4cd863b5c2b5811dab12a542b672c5c725edbd
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-21 16:10:20 +0530

    [#AOC-50] blank address case should not be uploaded to the address manage tab for future site # mapping

--- src/com/avery/storage/dao/impl/OrderLineDao.java
+++ src/com/avery/storage/dao/impl/OrderLineDao.java
@@ -1,6 +1,7 @@
 package com.avery.storage.dao.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import com.avery.storage.dao.GenericDao;
 import com.avery.storage.entities.OrderLine;
@@ -9,6 +10,6 @@ import com.avery.storage.entities.ProductLine;
 public interface OrderLineDao extends GenericDao<OrderLine, Long>{
 	
 	public List<OrderLine> readAllByOrderID(Long orderID);
-	public void bulkUpdate(String jsonData,boolean insertAddress);
-	public void bulkUpdateAllById(String jsonData,boolean insertAddress,Long orderQueueId);
+	public void bulkUpdate(String jsonData,Map<String,Boolean> insertAddress);
+	public void bulkUpdateAllById(String jsonData,Map<String,Boolean> insertAddress,Long orderQueueId);
 }
