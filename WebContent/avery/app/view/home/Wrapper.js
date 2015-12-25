0014e77df010d0bc46cb2eda023caaf6db18ea79
Amit <Amit@ADLP057_Amit>
2015-12-25 09:51:27 +0530

    change to implement new design

---
+++ WebContent/avery/app/view/home/Wrapper.js
@@ -0,0 +1,19 @@
+Ext.define('AOC.view.home.Wrapper', {
+	extend : 'Ext.Container',
+	alias : 'widget.homewrapper',
+	requires:['AOC.view.home.HomeChart'],
+	layout: {
+        type: 'fit'
+        },
+	initComponent : function() {
+		Ext.apply(this, {
+		items:this.buildItems()	
+		});
+		this.callParent(arguments);
+	},
+	buildItems:function(){
+	    return [{
+		xtype:'odersoverviewchart'
+	    }]
+	}
+});
