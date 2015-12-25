54feb664408495ecbcc6aa766ff35076457ed36b
Amit <Amit@ADLP057_Amit>
2015-12-25 10:00:50 +0530

    Change to implement new GUI design

--- WebContent/avery/app/config/Settings.js
+++ WebContent/avery/app/config/Settings.js
@@ -1,8 +1,16 @@
 Ext.define('AOC.config.Settings',{
-    statics : {
-        buttonIcons : {
-           aocHeader:'avery/resources/images/aoc-header.png',
-           logout:'avery/resources/images/logout.png'
-        }
+    singleton : true,
+    config : {
+        baseBackgroundColor : 'background-color:#F6F6F6'
+    },
+    buttonIcons : {
+         logoImage:'avery/resources/images/logo_avery.gif',
+         logout:'avery/resources/images/logout.png',
+         settings:'avery/resources/images/settings.png',
+         notification:'avery/resources/images/notification.png',
+         arrowDown:'avery/resources/images/arrow-down.png'
+    },
+    constructor : function(config){
+            this.initConfig(config);
     }
 });
