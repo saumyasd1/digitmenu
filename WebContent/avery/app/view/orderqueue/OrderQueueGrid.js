ae9ad544e8e719334488c8064ec190555f12fc9a
Shaifali <Shaifali@ADLP046>
2015-12-26 10:55:18 +0530

    added column lines

--- WebContent/avery/app/view/orderqueue/OrderQueueGrid.js
+++ WebContent/avery/app/view/orderqueue/OrderQueueGrid.js
@@ -10,7 +10,7 @@ Ext.define('AOC.view.orderqueue.OrderQueueGrid', {
 	this.fieldArray = [];
         Ext.apply(this,{
             columns : this.buildColumns(),
-			columnLines:true,
+			columnLines:false,
 			dockedItems : this.buildDockedItems(),
 			layout:'fit',
 			tbar: { 
@@ -25,23 +25,29 @@ Ext.define('AOC.view.orderqueue.OrderQueueGrid', {
     },
     buildColumns : function(){
     	var store= Ext.data.StoreManager.lookup('orderfilequeueid') == null ? AOC.util.Helper.getCodeStore('orderfilequeue') : Ext.data.StoreManager.lookup('orderfilequeueid')
-        return [{
-        	xtype:'rownumberer'
-        },{
-        	header: '<img src="' + menuIcon + '" />',
+        return [
+//                {
+//        	xtype:'rownumberer'
+//             },
+             {
+//        	header: '<img src="' + menuIcon + '" />',
+            text:'',
             width:25,
 			xtype:'actioncolumn',
 			menuDisabled  :true,
+			baseCls:'custom-action',
 			items:[{
 			  icon:menuIcon,
 			  handler:'showMenu'
 		  }]
         },
         {
-        	header: '<img src="' + commentIcon + '" />',
+//        	header: '<img src="' + commentIcon + '" />',
+        	text:'',
             width:40,
 			dataIndex:'Comments',
 			menuDisabled  :true,
+			baseCls:'custom-action',
 			renderer:function(value, metadata,rec){
 				if(value){
 					var comment=Ext.String.htmlEncode(rec.data.Comments);
@@ -53,10 +59,12 @@ Ext.define('AOC.view.orderqueue.OrderQueueGrid', {
 		   	},
 			
         {
-		    header: '<img src="' + errorIcon + '" />',
+//		    header: '<img src="' + errorIcon + '" />',
+		   	text:'',
             width:40,
 			dataIndex:'error',
 			menuDisabled  :true,
+			baseCls:'custom-action',
 			renderer:function(value, metadata,rec){
 				if(value){
 					var error=Ext.String.htmlEncode(rec.data.error);
@@ -71,6 +79,7 @@ Ext.define('AOC.view.orderqueue.OrderQueueGrid', {
             width:40,
 			dataIndex:'OrderSource',
 			menuDisabled  :true,
+			baseCls:'custom-action',
 			renderer:function(v,cell,record){
 				if(v=='Email')
 					return '<div><img data-qtip="<font color=blue>Email</font>" src="' + mailIcon + '" /></div>';
@@ -81,17 +90,20 @@ Ext.define('AOC.view.orderqueue.OrderQueueGrid', {
         {
             text : 'Order track#',
             width:45,
-			dataIndex:'id'
+			dataIndex:'id',
+			baseCls:'custom-action'
         },
         {
             text : 'PO#',
             width:120,
-			dataIndex:'ponumber'
+			dataIndex:'ponumber',
+			baseCls:'custom-action'
         },
         {
             text : 'Order File',
             width:45,
 			dataIndex:'OrderFile',
+			baseCls:'custom-action',
 			renderer:function(v){
 				if(v.length!=0){
 					var fileName=v[0].fileName
@@ -105,6 +117,7 @@ Ext.define('AOC.view.orderqueue.OrderQueueGrid', {
             width:75,
             hideable: true,
             dataIndex:'attachmentPresent',
+            baseCls:'custom-action',
 			renderer:function(v,cell,record){
 				if(v)
 					return '<div><img class="viewattachment" src="' + attacheImageSrc + '" /></div>';
@@ -114,44 +127,58 @@ Ext.define('AOC.view.orderqueue.OrderQueueGrid', {
         },{
             text : 'Partner Name',
             width:80,
-            dataIndex:'PartnerName'
+            dataIndex:'PartnerName',
+            baseCls:'custom-action'
         },
 		{
             text : 'RBO',
             width:80,
-            dataIndex:'RBOName'
+            dataIndex:'RBOName',
+            baseCls:'custom-action'
         },{
             text : 'Product Line', 
             width:80,
-            dataIndex:'productLineType'
+            dataIndex:'productLineType',
+            baseCls:'custom-action'
         },{
             text : 'Order Status',
-            width:120,
+            width:200,
 			dataIndex:'Status',
+			baseCls:'custom-action',
 			editor:{
 				xtype:'combo',
 				store: Ext.data.StoreManager.lookup('orderfilequeueid') == null ? AOC.util.Helper.getCodeStore('orderfilequeue') : Ext.data.StoreManager.lookup('orderfilequeueid')
 			},
 			renderer:function(v){
 				var statusRecord=store.findRecord( 'code', v);
-				if(statusRecord.get('value')!='')
+				var va=statusRecord.get('value');
+				if(statusRecord.get('value')=="SO Generated" || statusRecord.get('value')=="SO Submitted" || statusRecord.get('value')=="Booked" || statusRecord.get('value')=="Pre-Processed" || statusRecord.get('value')=="Received")
 				   {
-					var va=statusRecord.get('value');
-				    return '<div><span data-qtip="'+va+'" />'+va+'</span></div>';
+				    return '<div><img  src="' + tickIcon + '" /><font color=#009966>&nbsp&nbsp&nbsp'+va+'</font></div>';
 				   }
+				else if(statusRecord.get('value')=="Waiting CS Verification")
+					{
+					 return '<div><img  src="' + warningImageSrc + '" /><font color=#FF9933>&nbsp&nbsp&nbsp'+va+'</font></div>';
+					}
+				else if(statusRecord.get('value')=="Cancelled")
+				{
+				 return '<div><img  src="' + cancelIcon + '" /><font color=silver>&nbsp&nbsp&nbsp'+va+'</font></div>';
+				}
 				else
-					return '';
+					return '<div><img  src="' + errorStatusIcon + '" /><font color=red>&nbsp&nbsp&nbsp'+va+'</font</div>';
 			}
         },
 		{
             text : 'Processed Date',
             width:98,
-			dataIndex:'receivedDate'
+			dataIndex:'receivedDate',
+			baseCls:'custom-action'
         },
         {
             text : 'Sender Email ID',
             width:128,
 			dataIndex:'SenderEmailID',
+			baseCls:'custom-action',
 			renderer:function(v){
 				if(v){
 					return '<div><span data-qtip="'+v+'" />'+v+'</span></div>';
@@ -163,6 +190,7 @@ Ext.define('AOC.view.orderqueue.OrderQueueGrid', {
             text : 'Subject',
             width:150,
 			dataIndex:'Subject',
+			baseCls:'custom-action',
 			renderer:function(v){
 				if(v){
 					return '<div><span data-qtip="<font color=blue>'+v+'</font>" />'+v+'</span></div>';
@@ -174,6 +202,7 @@ Ext.define('AOC.view.orderqueue.OrderQueueGrid', {
             text : 'Email Body',
             width:150,
 			dataIndex:'EmailBody',
+			baseCls:'custom-action',
 			renderer:function(v){
 				if(v){
 					return '<div><span data-qtip="<font color=blue>'+v+'<font>" />'+v+'</span></div>';
@@ -184,18 +213,25 @@ Ext.define('AOC.view.orderqueue.OrderQueueGrid', {
 		{
             text : 'Submitted By',
             width:82,
-			dataIndex:'SubmittedBy'
+			dataIndex:'SubmittedBy',
+			baseCls:'custom-action'
         },
 		{
             text : 'Submitted Date',
             width:95,
-			dataIndex:'submittedDate'
+			dataIndex:'submittedDate',
+			baseCls:'custom-action'
         }
 		];
     },
     buildtbar:function(){
 		var me=this;
 			return [
+				   {
+					xtype : 'tbtext',
+					itemId : 'OrderQueuetextItemId',
+					text : '<div style="color:"><b>Order Queue</b></div>'
+	               },
 					 '->',
 				{
 	            	xtype: 'customsearchfield',
@@ -203,16 +239,34 @@ Ext.define('AOC.view.orderqueue.OrderQueueGrid', {
 	            	message:'Showing all accounts with',
 	    			store : Ext.data.StoreManager.lookup(me.store),
 	    			width: 200,
-	    			emptyText: "Quick Search:Partner Name "
+	    			emptyText: "Search Partner Name "
 				 },
-	            {
-					xtype:'button',
-					itemId:'advancesearchbutton',
-					text:advSearchText,
-					icon   : advSearchIcon,
-					iconAlign: "right",
-					handler:'openAdvancedSearchWindow'
-				 },
+				 {
+	                	xtype :'tbspacer',
+	                	width :10
+	        		},
+	        		{
+	        		    xtype: 'component',
+	        		    autoEl: {
+	        		        tag: 'a',
+	        		        href: '#',
+	        		        html:'<font color=#3300FF><b>Advanced Search</b></font>'
+	        		    },
+	        		    listeners: {
+	        		    	 el : {
+	        		    		    click    : 'openAdvancedSearchWindow'
+	        		    		    
+	        		    	 }
+	        	            }
+	        		},
+//	            {
+//					xtype:'button',
+//					itemId:'advancesearchbutton',
+//					text:advSearchText,
+//					icon   : advSearchIcon,
+//					iconAlign: "right",
+//					handler:'openAdvancedSearchWindow'
+//				 },
 			{
 				hidden:true, 
 				icon   : clearSearchIcon,
