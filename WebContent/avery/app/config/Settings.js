64803afbe5612736e72faa618bbf6a51a40de996
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-27 18:15:17 +0530

    adding icon paths

--- WebContent/avery/app/config/Settings.js
+++ WebContent/avery/app/config/Settings.js
@@ -8,7 +8,12 @@ Ext.define('AOC.config.Settings',{
          logout:'avery/resources/images/logout.png',
          settings:'avery/resources/images/settings.png',
          notification:'avery/resources/images/notification.png',
-         arrowDown:'avery/resources/images/arrow-down.png'
+         arrowDown:'avery/resources/images/arrow-down.png',
+         backIcon:'avery/resources/images/arrow-lef.svg',
+         tick:'avery/resources/images/tick.svg',
+         warning:'avery/resources/images/warning.svg',
+         error:'avery/resources/images/error.png',
+         cancel:'avery/resources/images/cancel.png'
     },
     constructor : function(config){
             this.initConfig(config);
