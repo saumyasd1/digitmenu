f06c3e467805bb7e86375fe3f5ab1295e439238f
Amit <Amit@ADLP057_Amit>
2015-12-28 11:21:19 +0530

    Change to implement new GUI design

---
+++ WebContent/avery/app/view/widget/PlainButton.js
@@ -0,0 +1,7 @@
+Ext.define('AOC.view.widget.PlainButton',{
+    extend        : 'Ext.button.Button',
+    alias         : 'widget.plainbutton',
+    scale         : 'large',
+    ui            : 'plain',
+    text          : 'Button'
+});
