6b5f66ddce044d40f73fee0779605b01c57229f7
Amit <Amit@ADLP057_Amit>
2015-12-28 11:11:43 +0530

    Change to implement new GUI design

---
+++ WebContent/avery/app/view/home/HomeChartController.js
@@ -0,0 +1,10 @@
+Ext.define('AOC.view.home.HomeChartController', {
+	extend: 'Ext.app.ViewController',
+    alias: 'controller.homechartcontroller',
+    //stores:['OrderCharts','HomePageOders'],
+    runTime: AOC.config.Runtime,
+    changeDateRange:function(cmp,rec){
+    var me=this,
+    value = rec.get('val');
+    }
+})
