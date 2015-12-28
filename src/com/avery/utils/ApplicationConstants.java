00beaf93175868c3da345bd78cd63bc95a93408a
Amit <Amit@ADLP057_Amit>
2015-12-28 12:04:56 +0530

    Change to implement new GUI design

---
+++ src/com/avery/utils/ApplicationConstants.java
@@ -0,0 +1,13 @@
+package com.avery.utils;
+
+public interface ApplicationConstants {
+			
+		String INVALID_USER = "Invalid user name.";
+			
+		String INVALID_PASSWORD = "Invalid password.";
+			
+		String USER_NOT_ACTIVATED = "User is not active.";
+			
+		String INVALID_USER_PORPASSWORD = "Invalid user name or password.";
+		
+}
