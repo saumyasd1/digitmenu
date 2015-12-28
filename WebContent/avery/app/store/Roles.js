947b7badc6be7900f5d581dd43421ffe53c0bc89
Amit <Amit@ADLP057_Amit>
2015-12-28 10:56:20 +0530

    Change to implement new GUI design

---
+++ WebContent/avery/app/store/Roles.js
@@ -0,0 +1,5 @@
+Ext.define('AOC.store.Roles', {
+    extend : 'Ext.data.Store',
+    model  : 'AOC.model.Role',
+   data:[{"value":"admin","displayname":"Admin"},{"value":"normal","displayname":"Normal"}]
+});
