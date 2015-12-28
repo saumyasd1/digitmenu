87440d900b232fbbeeb413913440e778a65e1c0c
Amit <Amit@ADLP057_Amit>
2015-12-28 10:57:12 +0530

    Change to implement new GUI design

--- WebContent/avery/app/util/Helper.js
+++ WebContent/avery/app/util/Helper.js
@@ -144,5 +144,27 @@ Ext.define('AOC.util.Helper',{
 		}
 		else
 			return '<div><img  src="' + AOC.config.Settings.buttonIcons.error + '" /><font color=red>&nbsp&nbsp&nbsp'+va+'</font</div>';
+    },
+    setCookie:function(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires;
+    },
+    getCookie:function(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            c = c.charAt(0)==' '?c.substring(1):c;
+            if (c.indexOf(name) == 0){
+            return c.substring(name.length,c.length);
+            }
+        }
+        return "";
+    },
+    deleteCookie:function(cname){
+    	 var name = cname + "=";
+    	document.cookie = name+"; expires=Thu, 01 Jan 1970 00:00:00 UTC";
     }
 });
