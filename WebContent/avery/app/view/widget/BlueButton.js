1069e42f43f288dd815e2ddc48eddb988c3f3230
Amit <Amit@ADLP057_Amit>
2015-12-28 11:15:30 +0530

    Change to implement new GUI design

---
+++ WebContent/avery/app/view/widget/BlueButton.js
@@ -0,0 +1,7 @@
+Ext.define('AOC.view.widget.BlueButton',{
+    extend        : 'Ext.button.Button',
+    alias         : 'widget.bluebutton',
+    scale         : 'medium',
+    ui            : 'orange',
+    text          : 'Button'
+});
