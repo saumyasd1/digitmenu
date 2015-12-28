80944948a2142c2a655ba8d8cccc5972771b8fa2
Amit <Amit@ADLP057_Amit>
2015-12-28 11:22:24 +0530

    Change to implement new GUI design

---
+++ WebContent/avery/app/view/widget/WhiteButton.js
@@ -0,0 +1,7 @@
+Ext.define('AOC.view.widget.WhiteButton',{
+    extend        : 'Ext.button.Button',
+    alias         : 'widget.whitebutton',
+    scale         : 'medium',
+    ui            : 'white',
+    text          : 'Button'
+});
