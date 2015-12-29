4c40e5e78669c6384908230406ac625570aa9e52
Shaifali <Shaifali@ADLP046>
2015-12-29 10:30:34 +0530

    Added user store

---
+++ WebContent/avery/app/store/UserStore.js
@@ -0,0 +1,10 @@
+Ext.define('AOC.store.UserStore', {
+	extend : 'Ext.data.Store',
+	model:'AOC.model.UserModel',
+	autoLoad : true,
+	remoteSort: true,
+	totalCount:'total',
+	pageSize:pageSize,
+	storeId:'UserStoreId',
+});
+
