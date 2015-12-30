295cb43bb642814614aed6cc83786dc384da92c5
Amit <Amit@ADLP057_Amit>
2015-12-30 15:27:15 +0530

    Change to implement new GUI design

--- WebContent/avery/app/view/home/HomeChartController.js
+++ WebContent/avery/app/view/home/HomeChartController.js
@@ -4,7 +4,24 @@ Ext.define('AOC.view.home.HomeChartController', {
     //stores:['OrderCharts','HomePageOders'],
     runTime: AOC.config.Runtime,
     changeDateRange:function(cmp,rec){
-    var me=this,
-    value = rec.get('val');
+    var me=this;
+    var value = rec.get('val');
+    me.loadChartData(value,cmp.xtype=='image');
+    },
+    loadChartData:function(days,loadList){
+	var chart =this.getView().down('#dashboardchart'),
+	store = chart.getStore();
+	chart.mask('Loading...');
+	store.getProxy().url=AOC.config.Settings.getBaseOrderTrendUrl()+'/chart/'+days;
+	store.load({callback:function(){
+	    chart.unmask();
+	    chart.redraw();
+	}});
+	if(loadList){
+	    var list =this.getView().down('homepageorderlist'),
+	    liststore = list.getStore();
+	    liststore.getProxy().url=AOC.config.Settings.getBaseOrderTrendUrl()+'/homelist';
+	    liststore.load();
+	}
     }
 })
