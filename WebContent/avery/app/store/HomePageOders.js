f9f0cd3526e81c6ecaad890f06c51d3c7d46c742
Amit <Amit@ADLP057_Amit>
2015-12-30 15:20:28 +0530

    Change to implement new GUI design

--- WebContent/avery/app/store/HomePageOders.js
+++ WebContent/avery/app/store/HomePageOders.js
@@ -1,11 +1,15 @@
 Ext.define('AOC.store.HomePageOders', {
     extend : 'Ext.data.Store',
+    requires:['AOC.model.HomePageOder'],
     model  : 'AOC.model.HomePageOder',
-    data   : [
-             {"orderType":"Received" ,"lastOneDay":10,"lastWeak":76,"lastTwoWeak":78,"lastMonth":200},
-             {"orderType":"Waiting CS Review" ,"lastOneDay":10,"lastWeak":76,"lastTwoWeak":78,"lastMonth":200},
-             {"orderType":"Waiting System Response" ,"lastOneDay":10,"lastWeak":76,"lastTwoWeak":78,"lastMonth":200},
-             {"orderType":"Successful" ,"lastOneDay":10,"lastWeak":76,"lastTwoWeak":78,"lastMonth":200},
-             {"orderType":"Failed" ,"lastOneDay":10,"lastWeak":76,"lastTwoWeak":78,"lastMonth":200}
-    ]
+    autoLoad:true,
+    proxy : {
+	//	timeout:parseInt(requestTimeoutforStore, 10),
+		type : 'ajax',
+		url : applicationContext+'/rest/ordertrend/homelist',
+		reader:{
+	        type:'json'
+	     }
+    }
+    
 });
