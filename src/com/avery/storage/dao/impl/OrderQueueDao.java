7b899eef1a3fb6e31ed16073b9c5f5c53c9b1c05
Amit <Amit@ADLP057_Amit>
2015-12-30 15:34:43 +0530

    Change to implement new GUI design

--- src/com/avery/storage/dao/impl/OrderQueueDao.java
+++ src/com/avery/storage/dao/impl/OrderQueueDao.java
@@ -1,5 +1,7 @@
 package com.avery.storage.dao.impl;
 
+import java.util.Set;
+
 import com.avery.storage.dao.GenericDao;
 import com.avery.storage.entities.OrderQueue;
 
@@ -8,5 +10,7 @@ public interface OrderQueueDao extends GenericDao<OrderQueue, Long>{
 	public void submitOrderToSystem(String data,Long entityId);
 	
 	public void cancelOrder(String data, Long entityId);
+	
+	Set<OrderQueue> getList(int lastDays)throws Exception;
 
 }
