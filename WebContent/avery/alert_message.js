64212a5d60eecb9745fb89266444897a6194eff5
Sakshi <Sakshi@ADLP028_Sakshi>
2015-12-30 16:22:27 +0530

    Item number "NOT FOUND" should not be able to pass to oracle

--- WebContent/avery/alert_message.js
+++ WebContent/avery/alert_message.js
@@ -7,6 +7,8 @@ orderLineMandatoryFieldMissingAlt='One of the mandatory fields is empty. Please 
 orderLineMandatoryValidationFieldMissingAlt='One of the mandatory validation has failed. Please resolve it before proceeding.',
 validateFieldFailedConfirmatonMsg='Some of the validations has failed. Are you sure you want to continue?',
 confirmBulkUpdateSave='Are you Sure you want to make/save these changes?',
-noRecordsToUpdateMessage='There are no record(s) to update'
+noRecordsToUpdateMessage='There are no record(s) to update',
+changeOrderLineStatusAlert='One/some of the lines is/are not allow to proceed, please cancel or fix the error before submitting',
+InvalidComboValueAlert='One or more value are not valid.Please correct the values before proceeding'
 ;
 
