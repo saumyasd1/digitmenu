63a366a899e1f3dec4e6f810b1c94aa33adef1de
Amit <Amit@ADLP057_Amit>
2015-12-30 15:24:43 +0530

    Change to implement new GUI design

--- WebContent/avery/app/store/Sections.js
+++ WebContent/avery/app/store/Sections.js
@@ -1,12 +1,13 @@
 Ext.define('AOC.store.Sections', {
     extend : 'Ext.data.ArrayStore',
+    requires:['AOC.model.Section'],
     model  : 'AOC.model.Section',
     data   : [
               ['home.png','HOME','homewrapper',true,0],
-              ['orders.png','ORDER QUEUE','orderqueueview',false,10],
-              ['partners.png','PARTNERS','partnermanagement',false,3],
-              ['contacts.png','ADDRESS MANAGE','addressmanage',false,5],
+              ['orders.png','ORDER QUEUE','orderqueueview',false,0],
+              ['partners.png','PARTNERS','partnermanagement',false,0],
+              ['contacts.png','ADDRESS MANAGE','addressmanage',false,0],
               ['webform.png','WEB FORM','weborderview',false,0],
-              ['archives.png','ARCHIVE','archivemanage',false,3]
+              ['archives.png','ARCHIVE','archivemanage',false,0]
     ]
 });
