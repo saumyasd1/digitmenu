bf8523eeea5ca1c7669fd26bdea77eb6c1f48312
Sakshi <Sakshi@ADLP028_Sakshi>
2016-01-02 20:53:07 +0530

    Adding method for Reports

--- src/com/avery/storage/dao/impl/OrderQueueDao.java
+++ src/com/avery/storage/dao/impl/OrderQueueDao.java
@@ -1,6 +1,9 @@
 package com.avery.storage.dao.impl;
 
+import java.util.List;
 import java.util.Set;
+
+import javax.ws.rs.core.MultivaluedMap;
 
 import com.avery.storage.dao.GenericDao;
 import com.avery.storage.entities.OrderQueue;
@@ -12,5 +15,8 @@ public interface OrderQueueDao extends GenericDao<OrderQueue, Long>{
 	public void cancelOrder(String data, Long entityId);
 	
 	Set<OrderQueue> getList(int lastDays)throws Exception;
-
+	
+	public List getAllEntitiesListWithCriteria(MultivaluedMap queryMap) throws Exception;
+	
+	public List getAllEntitiesListForDailyReport() throws Exception;
 }
