ad0949d2e2ab96e1c896e6a696b95ca5b0dd8fab
Sakshi <Sakshi@ADLP028_Sakshi>
2016-01-04 13:07:21 +0530

    Binding store to paging toolbar

--- WebContent/avery/app/view/address/AddressManageGrid.js
+++ WebContent/avery/app/view/address/AddressManageGrid.js
@@ -36,6 +36,7 @@ Ext.define('AOC.view.address.AddressManageGrid', {
              				}
              				});
              			 obj.bindStore(store);
+             			 obj.down('pagingtoolbar').bindStore(store);
              		 }
              		 store.load();
              	  }
