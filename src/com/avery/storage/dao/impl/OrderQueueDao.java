e354987cd2d0ec39c3538937fb1b83be8969be10
Amit <Amit@ADLP057_Amit>
2016-01-04 12:26:47 +0530

    mysql-connector-java-5.1.30-bin.jar

--- src/com/avery/storage/dao/impl/OrderQueueDao.java
+++ src/com/avery/storage/dao/impl/OrderQueueDao.java
@@ -1,9 +1,6 @@
 package com.avery.storage.dao.impl;
 
-import java.util.List;
 import java.util.Set;
-
-import javax.ws.rs.core.MultivaluedMap;
 
 import com.avery.storage.dao.GenericDao;
 import com.avery.storage.entities.OrderQueue;
@@ -14,9 +11,6 @@ public interface OrderQueueDao extends GenericDao<OrderQueue, Long>{
 	
 	public void cancelOrder(String data, Long entityId);
 	
-	Set<OrderQueue> getList(int lastDays)throws Exception;
-	
-	public List getAllEntitiesListWithCriteria(MultivaluedMap queryMap) throws Exception;
-	
-	public List getAllEntitiesListForDailyReport() throws Exception;
+	Set<OrderQueue> getList(int lastDays,Set<String> status)throws Exception;
+
 }
