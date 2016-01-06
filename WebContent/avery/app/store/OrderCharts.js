67a9b8d1048052afd0299f92609e8656a1f66b7f
Amit <Amit@ADLP057_Amit>
2016-01-06 18:23:24 +0530

    Change to fix the performance issues

--- WebContent/avery/app/store/OrderCharts.js
+++ WebContent/avery/app/store/OrderCharts.js
@@ -2,9 +2,7 @@ Ext.define('AOC.store.OrderCharts', {
     extend : 'Ext.data.Store',
     requires:['AOC.model.OrderChart'],
     model  : 'AOC.model.OrderChart',
-    autoLoad:true,
     proxy : {
-	//	timeout:parseInt(requestTimeoutforStore, 10),
 		type : 'ajax',
 		url : applicationContext+'/rest/ordertrend/chart/1',
 		reader:{
