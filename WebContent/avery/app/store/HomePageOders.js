c0c15354f4eb7091a54669509d1b2f7a63f427f4
Amit <Amit@ADLP057_Amit>
2016-01-06 18:22:47 +0530

    Change to fix the performance issues

--- WebContent/avery/app/store/HomePageOders.js
+++ WebContent/avery/app/store/HomePageOders.js
@@ -2,9 +2,7 @@ Ext.define('AOC.store.HomePageOders', {
     extend : 'Ext.data.Store',
     requires:['AOC.model.HomePageOder'],
     model  : 'AOC.model.HomePageOder',
-    autoLoad:true,
     proxy : {
-	//	timeout:parseInt(requestTimeoutforStore, 10),
 		type : 'ajax',
 		url : applicationContext+'/rest/ordertrend/homelist',
 		reader:{
