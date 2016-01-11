d6be8835d11144db1aee27445e75f5e7cdd650cb
Amit <Amit@ADLP057_Amit>
2016-01-11 15:57:38 +0530

    implement resubmit functionality

--- WebContent/avery/app/view/webform/WebOrderView.js
+++ WebContent/avery/app/view/webform/WebOrderView.js
@@ -1,33 +1,82 @@
 Ext.define('AOC.view.webform.WebOrderView', {
-	extend : 'Ext.panel.Panel',
-	requires : ['AOC.view.base.BaseToolbar','AOC.view.webform.WebOrderPage'],
+	extend : 'Ext.Container',
+	requires : ['AOC.view.base.BaseToolbar','AOC.view.webform.AttachmentInfoGrid','AOC.view.webform.WebOrderForm'],
 	alias : 'widget.weborderview',
+	controller:'webFormMain',
 	itemId : 'webOrderViewItemId',
 	initComponent : function() {
+	    var me =this;
 		Ext.apply(this, {
-			layout :'fit', 
-			border:'4 4 4 4',
-			 layout: {
-		        type: 'vbox',
-		        align: 'stretch'
-		    },
-			items : [{
-		 				xtype:'panel',
-		 				flex:1,
-		 				layout:'fit',
-		 				items:[{
-							xtype:'weborderpage',
-						    flex:1
-						}]
-					}],
-			dockedItems: [{
-			    xtype: 'basetoolbar',
-			    dock: 'top',
-			    items: [{
-			    	xtype : 'tbtext',
-			    	text : '<div style="color:"><b>New Web Orders</b></div>'
-					}]
-			}]
+		    layout:'vbox',
+ 		      items:[
+ 		             {
+ 	                    xtype   : 'container',
+ 	                    width:'100%',
+	 	           style   : {
+	 	                	"background-color" : "#ffffff",
+	 	                	"padding"          : "10px 20px 10px 20px"
+	 	                    },
+ 	                    height  : 40,
+ 	                    items   : [{
+ 	                        xtype  : 'component',
+ 	                        html   : '<div  style="color: #333f49;font: 300 13px/16px helvetica, arial, verdana, sans-serif;"><b>New Web Orders</b></div>'
+ 	                      }]
+ 	        	      },
+ 	        	      {
+ 	        		 xtype:'container',
+ 	        		 width:'100%',
+ 	        		 flex:1,
+ 	        		 layout:'anchor',
+ 	        		 autoScroll:true,
+ 	        		 items:[
+ 	        	      {
+				     xtype:'weborderform',
+				     anchor:'100%'
+				},{
+				     xtype:'attachmentinfoGrid',
+				     //minHeight:80,
+				     margin:'0 200 10 200',
+				     anchor:'100%'
+    		             }]},
+    		             {
+    	 	                    xtype   : 'container',
+    	 	                    width:'100%',
+    	 	                    cls      : 'container-border',
+    	 	                    style   : {
+    	 	                	"background-color" : "#ffffff",
+    	 	                	"padding"          : "10px 20px 10px 20px"
+    	 	                    },
+    	 	                    height  : 50,
+    	 	                    items   : [{
+    	 	                   xtype   : 'component',
+    	 	                   flex    : 1,
+    	 	                   html   : '<div  >  </div>',
+    	 	                   cls	    : 'profile-info-header-text'
+    	 	                   },{
+    	 	                    xtype   : 'container',
+    	 	                   layout    : {
+    	 	                       type  : 'hbox',
+    	 	                       align : 'middle',
+    	 	                       pack  : 'end'
+    	 	                   },
+    	 	                   items   : [{
+    	 	                       xtype   : 'plainbutton',
+    	 	                       margin  : '0 0 0 10',
+    	 	                       itemId  : 'cancel',
+    	 	                       text    :'Cancel',
+    	 	                       handler : 'CancelDetails',
+    	 	                   },{
+    	 	                       xtype   : 'whitebutton',
+    	 	                       margin  : '0 0 0 10',
+    	 	                       text    :'Save',
+    	 	                       itemId  : 'save',
+    	 	                       handler:'SaveDetails',
+    	 	                   }
+    	 	                   ]
+    	 	                    }]
+    	 	        	      }
+ 	        	      ]
+
 	 	});
 	 	this.callParent(arguments);
 	}
