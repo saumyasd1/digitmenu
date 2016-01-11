488c630af164f4792b94eba12d382c1da7624128
Shaifali <Shaifali@ADLP046>
2016-01-11 15:16:34 +0530

    removed proxy

--- WebContent/avery/app/store/PartnerManagementStore.js
+++ WebContent/avery/app/store/PartnerManagementStore.js
@@ -5,20 +5,6 @@ Ext.define('AOC.store.PartnerManagementStore', {
 	remoteSort: true,
 	totalCount:'total',
 	pageSize:pageSize,
-	storeId:'PartnerManagementStoreId',
-	proxy: {
-        type: 'rest',
-        //url : 'aoc/Partner/GET?action=getList',
-        url         : applicationContext+'/rest/partners',
-        reader      : {
-            type          : 'json',
-            rootProperty          : 'partners',
-            totalProperty : 'totalCount'
-        },
-        headers     : {
-            "Authorization" : "Basic YWRtaW46aW5kaWdvMQ=="
-        }
-        
-    }
+	storeId:'PartnerManagementStoreId'
 });
 
