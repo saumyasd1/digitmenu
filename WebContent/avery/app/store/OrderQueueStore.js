d918c69167390c585729b76ca60e2d986131a609
Amit <Amit@ADLP057_Amit>
2016-01-13 15:23:59 +0530

    code re-factoring

--- WebContent/avery/app/store/OrderQueueStore.js
+++ WebContent/avery/app/store/OrderQueueStore.js
@@ -1,5 +1,14 @@
 Ext.define('AOC.store.OrderQueueStore', {
 	extend : 'Ext.data.Store',
 	model:'AOC.model.OrderQueueModel',
-	pageSize:pageSize
+	pageSize:pageSize,
+		proxy : {
+			type : 'rest',
+			url : applicationContext+'/rest/orders',
+			reader:{
+		        type:'json', 
+		        rootProperty: 'orders',
+		        totalProperty: 'totalCount'
+		    }
+		}
 });
