d21fb8b73d1425a12b903a8870480d38fe5ac8e7
Amit <Amit@ADLP057_Amit>
2016-01-14 15:18:04 +0530

    code re-factoring

--- WebContent/avery/app/view/partner/PartnerManagementGrid.js
+++ WebContent/avery/app/view/partner/PartnerManagementGrid.js
@@ -13,6 +13,11 @@ Ext.define('AOC.view.partner.PartnerManagementGrid', {
 		columnLines:false,
 		store:Ext.create('AOC.store.PartnerManagementStore', {
 				storeId:'PartnerManagementStoreId'}),
+		listeners:{
+			    activate:function(obj){
+			             		 me.down('pagingtoolbar').bindStore(obj.getStore());
+			             	  }
+			 },		
         tbar: { height: 40,
     		    items : me.buildtbar()
               },
