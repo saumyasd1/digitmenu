6d41b25a694b3db4691b8cfc32098890cc51bb48
Amit <Amit@ADLP057_Amit>
2016-01-14 16:20:30 +0530

    code re-factoring

--- WebContent/avery/app/view/archive/ArchiveManage.js
+++ WebContent/avery/app/view/archive/ArchiveManage.js
@@ -1,5 +1,5 @@
 Ext.define('AOC.view.archive.ArchiveManage', {
-	extend : 'Ext.panel.Panel',
+	extend : 'Ext.Container',
 	requires : ['AOC.view.base.BaseToolbar', 'AOC.view.archive.OrderFileQueueArchiveGrid', 'AOC.view.archive.OrderLineArchiveGrid', 
 		'AOC.view.archive.OrderLineDetailArchiveGrid', 'AOC.view.archive.PartnerArchiveGrid', 
 		'AOC.view.archive.ProductLineArchiveGrid','AOC.view.archive.SalesOrderArchiveGrid','AOC.view.archive.SalesOrderDetailArchiveGrid'],
@@ -8,11 +8,82 @@ Ext.define('AOC.view.archive.ArchiveManage', {
 	controller : 'archiveMain',
 	initComponent : function () {
 		Ext.apply(this, {
-			layout : 'fit',
-			border : '4 4 4 4',
+		    layout: {
+		        type: 'vbox',
+		        align: 'stretch'
+		         },
 			items : [{
+				xtype : 'basetoolbar',
+				dock : 'top',
+				title : 'Archive-Manage',
+				height:50,
+				items : [{
+						xtype : 'tbtext',
+						margin:'0 0 0 20',
+						text : '<div style="color:"><b>Archive-Manage</b></div>',
+						itemId : 'archivetitleItemid'
+					},'->', {
+						xtype : 'form',
+						ui : 'darktoolbar',
+						reference : 'cmbformArchive',
+						border : false,
+						buttonAlign : 'center',
+						layout : 'column',
+						style : 'background: #FFFFFF !important;border: 2px solid #FFFFFF;',
+						items : [{
+								xtype : 'combobox',
+								itemId:'choosetable',
+								fieldLabel : 'Choose Table',
+								labelWidth:120,
+								store : Ext.create('Ext.data.Store', {
+									fields : ['displayTableName', 'tableName'],
+									data : [{
+											"displayTableName" : "Order File Queue",
+											"tableName" : "ar_orderfilequeue"
+										}, {
+											"displayTableName" : "Order Line",
+											"tableName" : "ar_orderline"
+										}, {
+											"displayTableName" : "Order Line Detail",
+											"tableName" : "ar_orderlinedetail"
+										}, {
+											"displayTableName" : "Partner",
+											"tableName" : "ar_partner"
+										}, {
+											"displayTableName" : "Product Line",
+											"tableName" : "ar_partner_rboproductline"
+										}, {
+											"displayTableName" : "Sales Order",
+											"tableName" : "ar_salesorder"
+										}, {
+											"displayTableName" : "Sales Order Detail",
+											"tableName" : "ar_salesorderdetail"
+										}
+										//{"displayTableName":"Address", "tableName":"address"}
+									]
+								}),
+								name : 'tableName',
+								queryMode : 'local',
+								reference : 'cmbArchiveTables',
+								displayField : 'displayTableName',
+								valueField : 'tableName',
+								editable : false
+							},{
+								xtype : 'button',
+								text : 'Get Table Data',
+								margin:'0 20 0 20',
+								icon : advSearchIcon,
+								iconAlign : "right",
+								listeners : {
+									click : 'onButtonClick'
+								}
+							}
+						]
+					}
+				]
+			},{
 					xtype : 'container',
-					flex : 1.8,
+					flex : 1,
 					layout : 'card',
 					itemId : 'archivePanel',
 					collapsible : false,
@@ -21,82 +92,10 @@ Ext.define('AOC.view.archive.ArchiveManage', {
 					items : [{
 						   xtype:'displayfield',
 						   itemId:'messageitemId',
+						   width:'100%',
 					       value:'<center><font size=2>No data to display,Please Select Table</font></center>'
 					}
 					      
-					]
-				}
-			],
-			dockedItems : [{
-					xtype : 'basetoolbar',
-					dock : 'top',
-					title : 'Archive-Manage',
-					height:50,
-					items : [{
-							xtype : 'tbtext',
-							text : '<div style="color:"><b>Archive-Manage</b></div>',
-							itemId : 'archivetitleItemid'
-						}, {
-							xtype : 'form',
-							ui : 'darktoolbar',
-							reference : 'cmbformArchive',
-							border : false,
-							buttonAlign : 'center',
-							layout : 'column',
-							style : 'background: #FFFFFF !important;border: 2px solid #FFFFFF;',
-							items : [{
-									xtype : 'combobox',
-									itemId:'choosetable',
-									fieldLabel : 'Choose Table',
-									store : Ext.create('Ext.data.Store', {
-										fields : ['displayTableName', 'tableName'],
-										data : [{
-												"displayTableName" : "Order File Queue",
-												"tableName" : "ar_orderfilequeue"
-											}, {
-												"displayTableName" : "Order Line",
-												"tableName" : "ar_orderline"
-											}, {
-												"displayTableName" : "Order Line Detail",
-												"tableName" : "ar_orderlinedetail"
-											}, {
-												"displayTableName" : "Partner",
-												"tableName" : "ar_partner"
-											}, {
-												"displayTableName" : "Product Line",
-												"tableName" : "ar_partner_rboproductline"
-											}, {
-												"displayTableName" : "Sales Order",
-												"tableName" : "ar_salesorder"
-											}, {
-												"displayTableName" : "Sales Order Detail",
-												"tableName" : "ar_salesorderdetail"
-											}
-											//{"displayTableName":"Address", "tableName":"address"}
-										]
-									}),
-									name : 'tableName',
-									queryMode : 'local',
-									reference : 'cmbArchiveTables',
-									displayField : 'displayTableName',
-									valueField : 'tableName',
-									editable : false
-								}, {
-									xtype : 'tbspacer',
-									width : 20,
-									margin:'5 0 0 500'
-								},
-						        {
-									xtype : 'button',
-									text : 'Get Table Data',
-									icon : advSearchIcon,
-									iconAlign : "right",
-									listeners : {
-										click : 'onButtonClick'
-									}
-								}
-							]
-						}
 					]
 				}
 			]
