9e039deff969333446ad8a2f1295409f691bb7dc
Shaifali <Shaifali@ADLP046>
2016-02-02 10:16:31 +0530

    remove json of unused objects

--- src/com/avery/storage/MixIn/SalesOrderMixIn.java
+++ src/com/avery/storage/MixIn/SalesOrderMixIn.java
@@ -1,6 +1,8 @@
 package com.avery.storage.MixIn;
 
 
+import java.util.Date;
+
 import com.avery.storage.entities.SalesOrder;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -9,5 +11,18 @@ public abstract class SalesOrderMixIn {
 	
 		@JsonIgnore
 		public abstract SalesOrder getSalesOrderForVariableData();
+		
+		@JsonIgnore
+		public abstract String getCreatedBy(); //ignore CreatedBy belongs to main abstract entity class
+		
+		@JsonIgnore
+		public abstract Date getCreatedDate(); //ignore CreatedDate belongs to main abstract entity class
+		
+		@JsonIgnore
+		public abstract Date getLastModifiedDate(); //ignore LastModified Date belongs to main abstract entity class
+		
+		@JsonIgnore
+		public abstract String getLastModifiedBy(); //ignore LastModifiedBy belongs to main abstract entity class
+	
 		
 }
