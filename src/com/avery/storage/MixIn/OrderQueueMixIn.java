3a1e187c25272a304e5bdefb89780a32ae705a8f
Shaifali <Shaifali@ADLP046>
2016-02-02 10:17:01 +0530

    remove json of unused objects

--- src/com/avery/storage/MixIn/OrderQueueMixIn.java
+++ src/com/avery/storage/MixIn/OrderQueueMixIn.java
@@ -2,6 +2,7 @@ package com.avery.storage.MixIn;
 
 
 import java.sql.Blob;
+import java.util.Date;
 import java.util.Set;
 
 import com.avery.storage.entities.OrderLine;
@@ -19,6 +20,29 @@ public abstract class OrderQueueMixIn {
 		@JsonIgnore
 		public abstract String getRboID();
 		
+		@JsonIgnore
+		public abstract String getAddress(); //ignore address from partner
+		
+		@JsonIgnore
+		public abstract String getContactPerson(); //ignore contact person from partner
+		
+		@JsonIgnore
+		public abstract String getPhone(); //ignore phone from partner
+		
+		@JsonIgnore
+		public abstract Boolean isActive(); //ignore active from partner
+		
+		@JsonIgnore
+		public abstract String getCreatedBy(); //ignore CreatedBy from partner belongs to main abstract entity class
+		
+		@JsonIgnore
+		public abstract Date getCreatedDate(); //ignore CreatedDate from partner belongs to main abstract entity class
+		
+		@JsonIgnore
+		public abstract Date getLastModifiedDate(); //ignore LastModified Date from partner belongs to main abstract entity class
+		
+		@JsonIgnore
+		public abstract String getLastModifiedBy(); //ignore LastModifiedBy from partner belongs to main abstract entity class
 		
 		@JsonIgnore
 		public abstract Blob getFileData();
