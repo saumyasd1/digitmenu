70ca4f732c2c838999d5fa6cb9b291df62e0039a
Shaifali <Shaifali@ADLP046>
2016-02-02 09:59:48 +0530

    added new chnages

--- src/com/avery/storage/dao/impl/PartnerDao.java
+++ src/com/avery/storage/dao/impl/PartnerDao.java
@@ -1,14 +1,17 @@
 package com.avery.storage.dao.impl;
 
 import java.util.Map;
+import java.util.Set;
 
 import javax.ws.rs.core.MultivaluedMap;
 
 import com.avery.storage.dao.GenericDao;
 import com.avery.storage.entities.Partner;
+import com.avery.storage.entities.ProductLine;
 
 public interface PartnerDao extends GenericDao<Partner, Long>{
 	
 	public Boolean checkDuplicatePartnerName(String partnerName) throws Exception;
+
 
 }
