84cf67ca0355e40a0c0a45d4520ff5279672509e
Shaifali <Shaifali@ADLP046>
2016-02-02 10:17:31 +0530

    remove json of unused objects

--- src/com/avery/storage/MixIn/OrderLineMixIn.java
+++ src/com/avery/storage/MixIn/OrderLineMixIn.java
@@ -1,6 +1,8 @@
 package com.avery.storage.MixIn;
 
 
+import java.util.Date;
+
 import com.avery.storage.entities.OrderLine;
 import com.avery.storage.entities.OrderQueue;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -13,5 +15,18 @@ public abstract class OrderLineMixIn {
 		
 		@JsonIgnore
 		public abstract OrderLine getOrderLineForVariableData();
+		
+		@JsonIgnore
+		public abstract String getCreatedBy(); //ignore CreatedBy belongs to main abstract entity class
+		
+		@JsonIgnore
+		public abstract Date getCreatedDate(); //ignore CreatedDate belongs to main abstract entity class
+		
+		@JsonIgnore
+		public abstract Date getLastModifiedDate(); //ignore LastModified Date belongs to main abstract entity class
+		
+		@JsonIgnore
+		public abstract String getLastModifiedBy(); //ignore LastModifiedBy belongs to main abstract entity class
+	
 		
 }
