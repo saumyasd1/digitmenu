3f87579350eaba733d0679a913f7fc2af7443176
Sakshi <Sakshi@ADLP028_Sakshi>
2016-02-04 16:13:21 +0530

    [#AOC-102] Optimization of JSON containing the list of entities

--- src/com/avery/storage/MixIn/ProductLineMixIn.java
+++ src/com/avery/storage/MixIn/ProductLineMixIn.java
@@ -1,9 +1,6 @@
 package com.avery.storage.MixIn;
 
-import java.util.Set;
-
 import com.avery.storage.entities.Partner;
-import com.avery.storage.entities.ProductLine;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public abstract class ProductLineMixIn {
