78abe6137b077b59b42c6b69232010b472e90c5e
Sakshi <Sakshi@ADLP028_Sakshi>
2016-02-05 11:40:48 +0530

    [#AOC-102] Optimization of Java Code

--- src/com/avery/storage/dao/impl/SalesOrderDao.java
+++ src/com/avery/storage/dao/impl/SalesOrderDao.java
@@ -3,7 +3,6 @@ package com.avery.storage.dao.impl;
 import java.util.List;
 
 import com.avery.storage.dao.GenericDao;
-import com.avery.storage.entities.OrderLine;
 import com.avery.storage.entities.SalesOrder;
 
 public interface SalesOrderDao extends GenericDao<SalesOrder, Long>{
