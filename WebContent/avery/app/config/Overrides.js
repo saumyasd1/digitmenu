2d1bb2e223a55b4d15c8b6508f896c7112464b26
Sakshi <Sakshi@ADLP028_Sakshi>
2016-02-05 14:06:52 +0530

    [#AOC-103] Optimization of ExtJs Code

--- WebContent/avery/app/config/Overrides.js
+++ WebContent/avery/app/config/Overrides.js
@@ -4,15 +4,18 @@ Ext.define('AOC.config.Overrides',{
     		 Ext.override(Ext.form.field.Base,{
     	            initComponent : function(){
     	                var fl = this.fieldLabel,
-    	                    ab = this.allowBlank;
+    	                    ab = this.allowBlank,
+    	                    itemId=this.itemId;
     	                if (ab === false && fl) {
     	                    this.fieldLabel = '<span style="color:#EB4A00;">*</span> '+fl;
     	                } else if (ab === true && fl) {
     	                    this.fieldLabel = '  '+fl;
     	                }
+    	                if(itemId=='messageFieldItemId'){
+    	                	this.fieldCls='aoc-win-msg';
+    	                }
     	                this.callParent(arguments);
     	            }
     	        });
     }
-    
 });
