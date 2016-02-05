b88c2c24195208d78fd6af24859110dfc0c7191f
Sakshi <Sakshi@ADLP028_Sakshi>
2016-02-05 17:51:57 +0530

    [#AOC-103] Optimization of ExtJs Code

---
+++ WebContent/avery/app/resources/locale/aoc-en.js
@@ -0,0 +1,9 @@
+Ext.define('AOC.lang.lit',{
+    alternateClassName : ['AOCLit'],
+    singleton : true,
+    "partnerNameExistsMsg": "Confirm",
+    "userNameExistsMsg": "A user with the given Email already exists. Please choose another Email.",
+    "noFieldModifiedMsg": "No field has been modified.",
+    "fillMandatoryFieldMsg": 'Please fill valid entry in the field marked as <img src=' + errorIcon + ' width="15" height="15">',
+    "userGridSearchEmptyText": "Serach Email"
+    });
