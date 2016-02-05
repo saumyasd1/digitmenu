727ca6b64b410623a4906cfc0c5a98f30bb47468
Sakshi <Sakshi@ADLP028_Sakshi>
2016-02-05 11:24:12 +0530

    Deleting the file as Person is not needed in our application

--- src/com/avery/storage/dao/impl/PersonDao.java
+++
@@ -1,8 +0,0 @@
-package com.avery.storage.dao.impl;
-
-import com.avery.storage.dao.GenericDao;
-import com.avery.storage.entities.Person;
-
-public interface PersonDao extends GenericDao<Person, Long> {
-
-}
