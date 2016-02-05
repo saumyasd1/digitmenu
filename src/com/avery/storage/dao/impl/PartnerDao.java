95112ed683cef2465842d4c3cd90c06edb6f8b10
Sakshi <Sakshi@ADLP028_Sakshi>
2016-02-05 11:28:35 +0530

    [#AOC-102] Optimization of Java Code

--- src/com/avery/storage/dao/impl/PartnerDao.java
+++ src/com/avery/storage/dao/impl/PartnerDao.java
@@ -1,13 +1,7 @@
 package com.avery.storage.dao.impl;
 
-import java.util.Map;
-import java.util.Set;
-
-import javax.ws.rs.core.MultivaluedMap;
-
 import com.avery.storage.dao.GenericDao;
 import com.avery.storage.entities.Partner;
-import com.avery.storage.entities.ProductLine;
 
 public interface PartnerDao extends GenericDao<Partner, Long>{
 	
