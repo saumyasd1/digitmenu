4446b9fbb65ea81f1cf59f3c8b3dfca917158f07
Krishna <Krishna@ADLP052_Krishna>
2016-12-05 09:45:57 +0530

    Adding combo fields as per AOC-2A

--- WebContent/avery/app/view/home/ReportForm.js
+++ WebContent/avery/app/view/home/ReportForm.js
@@ -20,9 +20,54 @@ Ext.define('AOC.view.home.ReportForm',{
     buildItems : function(){
         var me = this;
         return [{
+			xtype: 'fieldcontainer',
+            layout: 'hbox',
+            margin : '20 0 0 10',
+            items:[{
+            	xtype:'combo',
+				reference:'partnerCombo',
+				itemId:'partnerCombo',
+				store:'PartnerManagementStore',
+				valueField:'id',
+				name:'partnerName',
+				editable:false,
+				allowBlank : false,
+				reference:'partner',
+				margin:'0 20 0 0',
+				flex:1,
+				displayField:'partnerName',
+				emptyText:'Select Partner',
+				listeners:{
+					'change':'onPartnerChange'
+				}
+            },{
+            	xtype:'combo',
+            	name:'rbo',
+            	labelAlign:'top',
+            	margin:'0 20 0 0',
+            	flex:1,
+            	reference:'rboName',
+            	displayField:'rboName',
+            	valueField:'id',
+            	emptyText:'Select RBO'
+                },{
+                	xtype : 'combo',
+					name:'Status',
+					labelSeparator : '',
+					flex:1,
+					margin:'0 20 0 0',
+					reference:'status',
+					labelAlign : 'top',
+					displayField:'value',
+					valueField:'code',
+					queryMode :'local',
+					emptyText:'Select Status',
+                	store: Ext.data.StoreManager.lookup('code') == null ? AOC.util.Helper.getCodeStore('orderfilequeue') : Ext.data.StoreManager.lookup('orderfilequeueid')
+                    }]
+        },{
         	xtype:'displayfield',
         	height:10,
-        	margin:'0 0 0 10',
+        	margin:'10 0 0 10',
         	value:'Please select the type of report you would like to get:'
         },{
 				xtype : 'radiogroup',
