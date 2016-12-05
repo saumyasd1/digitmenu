eb55e3ba9a1c60cad6e04cc3868119ca879b84fb
Krishna <Krishna@ADLP052_Krishna>
2016-12-05 09:47:38 +0530

    Adding onPartnerChange function

--- WebContent/avery/app/view/home/ReportFormController.js
+++ WebContent/avery/app/view/home/ReportFormController.js
@@ -3,8 +3,9 @@ Ext.define('AOC.view.home.ReportFormController', {
     alias: 'controller.reportcontroller',
     runTime: AOC.config.Runtime,
     getReport:function(){
+    	//debugger;
     	var view=this.getView(),url='';
-    	var radioGroupValue=this.lookupReference('radioGroup').getValue().rb,query='',obj;
+    	var radioGroupValue=this.lookupReference('radioGroup').getValue().rb,query='',obj='';
      	 if(radioGroupValue=='dailyReport'){
      		 url=applicationContext + '/rest/orders/download/dailyreport';
      	 }else{
@@ -12,8 +13,17 @@ Ext.define('AOC.view.home.ReportFormController', {
      		obj={datecriteriavalue:'receivedDate'};
      		obj.fromDate=this.lookupReference('fromDate').getSubmitValue();
      		obj.toDate=this.lookupReference('toDate').getSubmitValue();
-     		query=Ext.JSON.encode(obj);
-     	 }
+     	 
+//     		 obj.PartnerName=this.lookupReference('partner').getDisplayValue();
+//         	 obj.RBOName=this.lookupReference('rboName').getDisplayValue();
+//         	 obj.Status=this.lookupReference('status').getDisplayValue();
+     	 } 
+     	
+//     	 obj.PatnerName=this.lookupReference('id').getValue();
+//     	 obj.RBO=this.lookupReference('id').getValue();
+//     	 obj.Status=this.lookupReference('code').getValue();
+     	 query=Ext.JSON.encode(obj);
+       
      	var form = Ext.create('Ext.form.Panel', { 
             standardSubmit: true,   
             url : url
@@ -32,6 +42,52 @@ Ext.define('AOC.view.home.ReportFormController', {
     	}else{
     		this.lookupReference('fromDate').setHidden(false);
     		this.lookupReference('toDate').setHidden(false);
+    	}
+    },
+    
+    
+    onPartnerChange:function(obj,newValue){
+    	var me=this;
+    	if(newValue!=null){
+    	var productLineCombo=this.lookupReference('productLineCombo'),rboCombo=this.lookupReference('rboName');
+    	var store=null;
+        var response = Ext.Ajax.request({
+            async: false,
+            url        : applicationContext+'/rest/productLines/partner/'+newValue+'?partnerId='+newValue
+        });
+      	var jsonValue=Ext.decode(response.responseText).productlines;
+    	var serviceStoreData = [];
+    	if(jsonValue.length>0){
+    	jsonValue.forEach(function(item){
+  		var service = item;
+  		serviceStoreData.push(service);
+  		
+  	});
+    	store =  Ext.create('Ext.data.Store',{
+    		fields:['id'],
+	            data : serviceStoreData
+      });
+    	var uniqueValueArray1=store.collect('rboName');
+    	var serviceStoreData1= [];
+    	  if(uniqueValueArray1.length>0){
+    		 uniqueValueArray1.forEach(function(item){
+    			 var index=store.find('rboName',item);
+    			 var currentRecord=store.getAt(index);
+        	 serviceStoreData1.push(currentRecord);
+           });
+    	     var serviceStore =  Ext.create('Ext.data.Store',{
+     	   	 fields : ['rboName','id','productLineType'],	
+	         data : serviceStoreData1
+        });
+    	     
+    	     var form =me.getView();
+    	     if(form !=null && !form.isResubmit){
+    	    	 rboCombo.reset();
+    	     }
+    	     rboCombo.bindStore(serviceStore);
+    	     rboCombo.enable();
+    	  }
+    	}
     	}
     }
 })
