c2dc3c775cedd7203f31fc3fd7c1f5036bbf0eaa
Krishna <Krishna@ADLP052_Krishna>
2016-12-21 14:48:29 +0530

    adding new order email queue service

---
+++ src/com/avery/storage/service/OrderEmailQueueService.java
@@ -0,0 +1,34 @@
+package com.avery.storage.service;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.avery.storage.dao.impl.OrderEmailQueueDao;
+import com.avery.storage.entities.OrderEmailQueue;
+
+@Component
+public class OrderEmailQueueService extends GenericEntityService<OrderEmailQueue, Long>{
+	
+	private OrderEmailQueueDao orderEmailQueueDao;
+	
+	
+	public OrderEmailQueueDao getOrderEmailQueueDao() {
+		return orderEmailQueueDao;
+	}
+
+
+	@Autowired
+	public void setOrderEmailQueueDao(OrderEmailQueueDao orderEmailQueueDao) {
+		this.orderEmailQueueDao = orderEmailQueueDao;
+	}
+
+
+	@Transactional
+	public Map getWithUnidentifiedStatus(){
+		return getOrderEmailQueueDao().getUnidentifiedEntities();
+	}
+
+}
