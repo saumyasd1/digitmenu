cac2eab5613d2c5c909e117d2f15b81fd40d2d0e
Sakshi <Sakshi@ADLP028_Sakshi>
2016-12-25 11:22:54 +0530

    Modified to show listing of system based on site on Partner screen

--- src/com/avery/storage/entities/SystemInfo.java
+++ src/com/avery/storage/entities/SystemInfo.java
@@ -1,60 +1,89 @@
 package com.avery.storage.entities;
 
-import java.sql.Date;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+
+import com.avery.app.config.SpringConfig;
+import com.avery.storage.MainAbstractEntity;
+import com.avery.storage.MixIn.OrderLineMixIn;
+import com.avery.storage.MixIn.SystemInfoMixIn;
+import com.avery.storage.service.SystemInfoService;
+import com.avery.utils.ApplicationUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 
 @Entity
 @Table(name="system")
-public class SystemInfo {
+@Path("system")
+public class SystemInfo extends MainAbstractEntity{
+
 	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name = "id")
-	long id;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3946547049807497262L;
+
 	@Column(name = "name",length=50,unique=true)
 	String name;
+	
 	@Column(name = "comment",length=250)
 	String comment;
-	@Column(name = "createdBy",length=50)
-	String createdBy;
-	@Column(name = "createdDate")
-	Date createdDate;
-	@Column(name = "lastModifiedBy",length=50)
-	String lastModifiedBy;
-	@Column(name = "lastModifiedDate")
-	Date lastModifiedDate;
+
+	
+	
 	@OneToOne(cascade=CascadeType.ALL,fetch=FetchType.LAZY)
 	OrderSystemInfo varOrderSystemInfo;
 	
+
+	@ManyToOne(cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+	@JoinColumn(name="siteId",nullable=false)
+	Site site;
 	
+	@OneToMany(mappedBy="system",cascade=CascadeType.ALL,fetch=FetchType.LAZY)
+	List<Org> orgList=new ArrayList<Org>();
+	
+	public List<Org> getOrgList() {
+		return orgList;
+	}
+
+	public void setOrgList(List<Org> orgList) {
+		this.orgList = orgList;
+	}
+
+	public Site getSite() {
+		return site;
+	}
+
+	public void setSite(Site site) {
+		this.site = site;
+	}
+
 	public SystemInfo() {}
-
-
-
-
-	public long getId() {
-		return id;
-	}
-
-
-
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-
-
 
 	public String getName() {
 		return name;
@@ -75,47 +104,6 @@ public class SystemInfo {
 		this.comment = comment;
 	}
 
-
-	public String getCreatedBy() {
-		return createdBy;
-	}
-
-
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
-	}
-
-
-	public Date getCreatedDate() {
-		return createdDate;
-	}
-
-
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
-
-
-	public String getLastModifiedBy() {
-		return lastModifiedBy;
-	}
-
-
-	public void setLastModifiedBy(String lastModifiedBy) {
-		this.lastModifiedBy = lastModifiedBy;
-	}
-
-
-	public Date getLastModifiedDate() {
-		return lastModifiedDate;
-	}
-
-
-	public void setLastModifiedDate(Date lastModifiedDate) {
-		this.lastModifiedDate = lastModifiedDate;
-	}
-
-
 	OrderSystemInfo getVarOrderSystemInfo() {
 		return varOrderSystemInfo;
 	}
@@ -125,5 +113,36 @@ public class SystemInfo {
 		this.varOrderSystemInfo = varOrderSystemInfo;
 	}
 	
-	
+	@GET
+	@Path("/site/{id:[0-9]+}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getSystemInfoBySiteId(@Context UriInfo ui,
+			@Context HttpHeaders hh, @PathParam("id") String siteId) {
+		Response.ResponseBuilder rb = null;
+		List<SystemInfo> systems = null;
+		try{
+			Long entityId = Long.parseLong(siteId);
+			StringWriter writer = new StringWriter();
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.addMixIn(SystemInfo.class, SystemInfoMixIn.class);
+			mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+			SystemInfoService systemInfoService = (SystemInfoService) SpringConfig
+					.getInstance().getBean("systemInfoService");
+			systems = systemInfoService.readAllBySiteId(entityId);
+			if (systems == null)
+				throw new Exception("Unable to find Order Line");
+			mapper.setDateFormat(ApplicationUtils.df);
+			mapper.writeValue(writer, systems);
+			rb = Response.ok(writer.toString());
+		} catch (WebApplicationException ex) {
+			throw ex;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new WebApplicationException(Response
+					.status(Status.INTERNAL_SERVER_ERROR)
+					.entity(ExceptionUtils.getRootCauseMessage(e))
+					.type(MediaType.TEXT_PLAIN_TYPE).build());
+		}
+		return rb.build();
+	}
 }
