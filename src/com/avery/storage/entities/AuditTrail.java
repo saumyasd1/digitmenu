c2efca89a77ede51b8789190286337e1eebf622d
Krishna <Krishna@ADLP052_Krishna>
2016-12-26 10:41:25 +0530

    Removing comment from entity

--- src/com/avery/storage/entities/AuditTrail.java
+++ src/com/avery/storage/entities/AuditTrail.java
@@ -10,7 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-//@Entity
+@Entity
 @Table(name = "AuditTrail")
 public class AuditTrail {
 	
