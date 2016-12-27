4cbe25d90b4e7d9446b759b6ca2fb2473189ffc2
Krishna <Krishna@ADLP052_Krishna>
2016-12-27 14:46:35 +0530

    modified code

--- WebContent/avery/app/view/viewmail/EmailAttachmentInfoGrid.js
+++ WebContent/avery/app/view/viewmail/EmailAttachmentInfoGrid.js
@@ -8,12 +8,18 @@ Ext.define('AOC.view.viewmail.EmailAttachmentInfoGrid', {
             columns : this.buildColumns(),
 			columnLines:true,
 			layout:'fit',
-			listeners:{
-				'cellclick':'onAttachmentGridCellClick'
-			}
+//			listeners:{
+//				'cellclick':'onAttachmentGridCellClick'
+//			}
         });
         this.callParent(arguments);
     },
+    plugins: [
+              Ext.create('Ext.grid.plugin.CellEditing', {
+                  clicksToEdit: 1
+              })
+          ],
+          selType: 'checkboxmodel',
     buildColumns : function(){
         return {
        defaults : {
@@ -24,57 +30,66 @@ Ext.define('AOC.view.viewmail.EmailAttachmentInfoGrid', {
                },
                items:[
                       {
-        		            xtype:'actioncolumn',
-        		            width:25,
-        		            baseCls:'custom-action',
-        		  	        items:[
-        		  	      {
-        				  	    	  icon: AOC.config.Settings.buttonIcons.menuIcon,
-        				  	    	  handler: 'onClickMenu'
-        				  	    	
-        		  	      }]
-        		        },{
             text : 'File Name',
-            flex :1,
-            dataIndex:'fileName'
+            flex :1.5,
+            dataIndex:'fileName',
+            name: 'fileName',
+            renderer: function (value) {
+                return '<a href="#">'+value+'</a>';
+            }
         },
         {
             text : 'Partner Data Structure',
             flex :1,
-	        dataIndex:'Partner Data Structure'
-        },
-        {
-            text : 'Download',
-            flex :1,
-	        dataIndex:'Download'
-        },
-        {
-            text : 'Group',
-            flex :1,
-	        dataIndex:'Group'
-        },
-        {   xtype: 'checkcolumn',
+	        dataIndex:'Partner Data Structure',
+	        name: 'Partner Data Structure',
+	        editor: {
+	        	xtype:'combobox'
+	        }
+        }, {
+        	xtype: 'checkcolumn',
             text : 'Additional File',
             flex :1,
 	        dataIndex:'Additional File',
+	        name: 'additionalDataFileKey',
 	        editor: {
                 xtype: 'checkbox',
                 cls: 'x-grid-checkheader-editor'
             }
+        },
+        {
+            text : 'Group',
+            flex :1,
+	        dataIndex:'Group',
+	        name: 'Group'
         },{
             text : 'RBO Match',
             flex :1,
-	        dataIndex:'RBO Match'
+	        dataIndex:'RBO Match',
+	        name: 'rboMatch'
         },
         {
             text : 'ProductLine Match',
             flex :1,
-	        dataIndex:'ProductLine Match'
+	        dataIndex:'ProductLine Match',
+	        name: 'productLineMatch'
         },
         {
             text : 'File Type Match',
+            flex :1.2,
+	        dataIndex:'fileContentType',
+	        name: 'fileContentType'
+        },
+        {
+        	xtype: 'checkcolumn',
+            text : 'Disregard',
             flex :1,
-	        dataIndex:'fileContentType'
+	        dataIndex:'disregard',
+	        name: 'disregard',
+	        editor: {
+                xtype: 'checkbox',
+               // cls: 'x-grid-checkheader-editor'
+            }
         }
         ]};
     }
