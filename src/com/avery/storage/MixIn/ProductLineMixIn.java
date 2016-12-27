35bcc40d6a9009ecb70c0b3845a4d2b5424075d6
Krishna <Krishna@ADLP052_Krishna>
2016-12-27 14:29:23 +0530

    updated productline mixin(Sakshi Dheer)

--- src/com/avery/storage/MixIn/ProductLineMixIn.java
+++ src/com/avery/storage/MixIn/ProductLineMixIn.java
@@ -1,10 +1,28 @@
 package com.avery.storage.MixIn;
 
+import java.util.List;
+
+import com.avery.storage.entities.OrderFileAttachment;
+import com.avery.storage.entities.OrderQueue;
+import com.avery.storage.entities.OrderSystemInfo;
 import com.avery.storage.entities.Partner;
+import com.avery.storage.entities.ProductLine;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public abstract class ProductLineMixIn {
 
 	@JsonIgnore
 	public abstract Partner getPartner();
+	
+	@JsonIgnore
+	public abstract List<ProductLine> getVarProductLine();
+	
+	@JsonIgnore
+	public abstract  List<OrderQueue> getListOrderFileQueue();
+	
+	@JsonIgnore
+	public abstract  List<OrderFileAttachment> getListOrderFileAttachments();
+	
+	@JsonIgnore
+	public abstract  List<OrderSystemInfo> getListOrderSystemInfo();
 }
