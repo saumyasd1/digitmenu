0a280eb522cb3079a94582dcbe1a9ed5f82c375f
Krishna <Krishna@ADLP052_Krishna>
2016-12-29 12:53:15 +0530

    changing mapping of partner data structure

--- WebContent/avery/app/model/ViewMailformModel.js
+++ WebContent/avery/app/model/ViewMailformModel.js
@@ -2,7 +2,7 @@ Ext.define('AOC.model.ViewMailModel',{
     extend: 'Ext.data.Model',
     fields: [
         {name: 'fileName', mapping:'fileName',type: 'string'},
-	    {name: 'Partner Data Structure', mapping:'Partner Data Structure',type: 'string'},
+	    {name: 'Partner Data Structure', mapping:'dataStructureName',type: 'string'},
 	    {name: 'senderEmailId', mapping:'senderEmailId',type: 'string'},
 	    {name: 'Group', mapping:'Group',type: 'string'},
 	    {name: 'additionalDataFileKey', mapping:'additionalDataFileKey',type: 'string'},
