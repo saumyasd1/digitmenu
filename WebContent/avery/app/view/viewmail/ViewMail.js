37b423465f32796f36b4397dffa1fe368fe56fb8
Krishna <Krishna@ADLP052_Krishna>
2016-12-29 14:21:52 +0530

    committing layout changes

--- WebContent/avery/app/view/viewmail/ViewMail.js
+++ WebContent/avery/app/view/viewmail/ViewMail.js
@@ -1,107 +1,75 @@
 Ext.define('AOC.view.viewmail.ViewMail', { 
-	extend : 'Ext.Container',
+	extend : 'Ext.Panel',
 	requires : ['AOC.view.base.BaseToolbar','AOC.view.viewmail.EmailAttachmentInfoGrid','AOC.view.viewmail.ViewMailForm'],
 	alias : 'widget.viewmail',
 	controller:'viewMailController',
 	itemId : 'viewMailItemId',
-	initComponent : function() {
-		Ext.apply(this, {
-		    layout:'vbox',
- 		      items:[
- 		             {
- 	                    xtype   : 'container',
- 	                    width:'100%',
- 	                    style   : {
-	 	                	"background-color" : "#ffffff",
-	 	                	"padding"          : "10px 20px 10px 20px"
-	 	                    },
- 	                    height  : 40,
- 	                    layout:'hbox',
- 	                    items   : [{
- 	                       xtype: 'component',
- 	                      autoEl: {
- 	                      	tag: 'img',
- 	                          src: AOC.config.Settings.buttonIcons.backIcon
- 	                      },
- 	                  	    listeners: {
- 	                  	    	 el : {
- 	                  	    		    click    : 'backButton'
- 	                  	    	 }
- 	                        }
- 	                      },
- 	                       {
- 	                        xtype  : 'component',
- 	                         padding:'5 0 0 10',
- 	                        itemId:'viewmaillabel',
- 	                        html   : '<div  style="color: #333f49;font: 300 13px/16px helvetica, arial, verdana, sans-serif;"><b>View Mail</b></div>'
- 	                      }]
- 	        	      },
- 	        	      {
- 	        		 xtype:'container',
- 	        		 width:'100%',
- 	        		 flex:1,
- 	        		 layout:'anchor',
- 	        		 autoScroll:true,
- 	        		 items:[
- 	        	      {
-				     xtype:'viewmailform',
-				     anchor:'100%',
-				     reference:'webform'
-				},{
-				     xtype:'emailattachmentinfoGrid',
-				     //minHeight:80,
-				     margin:'0 200 10 200',
-				     anchor:'100%'
-    		             }]},
-    		             {
-    	 	                    xtype   : 'container',
-    	 	                    width:'100%',
-    	 	                    cls      : 'container-border',
-    	 	                    style   : {
-    	 	                	"background-color" : "#ffffff",
-    	 	                	"padding"          : "10px 20px 10px 20px"
-    	 	                    },
-    	 	                    height  : 50,
-    	 	                    items   : [{
-    	 	                   xtype   : 'component',
-    	 	                   flex    : 1,
-    	 	                   html   : '<div  >  </div>',
-    	 	                   cls	    : 'profile-info-header-text'
-    	 	                   },{
-    	 	                    xtype   : 'container',
-    	 	                   layout    : {
-    	 	                       type  : 'hbox',
-    	 	                       align : 'middle',
-    	 	                       pack  : 'end'
-    	 	                   },
-    	 	                   items   : [{
-    	 	                       xtype   : 'whitebutton',
-    	 	                       margin  : '0 0 0 10',
-    	 	                       text    : 'Save',
-    	 	                       itemId  : 'saveAttachment',
-    	 	                       handler : 'SaveDetails'
-    	 	                   },{
-    	 	                       xtype   : 'whitebutton',
-    	 	                       margin  : '0 0 0 10',
-    	 	                       itemId  : 'downloadAttachments',
-    	 	                       text    : 'Download Attachment(s)',
-    	 	                       handler : 'CancelDetails'
-    	 	                   },{
-    	 	                       xtype   : 'whitebutton',
-    	 	                       margin  : '0 0 0 10',
-    	 	                       itemId  : 'processButton',
-    	 	                       text    : 'Process Order',
-    	 	                       handler : 'processOrder'
-    	 	                   }
-    	 	                  
-    	 	                   ]
-    	 	                    }]
-    	 	        	      }
- 	        	      ]
-
-	 	});
-	 	this.callParent(arguments);
+	layout:{
+		type:'border'
 	},
+	
+	initComponent:function(){
+		var me = this;
+		me.items = me.buildItems();
+		me.tbar = me.buildTBar();
+		me.bbar = me.buildBBar();
+		me.callParent(arguments);
+	},
+	buildTBar:function(){
+		return [
+			{
+				xtype:'tbtext',
+				text:'View Mail Detail',
+				style:'color: #333f49;font-weight:bold;font-size:15px;'
+			}
+		]
+	},
+	buildBBar:function(){
+		return [
+			{
+				xtype:'whitebutton',
+				text:'Back',
+				handler:'backButton'
+			},
+			'->',
+			{
+			   xtype   : 'whitebutton',
+			   margin  : '0 10 0 10',
+			   text    : 'Save',
+			   itemId  : 'saveAttachment',
+			   handler : 'SaveDetails'
+			},
+			{
+			   xtype   : 'whitebutton',
+			   margin  : '0 10 0 0',
+			   itemId  : 'downloadAttachments',
+			   text    : 'Download Attachment(s)'
+			   //handler : 'CancelDetails'
+			},
+			{
+			   xtype   : 'whitebutton',
+			   itemId  : 'cancel',
+			   text    : 'Process Order'
+			   //handler : 'CancelDetails'
+			}
+		]
+	},
+	buildItems:function(){
+		return [
+			 {
+				 xtype:'viewmailform',
+				 reference:'webform',
+				 region:'north',
+				 height:180
+			},
+			{
+				 xtype:'emailattachmentinfoGrid',
+				 scrollable:true,
+				 region:'center'
+			 }
+		]
+	},
+	
 	updateHeaderLabel:function(label){
 	    var cmp = this.down('#weborderlabel');
 	    cmp.update('<div  style="color: #333f49;font: 300 13px/16px helvetica, arial, verdana, sans-serif;"><b>'+label+'</b></div>')
