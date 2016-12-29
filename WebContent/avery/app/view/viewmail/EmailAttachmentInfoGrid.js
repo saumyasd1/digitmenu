13cea9cc26eeb1ecb81d40fe715a03dd6c13ebfd
Krishna <Krishna@ADLP052_Krishna>
2016-12-29 14:07:55 +0530

    committing layout changes

--- WebContent/avery/app/view/viewmail/EmailAttachmentInfoGrid.js
+++ WebContent/avery/app/view/viewmail/EmailAttachmentInfoGrid.js
@@ -3,95 +3,98 @@ Ext.define('AOC.view.viewmail.EmailAttachmentInfoGrid', {
     alias : 'widget.emailattachmentinfoGrid',
     itemId:'EmailAttachmentInfoGriditemId',
     controller:'viewMailController',
+    reserveScrollbar:true,
     initComponent : function(){
         Ext.apply(this,{
             columns : this.buildColumns(),
 			columnLines:true,
-			layout:'fit',
-			 plugins: [{
-			        ptype: 'cellediting',
-			        clicksToEdit: 1
-			    }]
+			//listeners:{
+			//	'cellclick':'onAttachmentGridCellClick'
+			//}
         });
         this.callParent(arguments);
     },
-          selType: 'checkboxmodel',
+    plugins: [
+		{
+			        ptype: 'cellediting',
+			        clicksToEdit: 1
+			    }
+	],
+	selType: 'checkboxmodel',
     buildColumns : function(){
         return {
-       defaults : {
+			defaults : {
                 draggable : false,
                 sortable : false,
                 hideable :false,
                 resizable:false
-               },
-               items:[
-                      {
-            text : 'File Name',
-            flex :1.5,
-            dataIndex:'fileName',
-            name: 'fileName',
-            renderer: function (value) {
-                return '<a href="#">'+value+'</a>';
-            }
-        },
-        {
-            text : 'Partner Data Structure',
-            flex :1.7,
-	        dataIndex:'Partner Data Structure',
-	        name: 'Partner Data Structure',
-	        editor: {
-	        	xtype:'combobox',
-	        	editable:false,
-	        	displayField:'dataStructureName',
-				valueField:'dataStructureName',
-				queryMode :'local',
-	        	store:Ext.data.StoreManager.lookup('PartnerProductLineStoreStoreId') == null ? Ext.create('AOC.store.PartnerProductLineStore') : Ext.data.StoreManager.lookup('orderlineid')
-	        }
-        }, {
-        	xtype: 'checkcolumn',
-            text : 'Additional File',
-            flex :1,
-	        dataIndex:'Additional File',
-	        name: 'additionalDataFileKey',
-	        editor: {
-                xtype: 'checkbox',
-                cls: 'x-grid-checkheader-editor'
-            }
-        },
-        {
-            text : 'Group',
-            flex :1,
-	        dataIndex:'Group',
-	        name: 'Group'
-        },{
-            text : 'RBO Match',
-            flex :1,
-	        dataIndex:'RBO Match',
-	        name: 'rboMatch'
-        },
-        {
-            text : 'ProductLine Match',
-            flex :1,
-	        dataIndex:'ProductLine Match',
-	        name: 'productLineMatch'
-        },
-        {
-            text : 'File Type Match',
-            flex :1.2,
-	        dataIndex:'fileContentType',
-	        name: 'fileContentType'
-        },
-        {
-        	xtype: 'checkcolumn',
-            text : 'Disregard',
-            flex :1,
-	        dataIndex:'disregard',
-	        name: 'disregard',
-	        editor: {
-                xtype: 'checkbox',
-               // cls: 'x-grid-checkheader-editor'
-            }
-        }
-        ]};
+			},
+		   items:[
+				{
+					text : 'File Name',
+					flex :1.5,
+					dataIndex:'fileName',
+					name: 'fileName',
+					renderer: function (value) {
+						return '<a href="#">'+value+'</a>';
+					}
+				},
+				{
+					text : 'Partner Data Structure',
+					flex :1,
+					dataIndex:'Partner Data Structure',
+					name: 'Partner Data Structure',
+					editor: {
+						xtype:'combobox'
+					}
+				}, 
+				{
+					xtype: 'checkcolumn',
+					text : 'Additional File',
+					flex :1,
+					dataIndex:'Additional File',
+					name: 'additionalDataFileKey',
+					editor: {
+						xtype: 'checkbox',
+						cls: 'x-grid-checkheader-editor'
+					}
+				},
+				{
+					text : 'Group',
+					flex :1,
+					dataIndex:'Group',
+					name: 'Group'
+				},
+				{
+					text : 'RBO Match',
+					flex :1,
+					dataIndex:'RBO Match',
+					name: 'rboMatch'
+				},
+				{
+					text : 'ProductLine Match',
+					flex :1,
+					dataIndex:'ProductLine Match',
+					name: 'productLineMatch'
+				},
+				{
+					text : 'File Type Match',
+					flex :1.2,
+					dataIndex:'fileContentType',
+					name: 'fileContentType'
+				},
+				{
+					xtype: 'checkcolumn',
+					text : 'Disregard',
+					flex :1,
+					dataIndex:'disregard',
+					name: 'disregard',
+					editor: {
+						xtype: 'checkbox',
+					   // cls: 'x-grid-checkheader-editor'
+					}
+				}
+			]
+		};
     }
 });
