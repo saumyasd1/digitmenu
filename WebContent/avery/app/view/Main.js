809b4344da14042aa2aa9adc0da33545964bc756
Krishna <Krishna@ADLP052_Krishna>
2016-12-29 14:12:08 +0530

    committing layout changes

--- WebContent/avery/app/view/Main.js
+++ WebContent/avery/app/view/Main.js
@@ -1,57 +1,58 @@
 Ext.define('AOC.view.Main',{
-	extend:'Ext.Container',
+	extend:'Ext.panel.Panel',
 	alias:'widget.maincontainer',
-	requires : ['AOC.view.home.Wrapper',
-	            'AOC.view.ToolbarView',
-	            'AOC.view.orderqueue.OrderQueueView',
-	            'AOC.view.partner.PartnerManagement',
-	            'AOC.view.email.EmailManagement',
-	            'AOC.view.address.AddressManage',
-	            'AOC.view.webform.WebOrderView',
-	            'AOC.view.archive.ArchiveManage',
-	            'AOC.view.users.myprofile.Wrapper',
-	            'AOC.view.users.manage.User'
-	            ],
+	requires : [
+        'AOC.view.home.Wrapper',
+        'AOC.view.ToolbarView',
+        'AOC.view.orderqueue.OrderQueueView',
+        'AOC.view.partner.PartnerManagement',
+        'AOC.view.email.EmailManagement',
+        'AOC.view.address.AddressManage',
+        'AOC.view.webform.WebOrderView',
+        'AOC.view.archive.ArchiveManage',
+        'AOC.view.users.myprofile.Wrapper',
+        'AOC.view.users.manage.User'
+    ],
+    layout: {
+    	type: 'card',
+    	deferredRender : true
+	},
+	activeItem : 0,
+	
 	initComponent : function(){
-    	var me=this;
-    	this.fieldArray = [],
-            Ext.apply(this,{
-                items:this.buildItem(),
-                layout: {
-                	type: 'card',
-                	deferredRender : true
-                	},
-                	activeItem : 0
-            });
-            this.callParent(arguments);
-        },
-        buildItem:function(){
-        	var me=this;
-        	return [   
-        	           {
-        	              xtype : 'homewrapper'
-        	           },
-        	           {
-        				   xtype:'emailmanagement'
-        			   },
-        			   {
-		    	     xtype : 'orderqueueview' 
-		           },
-		           {
-			       xtype : 'partnermanagement'
-			   },
-			   {
-			       xtype : 'addressmanage'
-			   },{
-			       xtype : 'weborderview'
-			   },{
-			       xtype : 'archivemanage'
-			   },
-			   {
-			       xtype:'profileinfowrapper'
-			   },
-			   {
-				   xtype:'users'
-			   }]
-                 }
-		});
+		this.items = this.buildItems();
+    	this.callParent(arguments);
+    },
+    
+    buildItems:function(){
+    	return [   
+		   {
+			   xtype : 'homewrapper'
+		   },
+		   {
+			   xtype:'emailmanagement'
+		   },
+		   {
+			   xtype : 'orderqueueview' 
+		   },
+		   {
+			   xtype : 'partnermanagement'
+		   },
+		   {
+		       xtype : 'addressmanage'
+		   },
+		   {
+		       xtype : 'weborderview'
+		   },
+		   {
+		       xtype : 'archivemanage'
+		   },
+		   {
+		       xtype:'profileinfowrapper'
+		   },
+		   {
+			   xtype:'users'
+		   }
+	   ]
+    }
+});
