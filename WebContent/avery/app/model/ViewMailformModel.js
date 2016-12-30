9cf1192946aa6a47fc66b34153ec9e6e0af49b0e
Krishna <Krishna@ADLP052_Krishna>
2016-12-30 21:30:12 +0530

    few changes in columns of EmailAttachmentGrid

--- WebContent/avery/app/model/ViewMailformModel.js
+++ WebContent/avery/app/model/ViewMailformModel.js
@@ -1,14 +1,16 @@
 Ext.define('AOC.model.ViewMailModel',{
     extend: 'Ext.data.Model',
     fields: [
-        {name: 'fileName', mapping:'fileName',type: 'string'},
-	    {name: 'Partner Data Structure', mapping:'dataStructureName',type: 'string'},
-	    {name: 'senderEmailId', mapping:'senderEmailId',type: 'string'},
-	    {name: 'Group', mapping:'Group',type: 'string'},
-	    {name: 'additionalDataFileKey', mapping:'additionalDataFileKey',type: 'string'},
-	    {name: 'rboMatch', mapping:'rboMatch',type: 'string'},
-	    {name: 'productLineMatch', mapping:'productLineMatch',type: 'string'},
-	    {name: 'fileContentType', mapping:'fileContentType',type: 'string'}
-	    ]
+        {name: 'fileName', mapping:'fileName', type: 'string'},
+	    {name: 'Partner Data Structure', mapping:'dataStructureName', type: 'string'},
+	    {name: 'senderEmailId', mapping:'senderEmailId', type: 'string'},
+	    {name: 'additionalDataFileKey', mapping:'additionalDataFileKey', type: 'string'},
+	    {name: 'rboMatch', mapping:'rboMatch', type: 'string'},
+	    {name: 'productLineMatch', mapping:'productLineMatch', type: 'string'},
+	    {name: 'fileContentType', mapping:'fileContentType', type: 'string'},
+	    {name: 'fileContentMatch', mapping:'fileContentMatch', type: 'string'},
+	    {name:'id', type:'int'},
+	    {name:'status', type:'int'}
+    ]
 });
 
