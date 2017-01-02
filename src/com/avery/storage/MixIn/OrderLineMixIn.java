e7dc22e0501b6faacba3f0758e64f37b69ed6c75
Krishna <Krishna@ADLP052_Krishna>
2017-01-02 18:07:14 +0530

    adding few columns (vishal sharma)

--- src/com/avery/storage/MixIn/OrderLineMixIn.java
+++ src/com/avery/storage/MixIn/OrderLineMixIn.java
@@ -2,9 +2,12 @@ package com.avery.storage.MixIn;
 
 
 import java.util.Date;
+import java.util.List;
 
 import com.avery.storage.entities.OrderLine;
+import com.avery.storage.entities.OrderLineDetail;
 import com.avery.storage.entities.OrderQueue;
+import com.avery.storage.entities.SalesOrder;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
@@ -29,4 +32,16 @@ public abstract class OrderLineMixIn {
 		public abstract String getLastModifiedBy(); //ignore LastModifiedBy belongs to main abstract entity class
 	
 		
+		@JsonIgnore
+		public abstract OrderQueue getVarOrderFileQueue();
+		
+		/*@JsonIgnore
+		public abstract List<OrderLineDetail> getListOrderlineDetails();
+		*/
+		@JsonIgnore
+		public abstract List<SalesOrder> getListSalesOrderLine();
+		
+		@JsonIgnore
+		public abstract OrderLine getVarOrderLine();
+
 }
