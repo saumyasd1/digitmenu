7df08fac9482c1dace8784f72dd3b8186a72bc8a
Krishna <Krishna@ADLP052_Krishna>
2017-01-02 12:08:26 +0530

    resolving save attachment functionality

--- WebContent/avery/app/model/ViewMailformModel.js
+++ WebContent/avery/app/model/ViewMailformModel.js
@@ -2,7 +2,7 @@ Ext.define('AOC.model.ViewMailModel',{
     extend: 'Ext.data.Model',
     fields: [
         {name: 'fileName', mapping:'fileName', type: 'string'},
-	    {name: 'Partner Data Structure', mapping:'dataStructureName', type: 'string'},
+	    {name: 'dataStructureName', mapping:'varProductLine.id', type: 'string'},
 	    {name: 'senderEmailId', mapping:'senderEmailId', type: 'string'},
 	    {name: 'additionalDataFileKey', mapping:'additionalDataFileKey', type: 'string'},
 	    {name: 'rboMatch', mapping:'rboMatch', type: 'string'},
