bacdcc2a0abfe5cd00aa12f59b6340ebe8f9f7f5
Saumya <Saumya@ADLP039_Saumya>
2017-01-03 12:54:48 +0530

    adding renderer of Status for Order Queue

--- WebContent/avery/app/view/orderqueue/OrderQueueGrid.js
+++ WebContent/avery/app/view/orderqueue/OrderQueueGrid.js
@@ -160,14 +160,14 @@ Ext.define('AOC.view.orderqueue.OrderQueueGrid', {
             text : AOCLit.orderStatus,
             width:200,
 			dataIndex:'status',
-//			editor:{
-//				xtype:'combo',
-//				queryMode :'local',
-//				store: Ext.data.StoreManager.lookup('orderfilequeueid') == null ? AOC.util.Helper.getCodeStore('orderfilequeue') : Ext.data.StoreManager.lookup('orderfilequeueid')
-//			},
-//			renderer:function(v){
-//				return AOC.util.Helper.getSatus(v);
-//			}
+			editor:{
+				xtype:'combo',
+				queryMode :'local',
+				store: Ext.data.StoreManager.lookup('orderfilequeueid') == null ? AOC.util.Helper.getCodeStore('OrderQueue') : Ext.data.StoreManager.lookup('orderfilequeueid')
+			},
+			renderer:function(v){
+				return AOC.util.Helper.getSatus(v);
+			}
         },
 		{
             text : AOCLit.processedDate,
