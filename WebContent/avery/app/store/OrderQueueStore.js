e793bf0b4944737385f9b54a36d66ca29588e480
Amit <Amit@ADLP083_Amit>
2017-01-03 17:06:36 +0530

    add View order functionality in Email Queue

--- WebContent/avery/app/store/OrderQueueStore.js
+++ WebContent/avery/app/store/OrderQueueStore.js
@@ -2,13 +2,14 @@ Ext.define('AOC.store.OrderQueueStore', {
 	extend : 'Ext.data.Store',
 	model:'AOC.model.OrderQueueModel',
 	pageSize:pageSize,
-		proxy : {
-			type : 'rest',
-			url : applicationContext+'/rest/orders',
-			reader:{
-		        type:'json', 
-		        rootProperty: 'orders',
-		        totalProperty: 'totalCount'
-		    }
-		}
+	autoLoad:true,
+	proxy : {
+		type : 'rest',
+		url : applicationContext+'/rest/orders',
+		reader:{
+	        type:'json', 
+	        rootProperty: 'orders',
+	        totalProperty: 'totalCount'
+	    }
+	}
 });
