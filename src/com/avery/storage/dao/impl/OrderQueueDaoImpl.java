87f480888af738e936883d3400438fcf2dca74b6
Saumya <Saumya@ADLP039_Saumya>
2017-01-04 18:44:45 +0530

    adding functionality for population of comment and error data

--- src/com/avery/storage/dao/impl/OrderQueueDaoImpl.java
+++ src/com/avery/storage/dao/impl/OrderQueueDaoImpl.java
@@ -101,6 +101,8 @@ public class OrderQueueDaoImpl extends GenericDaoImpl<OrderQueue, Long> implemen
 			.add(Projections.property("partner.partnerName"),"partnerName")
 			.add(Projections.property("id"),"id")
 			.add(Projections.property("subject"),"subject")
+			.add(Projections.property("comment"),"comment")
+			.add(Projections.property("error"),"error")
 			.add(Projections.property("poNumber"),"poNumber")
 			.add(Projections.property("prevOrderQueueId"),"prevOrderQueueId")
 			.add(Projections.property("status"),"status")
