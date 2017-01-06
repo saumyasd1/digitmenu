0218514e66a222dbd031f488160d23873b547a49
Krishna <Krishna@ADLP052_Krishna>
2017-01-06 14:43:40 +0530

    adding new columns

--- WebContent/avery/app/model/TaskManagerModel.js
+++ WebContent/avery/app/model/TaskManagerModel.js
@@ -2,12 +2,13 @@ Ext.define('AOC.model.TaskManagerModel',{
     extend: 'Ext.data.Model',
 	idProperty:null,
     fields: [
-        // set up the fields mapping into the xml doc
         {name: 'createdDate', mapping:'createdDate',type: 'string'},
 	    {name: 'senderEmailId', mapping:'senderEmailId',type: 'string'},
 		{name: 'subject', mapping:'subject',type: 'string'},
 		{name: 'ccMailId', mapping:'ccMailId',type: 'string'},
-		{name: 'assignCSR', mapping:'assignCSR',type: 'string'}
+		{name: 'assignCSR', mapping:'assignCSR',type: 'string'},
+		{name: 'lastModifiedBy', mapping:'lastModifiedBy',type: 'string'},
+		{name:'lastModifiedDate',mapping:'lastModifiedDate',type:'string'}
 		]
 });
 
