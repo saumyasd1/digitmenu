0cbcfb5daca29f43bb5cb90ad029deb2b77e972a
Saumya <Saumya@ADLP039_Saumya>
2017-01-07 11:22:01 +0530

    removing some fields from partner mixin

--- src/com/avery/storage/MixIn/PartnerMixIn.java
+++ src/com/avery/storage/MixIn/PartnerMixIn.java
@@ -4,10 +4,12 @@ package com.avery.storage.MixIn;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
+
 import com.avery.storage.entities.Address;
 import com.avery.storage.entities.OrderQueue;
 import com.avery.storage.entities.ProductLine;
 import com.avery.storage.entities.SalesOrder;
+import com.avery.storage.entities.Site;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public abstract class PartnerMixIn {
@@ -27,16 +29,18 @@ public abstract class PartnerMixIn {
 	@JsonIgnore
 	public abstract Date getCreatedDate(); //ignore CreatedDate from partner belongs to main abstract entity class
 	
-	@JsonIgnore
-	public abstract Date getLastModifiedDate(); //ignore LastModified Date from partner belongs to main abstract entity class
-	
-	@JsonIgnore
-	public abstract String getLastModifiedBy();
-	
+//	@JsonIgnore
+//	public abstract Date getLastModifiedDate(); //ignore LastModified Date from partner belongs to main abstract entity class
+//	
+//	@JsonIgnore
+//	public abstract String getLastModifiedBy();
+//	
 	@JsonIgnore
 	public abstract Boolean isActive() ;
 	
 	@JsonIgnore
 	public abstract List<SalesOrder> getListSalesOrderLine();	
 	
+	@JsonIgnore
+	public abstract Site getSite();
 }
