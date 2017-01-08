080966fe319d9687e0ca46ebc340d3d068f517b3
Saumya <Saumya@ADLP039_Saumya>
2017-01-08 15:49:24 +0530

    adding method for getting entities of org

--- src/com/avery/storage/dao/impl/OrgDao.java
+++ src/com/avery/storage/dao/impl/OrgDao.java
@@ -1,6 +1,7 @@
 package com.avery.storage.dao.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import com.avery.storage.dao.GenericDao;
 import com.avery.storage.entities.Org;
@@ -8,4 +9,5 @@ import com.avery.storage.entities.Org;
 public interface OrgDao extends GenericDao<Org, Long>{
 	
 	public List<Org> readAllBySystemId(Long systemId);
+	public Map getAllEntitiesList();
 }
