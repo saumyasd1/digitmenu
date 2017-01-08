941b1d022be0bb3880535d1cb840a9c9c319e3e9
Saumya <Saumya@ADLP039_Saumya>
2017-01-08 16:15:19 +0530

    removing storeId from site store

--- WebContent/avery/app/store/SiteStore.js
+++ WebContent/avery/app/store/SiteStore.js
@@ -1,16 +1,15 @@
 Ext.define('AOC.store.SiteStore', {
 	extend : 'Ext.data.Store',
 	fields:['id','name'],
-	storeId:'siteId',
+	
+	autoLoad:true,
 	proxy: {
         type: 'rest',
-        //url : 'aoc/Partner/GET?action=getList',
-        url         : applicationContext+'/rest/site',
-        reader      : {
-            type          : 'json',
-            rootProperty          : 'ArrayList'
+        url : applicationContext+'/rest/site',
+        reader: {
+            type: 'json',
+            rootProperty : 'site'
         }
-        
     }
 });
 
