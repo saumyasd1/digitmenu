6ee22d7299fa8d3b6966897fbe1f3719213576ff
Saumya <Saumya@ADLP039_Saumya>
2017-01-08 12:22:16 +0530

    adding method for systemId and OrgCodeId restrictions

--- src/com/avery/storage/dao/impl/OrderConfigurationDao.java
+++ src/com/avery/storage/dao/impl/OrderConfigurationDao.java
@@ -1,6 +1,7 @@
 package com.avery.storage.dao.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import com.avery.storage.dao.GenericDao;
 import com.avery.storage.entities.OrderConfiguration;
@@ -8,5 +9,6 @@ import com.avery.storage.entities.OrderConfiguration;
 public interface OrderConfigurationDao extends GenericDao<OrderConfiguration, Long> {
 	
 	public List<OrderConfiguration> readByPropertyName(String propertyName) throws Exception;
+	public List<OrderConfiguration> readByPropertyName(String propertyName, Long systemId, Long orgCodeId) throws Exception;
 
 }
