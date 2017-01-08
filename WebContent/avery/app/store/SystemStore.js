34bbccae0dc36b9a027120954da5a9c5152bdcd0
Saumya <Saumya@ADLP039_Saumya>
2017-01-08 16:23:42 +0530

    adding org and system store

---
+++ WebContent/avery/app/store/SystemStore.js
@@ -0,0 +1,26 @@
+Ext.define('AOC.store.SystemStore', {
+	extend : 'Ext.data.Store',
+	fields:['id','name','siteId'],
+//	model:'AOC.model.PartnerManagementModel',
+	//autoLoad : true,
+	autoLoad:true,
+	remoteSort: true,
+	totalCount:'total',
+	pageSize:pageSize,
+	//storeId:'SystemId',
+	proxy: {
+        type: 'rest',
+        //url : 'aoc/Partner/GET?action=getList',
+        url         : applicationContext+'/rest/system',
+        reader      : {
+            type          : 'json',
+            rootProperty          : 'system',
+            totalProperty : 'totalCount'
+        },
+        headers     : {
+            "Authorization" : "Basic YWRtaW46aW5kaWdvMQ=="
+        }
+        
+    }
+});
+
