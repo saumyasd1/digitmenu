0c5eeef5b07417d47ece11d67dac88b1933735a4
Saumya <Saumya@ADLP039_Saumya>
2017-01-08 15:55:43 +0530

    create method for getting entities of system

--- src/com/avery/storage/dao/impl/SystemInfoDao.java
+++ src/com/avery/storage/dao/impl/SystemInfoDao.java
@@ -1,6 +1,9 @@
 package com.avery.storage.dao.impl;
 
 import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.core.MultivaluedMap;
 
 import com.avery.storage.dao.GenericDao;
 import com.avery.storage.entities.SystemInfo;
@@ -8,4 +11,5 @@ import com.avery.storage.entities.SystemInfo;
 public interface SystemInfoDao extends GenericDao<SystemInfo, Long>{
 	
 	public List<SystemInfo> readAllBySiteId(Long siteId);
+	public Map getAllEntitiesList();
 }
