4477e126538997ac7fae6c901b417314a786ab62
Krishna <Krishna@ADLP052_Krishna>
2017-01-10 11:22:48 +0530

    adding alert message

--- WebContent/avery/app/view/taskmanager/AssignCSRController.js
+++ WebContent/avery/app/view/taskmanager/AssignCSRController.js
@@ -4,14 +4,23 @@ Ext.define('AOC.view.taskmanager.AssignCSRController', {
     runTime: AOC.config.Runtime,
     
     onSaveBtnClicked:function(btn){
+    	
     	var view = this.getView(),
     		ref = this.getReferences(),
     		csrCombo = ref.csrCombo,
+    		obj,
+    		comboValue = csrCombo.getValue();
+    		if(comboValue){
     		obj = {
     			name:csrCombo.getRawValue(),
     			assignCSRId:csrCombo.getValue(),
     			recordId:view.recordId
     		};
+    		}
+    		else{
+    			Ext.Msg.alert(AOCLit.warningTitle,'Please select CSR');
+    			return ;
+    		}
     	
     	if(Ext.isFunction(view.callback)){
     		view.callback.call(this, obj);
