aa12bb7d898b771598d94159f627bad88c0b9b7b
Amit <Amit@ADLP083_Amit>
2017-01-14 11:38:27 +0530

    add additional file attachment window

---
+++ WebContent/avery/app/view/orderqueue/OrderQueueAttachmentWindow.js
@@ -0,0 +1,84 @@
+Ext.define('AOC.view.orderqueue.OrderQueueAttachmentWindow', {
+	extend : 'AOC.view.base.NewBaseWindow',
+	alias : 'widget.orderqueueattachmentwin',
+	itemId : 'orderQueueAttachmentWin',
+	reference:'orderQueueAttachmentWin',
+	controller : 'orderqueue',
+	
+	layout:'fit',
+	width: 400,
+	height:400,
+	title:AOCLit.advancedSearchWindowTitle,
+	initComponent:function(){
+		var me = this;
+		me.items = me.buildItems();
+		
+		me.callParent(arguments);
+	},
+	afterRender:function(){
+		var me = this;
+		me.callParent(arguments);
+		this.mask(AOCLit.pleaseWaitTitle);
+		var	grid = me.lookupReference('attachmentGrid'),
+			store = Ext.create('AOC.store.ViewMailformStore',{
+				storeId: 'ViewMailformStoreId',
+				proxy : {
+					type : 'rest',
+					url : applicationContext+'/rest/orderattachements/order/'+ me.recordId,
+					reader:{
+						type:'json', 
+						rootProperty: 'viewmail',
+						totalProperty: 'totalCount'
+					}
+				}
+			});
+		
+		grid.bindStore(store);
+		this.unmask();
+	},
+	buildItems :function(){
+		var me = this;
+		return [
+		    {
+		    	xtype:'grid',
+		    	reference:'attachmentGrid',
+		    	reserveScrollbar:true,
+		        columnLines:true,
+		        border:true,
+		        emptyText: AOCLit.noContentTypeDispMsg,
+		        selType: 'checkboxmodel',
+		        reserveScrollbar:true,
+		        listeners:{
+		        	cellclick:'onAttachmentGridCellClick'
+		        },
+		        bbar:me.buildBbar(),
+		    	columns:[
+					{
+						text : 'File Name',
+						flex :1.5,
+						dataIndex:'fileName',
+						name: 'fileName',
+						resizable:false,
+						sortable:false,
+						resizable:false,
+						renderer: function (value) {
+							return '<a class="emailAttachmentLink" href="#">'+ value +'</a>';
+						}
+					}
+		    	]
+		    }
+		]
+	},
+	buildBbar :function(){
+		return [ 
+		     '->',
+			{
+				text : 'Download Attachments',
+				ui:'blue',
+				listeners : {
+					click  : 'onDownloadAttachmentBtnClicked'
+				}
+			}
+		]	
+	}
+});
