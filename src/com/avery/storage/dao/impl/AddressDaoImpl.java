4cf285727b5ad4536d86108c1653080d9bec880a
Vishal <Vishal@ADLP028_Sakshi>
2017-01-15 15:13:12 +0530

    adding criteria for getting partner and org name - changes by Saumya

--- src/com/avery/storage/dao/impl/AddressDaoImpl.java
+++ src/com/avery/storage/dao/impl/AddressDaoImpl.java
@@ -44,17 +44,38 @@ public class AddressDaoImpl extends GenericDaoImpl<Address, Long> implements
         Criteria criteria_system = null;
         Criteria criteria_site = null;
 		int totalCount=0;
+		ProjectionList proj = Projections.projectionList();
+		proj.add(Projections.property("varOrgCode.name"), "orgCodeName")
+		.add(Projections.property("varPartner.partnerName"), "partnerName")
+		.add(Projections.property("varPartner.id"), "partnerId")
+		.add(Projections.property("address1"),"address1")
+		.add(Projections.property("address2"),"address2")
+		.add(Projections.property("address3"),"address3")
+		.add(Projections.property("address4"),"address4")
+		.add(Projections.property("siteNumber"),"siteNumber")
+		.add(Projections.property("contact"),"contact")
+		.add(Projections.property("phone1"),"phone1")
+		.add(Projections.property("fax"),"fax")
+		.add(Projections.property("email"),"email")
+		.add(Projections.property("siteType"),"siteType")
+		//.add(Projections.property("varSite.name"), "siteName")
+		.add(Projections.property("id"),"id");
+			
 		String queryString=(String) queryMap.getFirst("query");
 		session = getSessionFactory().getCurrentSession();
 		criteria = session.createCriteria(Address.class);
+		criteria.createAlias("varOrgCode", "varOrgCode")
+		.createAlias("varPartner", "varPartner");
+		criteria.setProjection(proj).setResultTransformer(Transformers.aliasToBean(Address.class));
+		List<Address> list = criteria.list();
 		String limit=(String)queryMap.getFirst("limit");
 		String pageNo=(String) queryMap.getFirst("page");
 		if(queryString!=null){
 			Map<String,String> searchMap=ApplicationUtils.convertJSONtoMaps(queryString);
 			String dateType=searchMap.get("datecriteriavalue");
 			if(dateType!=null && !dateType.equals("")){
-				String sDate=searchMap.get("fromDate")+" 00:00:00";
-				String eDate=searchMap.get("toDate")+" 00:00:00";
+				String sDate=searchMap.get("fromDate");
+				String eDate=searchMap.get("toDate");
 				criteria=HibernateUtils.getCriteriaBasedOnDate(criteria, dateType, sDate, eDate);
 			}
 			String partnerName=searchMap.get("partnerName");
@@ -73,6 +94,15 @@ public class AddressDaoImpl extends GenericDaoImpl<Address, Long> implements
 				criteria.add(disCriteria);
 			}
 			
+			String systemname=searchMap.get("address");
+			if(address!=null && !"".equals(address)){
+				Disjunction disCriteria = Restrictions.disjunction();
+				disCriteria.add(Restrictions.ilike("address1", address,MatchMode.ANYWHERE));
+				disCriteria.add(Restrictions.ilike("address2", address,MatchMode.ANYWHERE));
+				disCriteria.add(Restrictions.ilike("address3", address,MatchMode.ANYWHERE));
+				disCriteria.add(Restrictions.ilike("address4", address,MatchMode.ANYWHERE));
+				criteria.add(disCriteria);
+			}
 		}   
 			totalCount=HibernateUtils.getAllRecordsCountWithCriteria(criteria);
 		    criteria.addOrder(Order.desc("lastModifiedDate"));
@@ -83,9 +113,30 @@ public class AddressDaoImpl extends GenericDaoImpl<Address, Long> implements
         criteria.setFirstResult((pageNO - 1) * pageSize);
         criteria.setMaxResults(pageSize);
 		}
+		if (queryMap.getFirst("orgCodeName") != null) {
+			String orgCode = (String) queryMap.getFirst("orgCodeName");
+			String orgUniqueName = orgCode;
+			criteria.add(Restrictions.eq("varOrgCode.name", orgUniqueName));
+			System.out.println(orgUniqueName);
 
+		}
+		if (queryMap.getFirst("partnerName") != null) {
+			String partnerName = (String) queryMap.getFirst("partnerName");
+			String partnerUniqueName = partnerName;
+			criteria.add(Restrictions.eq("varPartner.partnerName", partnerUniqueName));
+			System.out.println(partnerUniqueName);
+
+		}
+
+		if (queryMap.getFirst("partnerId") != null) {
+			String partnerId = (String) queryMap.getFirst("partnerId");
+			Long partnerUniqueId = Long.parseLong(partnerId);
+			criteria.add(Restrictions.eq("varPartner.id", partnerUniqueId));
+			System.out.println(partnerUniqueId);
+
+		}
 		entitiesMap.put("totalCount", totalCount);
-        entitiesMap.put("address", new LinkedHashSet(criteria.list()));
+        entitiesMap.put("address", new LinkedHashSet(list));
         
       
         return entitiesMap;
