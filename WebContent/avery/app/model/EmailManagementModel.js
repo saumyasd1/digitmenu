eed8eaedeb33211f7f5816ee0e3d3060691785a4
Krishna <Krishna@ADLP052_Krishna>
2017-01-17 14:56:36 +0530

    adding column for orderSource

--- WebContent/avery/app/model/EmailManagementModel.js
+++ WebContent/avery/app/model/EmailManagementModel.js
@@ -9,6 +9,7 @@ Ext.define('AOC.model.EmailManagementModel',{
 		{name: 'senderEmailId', mapping:'senderEmailId',type: 'string'},
 		{name: 'Email', mapping:'toMailId',type: 'string'},
 		{name: 'status', mapping:'status',type: 'string'},
+		{name: 'orderSource',mapping:'orderSource',type:'string'},
 		{name: 'receivedDate', mapping:'receivedDate',type: 'string'},
 		{name: 'readDate', mapping:'readDate',type: 'string'},
 		{name: 'acknowledgementDate', mapping:'acknowledgementDate',type: 'string'},
