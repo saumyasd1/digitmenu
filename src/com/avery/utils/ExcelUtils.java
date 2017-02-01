59ccb497027705f70ff765cc3fef2aa2107ed41b
Saumya <Saumya@ADLP039_Saumya>
2017-02-01 17:01:38 +0530

    adding cells and creating excel file using this utils

--- src/com/avery/utils/ExcelUtils.java
+++ src/com/avery/utils/ExcelUtils.java
@@ -20,7 +20,7 @@ public class ExcelUtils {
 		XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("Sheet 1");
         String[] headerNames = { "Order track #","Prv Order track#","Order Source", "PO #", "Order File",
-				"Additional data", "Partner Name", "RBO", "Product Line",
+				 "Partner Name", "RBO", "Product Line",
 				"Order Status", "Processed Date", "Sender Email ID", "Subject","Submitted By","Submitted Date","Acknowledgement Date","Comment","Error" };
         addHeader(sheet, headerNames);
         getReportData(sheet,OrderQueueList,headerNames.length);
@@ -42,72 +42,49 @@ public class ExcelUtils {
 	}
 	private static void getReportData(XSSFSheet sheet,List<OrderQueue> OrderQueueList,int headerLength){
 		int rowIndex=1,columncellCount=0;
-		String orderFile="",additionalData="";
-		Set<OrderFileAttachment> fileList=null;
 		Iterator<OrderQueue> CrunchifyIterator = OrderQueueList.iterator();
 		while (CrunchifyIterator.hasNext()) {
 			OrderQueue obj=CrunchifyIterator.next();
-			//fileList=obj.getOrderFileAttachment();
-			orderFile="";
-			additionalData="";
-			for(OrderFileAttachment fileObj:fileList){
-				if(fileObj.getFileContentType().equals("AdditionalData")){
-					additionalData=additionalData+","+fileObj.getFileName();
-				}else{
-					orderFile=orderFile+","+fileObj.getFileName();
-				}
-			}
-			if(!orderFile.equals("") && orderFile.length()>1){
-				orderFile=orderFile.substring(1, orderFile.length());
-			}
-			if(!additionalData.equals("") && additionalData.length()>1){
-				additionalData=additionalData.substring(1, additionalData.length());
-			}
 			Row row = sheet.createRow(rowIndex);
 			Cell cell1 = row.createCell(columncellCount);
 			cell1.setCellValue(obj.getId());
-			/*Cell cell2 = row.createCell(++columncellCount);
-			if(obj.getPrvOrderQueueID()!= null)
-			cell2.setCellValue(obj.getPrvOrderQueueID());
+			Cell cell2 = row.createCell(++columncellCount);
+			cell2.setCellValue(obj.getPrevOrderQueueId());
 			Cell cell3 = row.createCell(++columncellCount);
 			cell3.setCellValue(obj.getOrderSource());
 			Cell cell4 = row.createCell(++columncellCount);
-			cell4.setCellValue(obj.getPONumber());
+			cell4.setCellValue(obj.getPoNumber());
 			Cell cell5 = row.createCell(++columncellCount);
-			cell5.setCellValue(orderFile);
+			cell5.setCellValue(obj.getOrderFileName());
 			Cell cell6 = row.createCell(++columncellCount);
-			cell6.setCellValue(additionalData);
+			if(obj.getPartnerName()!=null && !obj.getPartnerName().equals(""))
+				cell6.setCellValue(obj.getPartnerName());
 			Cell cell7 = row.createCell(++columncellCount);
-			if(obj.getPartner()!=null && !obj.getPartner().equals(""))
-				cell7.setCellValue(obj.getPartner().getName());
+			cell7.setCellValue(obj.getRboName());
 			Cell cell8 = row.createCell(++columncellCount);
-			cell8.setCellValue(obj.getRboName());
+			if(obj.getProductLineType()!=null && !obj.getProductLineType().equals(""))
+				cell8.setCellValue(obj.getProductLineType());
 			Cell cell9 = row.createCell(++columncellCount);
-			if(obj.getProductLine()!=null && !obj.getProductLine().equals(""))
-				cell9.setCellValue(obj.getProductLine().getProductLineType());
+			cell9.setCellValue(OrderQueue.getCodeMap().get(obj.getStatus()));
 			Cell cell10 = row.createCell(++columncellCount);
-			cell10.setCellValue(OrderQueue.getCodeMap().get(obj.getStatus()));
-			Cell cell11 = row.createCell(++columncellCount);
 			if(obj.getReceivedDate()!=null)
-				cell11.setCellValue(obj.getReceivedDate().toString());
+				cell10.setCellValue(obj.getReceivedDate().toString());
+			Cell cell11 = row.createCell(++columncellCount);
+			cell11.setCellValue(obj.getSenderEmailId());
 			Cell cell12 = row.createCell(++columncellCount);
-			cell12.setCellValue(obj.getSenderEmailID());
+			cell12.setCellValue(obj.getSubject());
 			Cell cell13 = row.createCell(++columncellCount);
-			cell13.setCellValue(obj.getSubject());
-//			Cell cell13 = row.createCell(++columncellCount);
-//			cell13.setCellValue(obj.getEmailBody());
+			cell13.setCellValue(obj.getSubmittedBy());
 			Cell cell14 = row.createCell(++columncellCount);
-			cell14.setCellValue(obj.getSubmittedBy());
-			Cell cell15 = row.createCell(++columncellCount);
 			if(obj.getSubmittedDate()!=null)
-				cell15.setCellValue(obj.getSubmittedDate().toString());
+				cell14.setCellValue(obj.getSubmittedDate().toString());
+			Cell cell15 = row.createCell(++columncellCount);
+			if(obj.getFeedbackAcknowledgementDate()!=null)
+			cell15.setCellValue(obj.getFeedbackAcknowledgementDate().toString());                                               
 			Cell cell16 = row.createCell(++columncellCount);
-			if(obj.getAcknowledgementDate()!=null)
-			cell16.setCellValue(obj.getAcknowledgementDate().toString());*/
+			cell16.setCellValue(obj.getComment());
 			Cell cell17 = row.createCell(++columncellCount);
-			cell17.setCellValue(obj.getComment());
-			Cell cell18 = row.createCell(++columncellCount);
-			cell18.setCellValue(obj.getError());
+			cell17.setCellValue(obj.getError());
 			columncellCount=0;
 			rowIndex++;
 		}
