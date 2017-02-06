821056fc4516924f029e996faf88691edf881769
Amit <Amit@ADLP083_Amit>
2017-02-06 15:55:42 +0530

    remove checks for oracle error to cancel order as suggested by (Bosco)

--- WebContent/avery/app/view/orderqueue/OrderQueueController.js
+++ WebContent/avery/app/view/orderqueue/OrderQueueController.js
@@ -377,7 +377,7 @@ Ext.define('AOC.view.orderqueue.OrderQueueController', {
 					return Helper.getDisableMenuItemStyle();
 				},
 				getCancelOrderEnableDisableClass:function(v){
-					if(v.Status == AOCLit.waitingForCSRStatusOrderQueue || v.Status == AOCLit.errorStatusOrderQueue || v.status == AOCLit.oracleErrorStatusOrderQueue){
+					if(v.Status == AOCLit.waitingForCSRStatusOrderQueue || v.Status == AOCLit.errorStatusOrderQueue){
 						 return 'user-profile-menu-item';
 					}
 					return 'order-profile-menu-item';
