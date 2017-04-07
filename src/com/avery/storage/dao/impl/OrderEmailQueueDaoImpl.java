e6268aa2b4c9da50357a01aa638c47d7ea98c4ea
Krishna Kumar Varshney <krishna.varshney@adeptia.com>
2017-04-07 18:18:16 +0530

    Update OrderEmailQueueDaoImpl.java
    Reverting back unneccesary commit

--- src/com/avery/storage/dao/impl/OrderEmailQueueDaoImpl.java
+++ src/com/avery/storage/dao/impl/OrderEmailQueueDaoImpl.java
@@ -53,7 +53,6 @@ OrderEmailQueueDao {
 	
 	@Override
 	public Map getAllEntitiesWithCriteria(MultivaluedMap queryMap) throws Exception {
-		System.out.println(new Date());
 		Map entitiesMap = new HashMap();
 		int totalCount = 0;
 		Criteria criteria = getCriteria(queryMap);//session.createCriteria(OrderEmailQueue.class);
