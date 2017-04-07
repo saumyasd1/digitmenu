dbe9e74aaa1727118c18b025f3bd5b470bd07ebb
Krishna Kumar Varshney <krishna.varshney@adeptia.com>
2017-04-07 18:16:46 +0530

    Update PartnerMixIn.java
    Reverting back unneccesary commit

--- src/com/avery/storage/MixIn/PartnerMixIn.java
+++ src/com/avery/storage/MixIn/PartnerMixIn.java
@@ -10,7 +10,6 @@ import com.avery.storage.entities.OrderQueue;
 import com.avery.storage.entities.ProductLine;
 import com.avery.storage.entities.SalesOrder;
 import com.avery.storage.entities.Site;
-import com.avery.storage.entities.Wi;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public abstract class PartnerMixIn {
@@ -39,8 +38,4 @@ public abstract class PartnerMixIn {
 	@JsonIgnore
 	public abstract List<Address> getAddressList();
 	
-	@JsonIgnore
-	public abstract List<Wi> getListWi();
-
-
 }
