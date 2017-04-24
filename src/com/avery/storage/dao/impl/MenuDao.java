486fe51801106e04820a714b102a86e0c05afba3
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-04-24 17:59:53 +0530

    Code for User Management

---
+++ src/com/avery/storage/dao/impl/MenuDao.java
@@ -0,0 +1,8 @@
+package com.avery.storage.dao.impl;
+
+import com.avery.storage.dao.GenericDao;
+import com.avery.storage.entities.Menu;
+
+public interface MenuDao extends GenericDao<Menu, Long> {
+
+}
