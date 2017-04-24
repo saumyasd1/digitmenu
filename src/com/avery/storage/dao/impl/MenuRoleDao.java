f090732d226cd03658899302bacf8349312eeb33
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-04-24 17:54:05 +0530

    user Managment code

---
+++ src/com/avery/storage/dao/impl/MenuRoleDao.java
@@ -0,0 +1,8 @@
+package com.avery.storage.dao.impl;
+
+import com.avery.storage.dao.GenericDao;
+import com.avery.storage.entities.MenuRole;
+
+public interface MenuRoleDao extends GenericDao<MenuRole, Long> {
+
+}
---
+++ src/com/avery/storage/dao/impl/MenuRoleDaoImpl.java
@@ -0,0 +1,54 @@
+package com.avery.storage.dao.impl;
+
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+import org.springframework.stereotype.Repository;
+
+import com.avery.storage.dao.GenericDaoImpl;
+import com.avery.storage.entities.MenuRole;
+import com.avery.utils.HibernateUtils;
+
+@SuppressWarnings("unchecked")
+@Repository
+public class MenuRoleDaoImpl extends GenericDaoImpl<MenuRole, Long> implements MenuRoleDao {
+
+	@Override
+	public Map getAllEntitiesWithCriteria(MultivaluedMap queryMap) throws Exception {
+		
+		Map entitiesMap = new HashMap();
+		Session session = getSessionFactory().getCurrentSession();
+		int totalCount = 0;
+		Criteria criteria = session.createCriteria(MenuRole.class);
+		String limit = (String) queryMap.getFirst("limit");
+		String pageNo = (String) queryMap.getFirst("page");
+		totalCount = HibernateUtils.getAllRecordsCountWithCriteria(criteria);
+		criteria.addOrder(Order.desc("lastModifiedDate"));
+		String pageNumber = pageNo == null ? "" : pageNo;
+		int pageNO = (!"".equals(pageNumber)) ? Integer.parseInt(pageNumber): 0;
+		int pageSize = (limit != null && !"".equals(limit)) ? Integer.parseInt(limit) : 0;
+		if (pageNO != 0) {
+			criteria.setFirstResult((pageNO - 1) * pageSize);
+			criteria.setMaxResults(pageSize);
+		}
+		
+		if (queryMap.getFirst("id") != null) {
+			String id = (String) queryMap.getFirst("id");
+			Long csrId = Long.parseLong(id);
+			criteria.add(Restrictions.eq("id", csrId));
+		}
+		
+		entitiesMap.put("totalCount", totalCount);
+		entitiesMap.put("menuRolelist", new LinkedHashSet(criteria.list()));
+		return entitiesMap;
+	}
+	
+}
