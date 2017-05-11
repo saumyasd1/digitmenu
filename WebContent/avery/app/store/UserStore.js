c774016b0905c9a82442c7d99e823245b2c55de5
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-05-11 14:55:22 +0530

    Removing autoload:true for sending query params in proxy

--- WebContent/avery/app/store/UserStore.js
+++ WebContent/avery/app/store/UserStore.js
@@ -1,10 +1,18 @@
 Ext.define('AOC.store.UserStore', {
 	extend : 'Ext.data.Store',
 	model:'AOC.model.UserModel',
-	autoLoad : true,
 	remoteSort: true,
 	totalCount:'total',
 	pageSize:pageSize,
-	storeId:'UserStoreId'
+	storeId:'UserStoreId',
+	proxy: {
+        type: 'rest',
+        url: applicationContext+'/rest/users',
+        reader: {
+            type: 'json',
+            rootProperty: 'users',
+            totalProperty: 'totalCount'
+        }
+    },
 });
 
