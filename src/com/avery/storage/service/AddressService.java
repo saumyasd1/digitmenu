6b803c163292b011c4a6835062b8226f08d7a2c9
Saumya <Saumya@ADLP039_Saumya>
2017-05-12 11:16:50 +0530

    creating service for site id filteration on address grd

--- src/com/avery/storage/service/AddressService.java
+++ src/com/avery/storage/service/AddressService.java
@@ -1,12 +1,31 @@
 package com.avery.storage.service;
 
-import org.springframework.stereotype.Component;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.avery.storage.dao.impl.AddressDao;
 import com.avery.storage.entities.Address;
+import com.avery.storage.entities.User;
 
 @Component
 public class AddressService extends GenericEntityService<Address, Long>{
-	
-	
+	private AddressDao addressDao;
+
+	public AddressDao getAddressDao() {
+		return addressDao;
+	}
+
+	@Autowired
+	public void setAddressDao(AddressDao addressDao) {
+		this.addressDao = addressDao;
+	}
+
+	@Transactional
+	public List<Address> getAddress(String siteId) {
+		return getAddressDao().getAddress(siteId);
+	}
 
 }
