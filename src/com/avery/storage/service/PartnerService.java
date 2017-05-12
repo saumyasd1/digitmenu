9bae2792c168afe0724bdbfaaafd112b5ac9fc96
Saumya <Saumya@ADLP039_Saumya>
2017-05-12 11:12:16 +0530

    adding service for site filteration on partner grid

--- src/com/avery/storage/service/PartnerService.java
+++ src/com/avery/storage/service/PartnerService.java
@@ -1,10 +1,13 @@
 package com.avery.storage.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.avery.storage.dao.impl.PartnerDao;
+import com.avery.storage.entities.Address;
 import com.avery.storage.entities.Partner;
 
 
@@ -26,6 +29,11 @@ public class PartnerService extends GenericEntityService<Partner, Long>{
 	@Transactional
 	public Boolean checkDuplicatePartnerName(Partner partnerName) throws Exception {
 		return partnerDao.checkDuplicatePartnerName(partnerName);
+	}
+
+	@Transactional
+	public List<Partner> getPartner(String siteId) {
+		return getPartnerDao().getPartner(siteId);
 	}
 
 
