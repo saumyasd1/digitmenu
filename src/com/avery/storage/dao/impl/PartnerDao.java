d95988f4389934f9a0f2e1161e13a784cab34f02
Saumya <Saumya@ADLP039_Saumya>
2017-05-12 11:10:32 +0530

    adding method for site filteration on patner grid

--- src/com/avery/storage/dao/impl/PartnerDao.java
+++ src/com/avery/storage/dao/impl/PartnerDao.java
@@ -1,11 +1,15 @@
 package com.avery.storage.dao.impl;
 
+import java.util.List;
+
 import com.avery.storage.dao.GenericDao;
 import com.avery.storage.entities.Partner;
 
 public interface PartnerDao extends GenericDao<Partner, Long>{
 	
 	public Boolean checkDuplicatePartnerName(Partner partnerName) throws Exception;
+
+	public List<Partner> getPartner(String siteId);
 
 
 }
