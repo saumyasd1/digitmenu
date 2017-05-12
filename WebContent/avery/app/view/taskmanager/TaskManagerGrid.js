9a412856264572e36b2b2034ee8f631d95e96c2e
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-05-12 10:50:46 +0530

    [Jira#387] Changes for clearing cookie at login-logout

--- WebContent/avery/app/view/taskmanager/TaskManagerGrid.js
+++ WebContent/avery/app/view/taskmanager/TaskManagerGrid.js
@@ -1,211 +1,194 @@
 Ext.define('AOC.view.taskmanager.TaskManagerGrid', {
-	extend : 'Ext.grid.Panel',
-	requires : ['AOC.util.Helper'],
-	controller:'taskManagerController',
-	itemId : 'TaskManagerGriditemId',
-    alias : 'widget.taskmanagergrid',
-    cls:'aoc-panel',
+    extend: 'Ext.grid.Panel',
+    requires: ['AOC.util.Helper'],
+    controller: 'taskManagerController',
+    itemId: 'TaskManagerGriditemId',
+    alias: 'widget.taskmanagergrid',
+    cls: 'aoc-panel',
     emptyText: AOCLit.emptyDataMsg,
-    reserveScrollbar:true,
-    requires:['AOC.store.TaskManagerStore','AOC.view.taskmanager.TaskManagerController'],
-	columnLines:false,
-	viewConfig : {
-		stripeRows : true,
-		enableTextSelection : true
-	},
-    initComponent : function(){
-    	var me=this;
-    		me.columns = this.buildColumns();
-    		me.dockedItems = this.buildDockedItems();
-			userInfo = AOCRuntime.getUser(),
-			roleId = userInfo.role,
-			siteId = userInfo.siteId,
-			userId = userInfo.id,
-			userEmailId = userInfo.email;
-		
-        Ext.apply(me,{
-    		listeners:{
-    	        activate:function(obj){
-					me.down('pagingtoolbar').bindStore(obj.getStore());
-					obj.getStore().proxy.extraParams = { siteId:siteId,roleId:roleId,userId:userId,userEmailId:userEmailId };
-				}
-			},		
-            tbar: { 
-				height: 50,
-				items : me.buildTbar()
-			},
-			store:Ext.create('AOC.store.TaskManagerStore', {	
-				storeId:'TaskManagerStoreId'
-			})
-        });
-	   me.callParent(arguments);
-	},
-	plugins: [
-		Ext.create('Ext.grid.plugin.CellEditing', {
-			clicksToEdit: 1
-		})
-	],
-	buildColumns : function(){
-      	var me=this;
-		return [  
-			{
-				xtype:'actioncolumn',
-				width:25,
-				header: '<img src="'+AOC.config.Settings.buttonIcons.menuIcon + '" />',
-				baseCls:'custom-action',
-				items:[
-					{	
-						icon: AOC.config.Settings.buttonIcons.menuIcon,
-						handler: 'onClickMenu'
-					}
-				]
-			},
-			{
-  		       	header: Settings.config.defaultIcons.commentColumnIcon,
-  	            width:40,
-  				dataIndex:'comment',
-  				tooltip:'Comments',
-  				menuDisabled :true,
-  				baseCls:'custom-action',
-  				renderer:function(value, metaData, rec){
-  					if(value){
-  						var comment = Ext.String.htmlEncode(value);
-  						metaData.tdAttr = 'data-qtip="<font color=blue>' + comment + '</font>"';
-						return Settings.config.defaultIcons.commentColumnIcon;
-  					}else{
-  						return '';
-					}
-  				} 
-			},
-			{  
-				text : AOCLit.TrackingNo,
-				align: 'left',
-				dataIndex:'id',
-				flex:0.5,
-				renderer:function(v, metadata, record){
-					return 'E# '+ v;
-				}
-			},
-			{
-				text :AOCLit.CSRName,
-				sortable : true,
-				dataIndex:'csrName',
-				flex:0.5
-			},
-			{
-				text : AOCLit.from,
-				sortable : true,
-				dataIndex:'senderEmailId',
-				flex:0.9
-			},
-			{
-				text :AOCLit.Subject,
-				sortable : true,
-				dataIndex:'subject',
-				flex:0.5,
-				renderer:function(v,metadata){
-					return me.tipRenderer(v, metadata);
-				}
-			},
-			{
-				text :AOCLit.ccMailId,
-				sortable : true,
-				dataIndex:'ccMailId',
-				flex:0.5,
-				renderer:function(v,metadata){
-					return me.tipRenderer(v, metadata);
-				}
-			},
-			{
-				text :AOCLit.Status,
-				sortable : true,
-				dataIndex:'status',
-				flex:0.5,
-				renderer:function(v, metadata,rec){
-					return Helper.getSatus(rec);
-				}
-			},
-			{
-				text : AOCLit.dateTime,
-				sortable : true,
-				dataIndex:'createdDate',
-				align: 'right',
-				flex:0.6
-			},
-			{
-				text :AOCLit.lastModifiedBy,
-				dataIndex:'lastModifiedBy',
-				flex:0.5
-			},
-			{
-				text :AOCLit.lastModifiedDate,
-				dataIndex:'lastModifiedDate',
-				flex:0.6
-			}
-		];
-	},
-	tipRenderer:function(v, metaData){
-		if(v){
-			metaData.tdAttr = 'data-qtip="<font color=blue>' + Ext.String.htmlEncode(v) + '</font>"';
-			return Ext.String.htmlEncode(v);
-		}else {
-			return '';
-		}
-	},
-	buildTbar:function(){
-  		var me=this;
-		return [
-			{
-				xtype : 'tbtext',
-				itemId : 'TaskManagerTextId',
-				text:'Task Manager',
-				style:'color:#2c3e50;font-weight:bold;'
-			},'->',
-			{
-				xtype: 'customsearchfield',
-				searchCriteria:'',
-				store : Ext.data.StoreManager.lookup(me.store),
-				width: 200,
-				emptyText: "Search by Email Subject"
-			},
-		    {
-				xtype:'button',
-				refrence:'advancesearchbutton',
-				text:AOCLit.advSearchText,
-				//icon   :  AOC.config.Settings.buttonIcons.advSearchIcon,
-				iconCls:'fa fa-search',
-				iconAlign: "right",
-				ui:'blue',
-				cls:'blue-btn',
-				handler:'openAdvancedSearchWindow'
-			 },
-			 {
-				hidden:true, 
-				//icon   :  AOC.config.Settings.buttonIcons.clearSearchIcon,
-				iconCls:'fa fa-times',
-				ui:'blue',
-				cls:'blue-btn',
-				itemId:'clearadvanedsearch',
-				reference:'clearAdvSearch',
-				handler:'clearAdvancedSearch',
-				tooltip:'Clear Search'
-			 }
-		];
-	},
-	buildDockedItems : function(){
-     	var me=this;
-		return [
-			{
-				xtype : 'pagingtoolbar',
-				dock : 'bottom',
-				ui : 'darktoolbar',
-				itemId:'pagingtoolbar',
-				store:me.store,
-				displayInfo:true,
-				pageSize:pageSize,
-				plugins:Ext.create('Ext.ux.ProgressBarPager',{width:250})
-			}
-		];
-	}
-});
+    reserveScrollbar: true,
+    requires: ['AOC.store.TaskManagerStore', 'AOC.view.taskmanager.TaskManagerController'],
+    columnLines: false,
+    viewConfig: {
+        stripeRows: true,
+        enableTextSelection: true
+    },
+    initComponent: function () {
+        var me = this;
+        me.columns = this.buildColumns();
+        me.dockedItems = this.buildDockedItems();
 
+        Ext.apply(me, {
+            listeners: {
+                activate: function (obj) {
+                    var userInfo = AOCRuntime.getUser(),
+                        roleId = userInfo.role,
+                        siteId = userInfo.siteId,
+                        userId = userInfo.id,
+                        userEmailId = userInfo.email;
+                    me.down('pagingtoolbar').bindStore(obj.getStore());
+                    obj.getStore().proxy.extraParams = {
+                        siteId: siteId,
+                        roleId: roleId,
+                        userId: userId,
+                        userEmailId: userEmailId
+                    };
+                }
+            },
+            tbar: {
+                height: 50,
+                items: me.buildTbar()
+            },
+            store: Ext.create('AOC.store.TaskManagerStore', {
+                storeId: 'TaskManagerStoreId'
+            })
+        });
+        me.callParent(arguments);
+    },
+    plugins: [
+        Ext.create('Ext.grid.plugin.CellEditing', {
+            clicksToEdit: 1
+        })
+    ],
+    buildColumns: function () {
+        var me = this;
+        return [{
+            xtype: 'actioncolumn',
+            width: 25,
+            header: '<img src="' + AOC.config.Settings.buttonIcons.menuIcon + '" />',
+            baseCls: 'custom-action',
+            items: [{
+                icon: AOC.config.Settings.buttonIcons.menuIcon,
+                handler: 'onClickMenu'
+            }]
+        }, {
+            header: Settings.config.defaultIcons.commentColumnIcon,
+            width: 40,
+            dataIndex: 'comment',
+            tooltip: 'Comments',
+            menuDisabled: true,
+            baseCls: 'custom-action',
+            renderer: function (value, metaData, rec) {
+                if (value) {
+                    var comment = Ext.String.htmlEncode(value);
+                    metaData.tdAttr = 'data-qtip="<font color=blue>' + comment + '</font>"';
+                    return Settings.config.defaultIcons.commentColumnIcon;
+                } else {
+                    return '';
+                }
+            }
+        }, {
+            text: AOCLit.TrackingNo,
+            align: 'left',
+            dataIndex: 'id',
+            flex: 0.5,
+            renderer: function (v, metadata, record) {
+                return 'E# ' + v;
+            }
+        }, {
+            text: AOCLit.CSRName,
+            sortable: true,
+            dataIndex: 'csrName',
+            flex: 0.5
+        }, {
+            text: AOCLit.from,
+            sortable: true,
+            dataIndex: 'senderEmailId',
+            flex: 0.9
+        }, {
+            text: AOCLit.Subject,
+            sortable: true,
+            dataIndex: 'subject',
+            flex: 0.5,
+            renderer: function (v, metadata) {
+                return me.tipRenderer(v, metadata);
+            }
+        }, {
+            text: AOCLit.ccMailId,
+            sortable: true,
+            dataIndex: 'ccMailId',
+            flex: 0.5,
+            renderer: function (v, metadata) {
+                return me.tipRenderer(v, metadata);
+            }
+        }, {
+            text: AOCLit.Status,
+            sortable: true,
+            dataIndex: 'status',
+            flex: 0.5,
+            renderer: function (v, metadata, rec) {
+                return Helper.getSatus(rec);
+            }
+        }, {
+            text: AOCLit.dateTime,
+            sortable: true,
+            dataIndex: 'createdDate',
+            align: 'right',
+            flex: 0.6
+        }, {
+            text: AOCLit.lastModifiedBy,
+            dataIndex: 'lastModifiedBy',
+            flex: 0.5
+        }, {
+            text: AOCLit.lastModifiedDate,
+            dataIndex: 'lastModifiedDate',
+            flex: 0.6
+        }];
+    },
+    tipRenderer: function (v, metaData) {
+        if (v) {
+            metaData.tdAttr = 'data-qtip="<font color=blue>' + Ext.String.htmlEncode(v) + '</font>"';
+            return Ext.String.htmlEncode(v);
+        } else {
+            return '';
+        }
+    },
+    buildTbar: function () {
+        var me = this;
+        return [{
+            xtype: 'tbtext',
+            itemId: 'TaskManagerTextId',
+            text: 'Task Manager',
+            style: 'color:#2c3e50;font-weight:bold;'
+        }, '->', {
+            xtype: 'customsearchfield',
+            searchCriteria: '',
+            store: Ext.data.StoreManager.lookup(me.store),
+            width: 200,
+            emptyText: "Search by Email Subject"
+        }, {
+            xtype: 'button',
+            refrence: 'advancesearchbutton',
+            text: AOCLit.advSearchText,
+            iconCls: 'fa fa-search',
+            iconAlign: "right",
+            ui: 'blue',
+            cls: 'blue-btn',
+            handler: 'openAdvancedSearchWindow'
+        }, {
+            hidden: true,
+            iconCls: 'fa fa-times',
+            ui: 'blue',
+            cls: 'blue-btn',
+            itemId: 'clearadvanedsearch',
+            reference: 'clearAdvSearch',
+            handler: 'clearAdvancedSearch',
+            tooltip: 'Clear Search'
+        }];
+    },
+    buildDockedItems: function () {
+        var me = this;
+        return [{
+            xtype: 'pagingtoolbar',
+            dock: 'bottom',
+            ui: 'darktoolbar',
+            itemId: 'pagingtoolbar',
+            store: me.store,
+            displayInfo: true,
+            pageSize: pageSize,
+            plugins: Ext.create('Ext.ux.ProgressBarPager', {
+                width: 250
+            })
+        }];
+    }
+});
