1fb3518fabf904f48bd1f806174f072b3570bb15
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-05-12 10:51:07 +0530

    [Jira#387] Changes for clearing cookie at login-logout

--- WebContent/avery/app/view/users/manage/UserGrid.js
+++ WebContent/avery/app/view/users/manage/UserGrid.js
@@ -1,129 +1,136 @@
 Ext.define('AOC.view.users.manage.UserGrid', {
-	extend : 'Ext.grid.Panel',
-	requires : [ 'AOC.view.ux.CustomSearchField' ],
-	itemId : 'usersgriditemId',
-	alias : 'widget.usersgrid',
-	cls : 'aoc-panel',
-	controller : 'userMain',
-	emptyText : AOCLit.emptyDataMsg,
-	recordBeingEdit : null,
-	initComponent : function() {
-		var me = this,
-			userInfo = AOCRuntime.getUser(),
-			roleId = userInfo.role,
-			siteId = userInfo.siteId,
-			userId = userInfo.id,
-			userEmailId = userInfo.email;
-		Ext.apply(this, {
-			columns : this.buildColumns(),
-			columnLines : false,
-			store:Ext.create('AOC.store.UserStore', {
-					storeId:'UserStoreId'}),
-			tbar : {
-				height : AOC.config.Settings.config.defaultTbarHeight,
-				items : me.buildtbar()
-			},
-			dockedItems : this.buildDockedItems(),
-			listeners : {
-				activate : function(obj) {
-					me.down('pagingtoolbar').bindStore(obj.getStore());
-	             	obj.getStore().proxy.extraParams = { siteId:siteId,roleId:roleId,userId:userId,userEmailId:userEmailId };
-	             	obj.getStore().load();
-				}
-			},
-			viewConfig : {
-				stripeRows : true,
-				enableTextSelection : true
-			}
-		});
-		this.callParent(arguments);
-	},
-	buildColumns : function() {
-		var me = this;
-		return [ {
-			xtype : 'actioncolumn',
-			width : 25,
-			baseCls : 'custom-action',
-			items : [ {
-				icon : AOC.config.Settings.buttonIcons.menuIcon,
-				handler : 'onClickMenu'
+    extend: 'Ext.grid.Panel',
+    requires: ['AOC.view.ux.CustomSearchField'],
+    itemId: 'usersgriditemId',
+    alias: 'widget.usersgrid',
+    cls: 'aoc-panel',
+    controller: 'userMain',
+    emptyText: AOCLit.emptyDataMsg,
+    recordBeingEdit: null,
+    initComponent: function () {
+        var me = this;
 
-			} ]
-		}, {
-			text : 'First Name',
-			width : 120,
-			sortable : true,
-			dataIndex : 'firstName',
-			flex : 0.5
+        Ext.apply(this, {
+            columns: this.buildColumns(),
+            columnLines: false,
+            store: Ext.create('AOC.store.UserStore', {
+                storeId: 'UserStoreId'
+            }),
+            tbar: {
+                height: AOC.config.Settings.config.defaultTbarHeight,
+                items: me.buildtbar()
+            },
+            dockedItems: this.buildDockedItems(),
+            listeners: {
+                activate: function (obj) {
+                    var userInfo = AOCRuntime.getUser(),
+                        roleId = userInfo.role,
+                        siteId = userInfo.siteId,
+                        userId = userInfo.id,
+                        userEmailId = userInfo.email;
+                    me.down('pagingtoolbar').bindStore(obj.getStore());
+                    obj.getStore().proxy.extraParams = {
+                        siteId: siteId,
+                        roleId: roleId,
+                        userId: userId,
+                        userEmailId: userEmailId
+                    };
+                    obj.getStore().load();
+                }
+            },
+            viewConfig: {
+                stripeRows: true,
+                enableTextSelection: true
+            }
+        });
+        this.callParent(arguments);
+    },
+    buildColumns: function () {
+        var me = this;
+        return [{
+            xtype: 'actioncolumn',
+            width: 25,
+            baseCls: 'custom-action',
+            items: [{
+                icon: AOC.config.Settings.buttonIcons.menuIcon,
+                handler: 'onClickMenu'
 
-		}, {
-			text : 'Last Name',
-			width : 120,
-			sortable : true,
-			dataIndex : 'lastName',
-			flex : 1
-		}, {
-			text : 'Role',
-			width : 120,
-			sortable : true,
-			dataIndex : 'role',
-			flex : 0.5,
-			renderer : function(value, metadata, record) {
-				if (value == 1) {
-					return 'Super Admin';
-				} else if (value == 2) {
-					return 'Site Manager';
-				} else {
-					return 'CSR';
-				}
-			}
-		}, {
-			text : 'Email',
-			width : 120,
-			sortable : true,
-			dataIndex : 'email',
-			flex : 0.5
-		} ];
-	},
-	buildtbar : function() {
-		var me = this;
-		return [ {
-			xtype : 'tbtext',
-			itemId : 'UsertextItemId',
-			text : '<b>Users</b>'
-		}, {
-			xtype : 'tbspacer',
-			width : 10
-		}, {
-			text : 'New',
-			itemId : 'newUser',
-			handler : 'createuser',
-			hidden : false,
-			iconCls : 'fa fa-plus',
-			iconAlign : 'left',
-			ui : 'blue',
-			cls : 'blue-btn'
-		}, '->', {
-			xtype : 'customsearchfield',
-			searchCriteria : '',
-			store : Ext.data.StoreManager.lookup(me.store),
-			width : 200,
-			emptyText : AOCLit.userGridSearchEmptyText
-		} ];
-	},
-	buildDockedItems : function() {
-		var me = this;
-		return [ {
-			xtype : 'pagingtoolbar',
-			dock : 'bottom',
-			ui : 'darktoolbar',
-			itemId : 'pagingtoolbar',
-			store : me.store,
-			displayInfo : true,
-			plugins : Ext.create('Ext.ux.ProgressBarPager', {
-				width : 250
-			})
+            }]
+        }, {
+            text: 'First Name',
+            width: 120,
+            sortable: true,
+            dataIndex: 'firstName',
+            flex: 0.5
 
-		} ];
-	}
+        }, {
+            text: 'Last Name',
+            width: 120,
+            sortable: true,
+            dataIndex: 'lastName',
+            flex: 1
+        }, {
+            text: 'Role',
+            width: 120,
+            sortable: true,
+            dataIndex: 'role',
+            flex: 0.5,
+            renderer: function (value, metadata, record) {
+                if (value == 1) {
+                    return 'Super Admin';
+                } else if (value == 2) {
+                    return 'Site Manager';
+                } else {
+                    return 'CSR';
+                }
+            }
+        }, {
+            text: 'Email',
+            width: 120,
+            sortable: true,
+            dataIndex: 'email',
+            flex: 0.5
+        }];
+    },
+    buildtbar: function () {
+        var me = this;
+        return [{
+            xtype: 'tbtext',
+            itemId: 'UsertextItemId',
+            text: '<b>Users</b>'
+        }, {
+            xtype: 'tbspacer',
+            width: 10
+        }, {
+            text: 'New',
+            itemId: 'newUser',
+            handler: 'createuser',
+            hidden: false,
+            iconCls: 'fa fa-plus',
+            iconAlign: 'left',
+            ui: 'blue',
+            cls: 'blue-btn'
+        }, '->', {
+            xtype: 'customsearchfield',
+            searchCriteria: '',
+            store: Ext.data.StoreManager.lookup(me.store),
+            width: 200,
+            emptyText: AOCLit.userGridSearchEmptyText
+        }];
+    },
+    buildDockedItems: function () {
+        var me = this;
+        return [{
+            xtype: 'pagingtoolbar',
+            dock: 'bottom',
+            ui: 'darktoolbar',
+            itemId: 'pagingtoolbar',
+            store: me.store,
+            displayInfo: true,
+            plugins: Ext.create('Ext.ux.ProgressBarPager', {
+                width: 250
+            })
+
+        }];
+    }
 });
