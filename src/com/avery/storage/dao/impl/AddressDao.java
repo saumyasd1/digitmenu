c6ea9af36d25926299b1dbee6387afd5f10fec8d
Saumya <Saumya@ADLP039_Saumya>
2017-05-12 11:14:46 +0530

    adding method for site filteration on address grid

--- src/com/avery/storage/dao/impl/AddressDao.java
+++ src/com/avery/storage/dao/impl/AddressDao.java
@@ -1,10 +1,12 @@
 package com.avery.storage.dao.impl;
 
+import java.util.List;
+
 import com.avery.storage.dao.GenericDao;
 import com.avery.storage.entities.Address;
 
 public interface AddressDao extends GenericDao<Address, Long>{
-	
-	
 
+	List<Address> getAddress(String siteId);
+	
 }
