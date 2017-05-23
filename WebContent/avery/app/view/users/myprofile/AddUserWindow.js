d3a958e11375dea771d94e1d95dd545a428096c4
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-05-23 11:48:14 +0530

    [Jira# AOC-379]Changes related to Enhanced GUI

---
+++ WebContent/avery/app/view/users/myprofile/AddUserWindow.js
@@ -0,0 +1,189 @@
+Ext.define('AOC.view.users.myprofile.AddUserWindow', {
+    extend: 'AOC.view.base.NewBaseWindow',
+    alias: 'widget.createuser',
+    itemId: 'createuserItemId',
+    controller: 'adduserwindow',
+    reference: 'addEditUserWin',
+    mode: 'add',
+    bodyPadding: 7,
+    title: 'Add User',
+    layout: {
+        type: 'vbox',
+        align: 'stretch'
+    },
+    listeners: {
+        afterrender: 'onAddUserAfterRender'
+    },
+    initComponent: function () {
+        var me = this;
+        Ext.apply(me, {
+            items: me.buildItems(),
+            buttons: me.buildButtons(),
+
+        });
+        me.callParent(arguments);
+    },
+    buildButtons: function () {
+        return [{
+            text: 'Save',
+            handler: 'SaveDetails'
+        }, {
+            text: 'Cancel',
+            handler: 'CancelDetails'
+        }];
+    },
+    buildItems: function () {
+        var me = this,
+            maxCharText = AOCLit.maximumNChar;
+        return [{
+            xtype: 'form',
+            itemId: 'editform',
+            layout: 'hbox',
+            reference: 'addEditUserWinForm',
+            mode: me.mode,
+            items: [{
+                xtype: 'tbspacer',
+                width: 20
+            	}, {
+                xtype: 'fieldcontainer',
+                layout: 'vbox',
+                items: [{
+                    xtype: 'image',
+                    anchor: '40%',
+                    reference:'profileImage',
+                    margin: me.mode == 'edit'? '20 0 0 0' : '35 0 0 0',
+                    width: 160,
+                    height: 160,
+                    style: {
+                        border: 'solid 1.5px #ddd; border-radius:160px;'
+                    }
+                }, {
+                    xtype: 'fileuploadfield',
+                    buttonText: AOCLit.uploadPhoto,
+                    margin: '10 0 0 35',
+                    buttonOnly: true,
+                    listeners:{
+						'change':'onClickUpdatePhoto',
+						'beforerender':'beforeRenderUploadbtn'
+					}
+                }]
+            }, {
+                xtype: 'tbspacer',
+                width: 20
+            }, {
+                xtype: 'fieldcontainer',
+                anchor: '60%',
+                layout: {
+                    type: 'vbox',
+                    align: 'stretch'
+                },
+                defaults: {
+                    xtype: 'textfield',
+                    labelWidth: 165,
+                    width: 615,
+                    margin: '0 0 5 0',
+                    labelSeparator: '',
+                    labelStyle: Settings.config.defaultFormLabelStyle,
+                    labelAlign: Settings.form.defaultLabelAlign
+                },
+                items: 
+                	[{
+                      name: 'firstName',
+                      fieldLabel: AOCLit.firstName,
+                      allowBlank: false,
+                      maxLength: 64,
+                      itemId:'name',
+                      maxLengthText: maxCharText.replace("$$$$", 64),
+                      regex: new RegExp(AOC.config.Settings.getNameRegex()),
+                      regexText: AOCLit.splCharNotAllowed
+                }, {
+                    name: 'lastName',
+                    fieldLabel: AOCLit.lastName,
+                    maxLength: 64,
+                    maxLengthText: maxCharText.replace("$$$$", 64),
+                    regex: new RegExp(AOC.config.Settings.getNameRegex()),
+                    regexText: AOCLit.splCharNotAllowed
+                }, {
+                    xtype: 'radiogroup',
+                    fieldLabel: AOCLit.iam,
+                    width: 300,
+                    items: [{
+                        boxLabel: AOCLit.male,
+                        name: 'gender',
+                        width: 100,
+                        inputValue: 'Male'
+                    }, {
+                        boxLabel: AOCLit.female,
+                        name: 'gender',
+                        width: 100,
+                        inputValue: 'Female'
+                    }]
+                }, {
+                    name: 'email',
+                    fieldLabel: AOCLit.emailAddress,
+                    allowBlank: false,
+                    regex: /^((([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z\s?]{2,5}){1,25})*(\s*?,\s*?)*)*$/	
+                }, {
+                    name: 'jobTitle',
+                    fieldLabel: AOCLit.jobTitle,
+                    maxLength: 64,
+                    maxLengthText: maxCharText.replace("$$$$", 64)
+                }, {
+                    xtype: 'combo',
+                    editable: false,
+                    fieldLabel: AOCLit.role,
+                    name: 'role',
+                    flex: 1,
+                    allowBlank: false,
+                    itemId:'role',
+                    displayField: 'roleName',
+                    reference: 'roleName',
+                    valueField: 'id',
+                    store: Ext.create('AOC.store.RoleStore'),
+                    queryMode: 'local'
+                }, {
+                    xtype: 'combo',
+                    name: 'siteId',
+                    itemId:'site',
+                    fieldLabel: AOCLit.site,
+                    allowBlank: false,
+                    flex: 1,
+                    editable: false,
+                    displayField: 'name',
+                    reference: 'siteName',
+                    valueField: 'id',
+                    store: Ext.create('AOC.store.SiteStore'),
+                    queryMode: 'local'
+                }, {
+                    xtype: 'textfield',
+                    inputType: 'password',
+                    itemId: 'newPassword',
+                    name: 'password',
+                    fieldLabel: AOCLit.password,
+                    allowBlank: me.mode=='add'? false : true,
+                    blankText: 'Password is required(should be atleast of length 8)',
+                    validateOnChange: false,
+                    vtype: 'newpassword',
+                    hidden: me.mode=='edit'? true : false,
+                    listeners: {
+                        scope: me
+                    }
+                }, {
+                    xtype: 'textfield',
+                    inputType: 'password',
+                    itemId: 'confirmPassword',
+                    name: 'confirmPassword',
+                    allowBlank: me.mode=='add'? false : true,
+                    blankText: 'Confirm Password is required',
+                    fieldLabel: AOCLit.confirmPassword,
+                    hidden:me.mode=='edit'? true : false,
+                    vtype: 'password',
+                    initialPassField: 'newPassword'
+                }, {
+                    xtype: 'hidden',
+                    name: 'id'
+                }]
+            }]
+        }];
+    }
+});
---
+++ WebContent/avery/app/view/users/myprofile/AddUserWindowController.js
@@ -0,0 +1,180 @@
+Ext.define('AOC.view.users.myprofile.AddUserWindowController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.adduserwindow',
+    runTime: AOC.config.Runtime,
+    helper: AOC.util.Helper,
+    fileArray:[],
+    SaveDetails: function () {
+        var me = this,
+            refs = me.getReferences(),
+            view = me.getView(),
+        	form = refs.addEditUserWinForm,
+        	profileImage = refs.profileImage,
+        	userInfo = AOCRuntime.getUser(),
+        	role = userInfo.role,
+        	firstName = userInfo.firstName,
+        	mode = view.mode,
+            userGrid = AOCRuntime.getActiveGrid(),
+            url = '',
+            valueObj = '',
+            method = '',
+            msg = '';
+        if (mode == 'edit') {
+            url = applicationContext + '/rest/users/' + view.ID; //Provide roleId for particular record
+            method = 'PUT';
+            valueObj = form.getValues(false, true, false, true);
+            length = Object.keys(valueObj).length;
+            msg = AOCLit.updateUserMsg;
+        } else {
+            url = applicationContext + '/rest/users';
+            valueObj = form.getValues(false, true, false, true);
+            method = 'POST';
+            length = 1;
+            msg = AOCLit.addUserMsg;
+        }
+        var parameters = Ext.JSON.encode(valueObj);
+        if (length == 0) {
+            Helper.showToast('failure', AOCLit.tickTabMsg);
+        } else {
+            if (form.isValid()) {
+                view.mask('Saving....');
+                Ext.Ajax.request({
+                    method: method,
+                    jsonData: parameters,
+                    url: url,
+                    success: function (response, opts) {
+                        var me = this,
+                        	jsonString = Ext.JSON.decode(response.responseText),
+                            valueExist = jsonString.valueExist;
+                        if (valueExist) {
+                        	view.unmask();
+                            Helper.showToast('failure',AOCLit.userExistMsg);
+                            return false;
+                        }
+                        view.unmask();
+                        view.close();
+                        Helper.showToast('success', msg);
+                        if(userGrid){
+                        	userGrid.store.load();
+                        }
+                        else{
+	                        AOCRuntime.setUser(jsonString.user);
+	                        Helper.updateHeaderUserName();
+	                        Helper.updateProfileInfo();
+	                        Helper.setCookie("userinfo", JSON.stringify(jsonString.user), 30);
+                        }
+                    },
+                    failure: function (response, opts) {
+                        msg = response.responseText;
+                        Helper.showToast('failure', msg);
+                        view.unmask();
+                        view.close();
+                    }
+                });
+            } else {
+                Helper.showToast('failure', AOCLit.userInfoInvalid);
+            }
+        }
+    },
+    CancelDetails: function () {
+        Ext.getBody().unmask();
+        this.getView().close();
+    },
+    onAddUserAfterRender: function (win) {
+        win.mask(AOCLit.pleaseWaitTitle);
+        var roleName = win.lookupReference('roleName'),
+            siteCombo = win.lookupReference('siteName'),
+            siteStore = siteCombo.store,
+            userInfo = AOCRuntime.getUser(),
+            roleId = userInfo.role,
+            siteId = userInfo.siteId,
+            store = roleName.store;
+        store.load({
+            params: {
+                role: roleId
+            }
+        });
+        siteStore.load({
+            params: {
+                siteId: siteId
+            }
+        })
+        win.unmask();
+    },
+    onClickUpdatePhoto: function (obj, value, eOpts) {
+    	var me = this,
+        	value = value.substring(value.lastIndexOf("\\"));
+            view = me.getView(),
+            refs = me.getReferences(),
+            addEditUserWin = refs.addEditUserWin,
+            profileImage = refs.profileImage,
+            userInfo = AOCRuntime.getUser(),
+            userId = userInfo.id,
+            userEmail = userInfo.email,
+            roleId = userInfo.role,
+            filePath = '';
+
+        var file = obj.getEl().down('input[type=file]').dom.files[0];
+        type = file.type;
+        if (!file) {
+            return;
+        }
+        if (type.indexOf('image') != -1) {
+            me.setImagePreview(file, profileImage);
+            me.fileArray.push(file);
+            me.uploadFiles(userId,roleId,userEmail, filePath,file);
+        } else {
+            Helper.showToast('failure', AOCLit.invalidImgFormat);
+        }
+    },
+    setImagePreview: function (file, imageContainer) {
+        var me = this,
+            name = file.name,
+        	fileReader = new window.FileReader();
+
+        fileReader.onload = function (e, b) {
+            var fileContent = e.target.result;
+            imageContainer.setSrc(fileContent);
+        }
+        fileReader.readAsDataURL(file);
+    },
+    uploadFiles: function (userId,roleId,userEmail, filePath,file) {
+        var me = this;
+        	refs = me.getReferences(),
+            addEditUserWin = refs.addEditUserWinForm,
+            form = addEditUserWin.getForm(),
+            xhttp = new XMLHttpRequest(),
+            fileArray = this.fileArray,
+            formData = new FormData(),
+            url = applicationContext + '/rest/users/pictureupload',
+            values = form.getValues(),
+            len = fileArray.length;
+
+            formData.append('file', fileArray[len - 1]);
+            formData.append('fileName', fileArray[len - 1].name);
+            formData.append('filePath', filePath);
+            formData.append('userId',userId);
+            formData.append('roleId',roleId);
+            formData.append('userEmail',userEmail);
+
+            xhttp.onreadystatechange = function () {
+                (4 === xhttp.readyState) && uploadDone(xhttp);
+            };
+            xhttp.open("POST", url, true);
+            xhttp.send(formData);
+
+            function uploadDone(xhttp) {
+                switch (xhttp.status) {
+                case 200:
+                    fileArray.pop();
+                    break;
+                }
+            }
+    },
+    beforeRenderUploadbtn: function(obj){
+    	var me = this,
+    		recordId = me.getView().down('form').getValues().id,
+    		userId = AOCRuntime.getUser().id;
+    	if(recordId!=userId) obj.setHidden(true);
+    }
+});
