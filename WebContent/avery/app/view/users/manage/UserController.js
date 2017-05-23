4a39d0c2df73c50daf180c86e7d6be41a0e5b206
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-05-23 11:47:36 +0530

    [Jira# AOC-379]Changes related to Enhanced GUI

--- WebContent/avery/app/view/users/manage/UserController.js
+++ WebContent/avery/app/view/users/manage/UserController.js
@@ -4,120 +4,18 @@ Ext.define('AOC.view.users.manage.UserController', {
 	runTime: AOC.config.Runtime,
 	helper: AOC.util.Helper,
 	createuser: function () {
-	   var win = Ext.ComponentQuery.query('#userWindowItemId')[0];
-	   if (!win) {
-	       win = Ext.create('AOC.view.base.NewBaseWindow', {
-	           height: 550,
-	           width: 1000,
-	           title: '<b>Add User</b>',
-	           itemId: 'userWindowItemId',
-	           layout: 'fit',
-	           draggable: false,
-	           modal: true,
-	           listeners:{
-                	'afterrender':function(){
-                		var me = this;
-                			me.mask(AOCLit.pleaseWaitTitle),
-                			form = this.down('form'),
-                			roleName = form.lookupReference('roleName'),
-                			siteCombo = form.lookupReference('siteName'),
-                			siteStore = siteCombo.store,
-                			userInfo = AOCRuntime.getUser(),
-                			roleId = userInfo.role,
-                			siteId = userInfo.siteId,
-                			store = roleName.store;
-                			store.load({params:{role:roleId}});
-                			siteStore.load({params:{siteId:siteId}})
-                			me.unmask();
-                	}
-                },
-	           items: [{
-		               xtype: 'useredit',
-		               showPasswordField: true,
-		               hidefield: false
-	           }]
+		var me = this,
+		    win = Ext.create('AOC.view.users.myprofile.AddUserWindow', {
+	    	   gridView :me.getView()
 	       });
-	   }
-	
 	   win.show();
 	},
-    SaveDetails: function () {
-        Ext.getBody().mask('Saving....');
-        var Msg = '';
-        var win = Ext.ComponentQuery.query('#userWindowItemId')[0];
-        var grid = Ext.ComponentQuery.query('#usersgriditemId')[0];
-        var valueObj = '',
-            form = this.getView().down('form');
-        var methodMode = '';
-        url = '';
-        valueObj = form.getValues(false, true, false, true);
-        var ID = valueObj.id;
-        var length = 0;
-        if (ID != null) {
-            url = applicationContext + '/rest/users/' + ID;
-            // form.updateRecord();
-            valueObj = form.getValues(false, true, false, true);
-            methodMode = 'PUT';
-            length = Object.keys(valueObj).length;
-            // Msg='User Updated Successfully';
-            var Msg = AOCLit.updateUserMsg;
-            var parameters = Ext.JSON.encode(valueObj);
-        } else {
-            var refs = this.getReferences();
-            url = applicationContext + '/rest/users';
-            valueObj = form.getValues(false, true, false, true);
-            methodMode = 'POST';
-            valueObj.status = 100;
-            valueObj.role = valueObj.role;
-            valueObj.gender = valueObj.gender;
-            // Msg='User Added Successfully';
-            var Msg = AOCLit.addUserMsg;
-            length = Object.keys(valueObj).length - 1;
-            var parameters = Ext.JSON.encode(valueObj);
-        }
-        if (length > 0) {
-            if (form.isValid()) {
-                Ext.Ajax
-                    .request({
-                        method: methodMode,
-                        jsonData: parameters,
-                        url: url,
-                        success: function (response, opts) {
-                            var jsonString = Ext.JSON.decode(response.responseText);
-                            var valueExist = jsonString.valueExist;
-                            if (valueExist) {
-                                Ext.getBody().unmask();
-                                win.down('#messageFieldItemId').show();
-                                win.down('#messageFieldItemId').setValue(AOCLit.userNameExistsMsg);
-                                return false;
-                            }
-                            Ext.getBody().unmask();
-                            win.destroy();
-                            Helper.showToast('Success', Msg);
-                            grid.store.load();
-                        },
-                        failure: function (response, opts) {
-                            Msg = response.responseText;
-                            Msg = Msg.replace("Exception:", " ");
-                            Ext.Msg.alert('Alert Message', Msg);
-                            Ext.getBody().unmask();
-                            win.destroy();
-                        }
-                    });
-            } else {
-                win.down('#messageFieldItemId').setValue(AOCLit.fillMandatoryFieldMsg).setVisible(true);
-            }
-        } else {
-            win.down('#messageFieldItemId').setValue(AOCLit.noFieldModifiedMsg).setVisible(true);
-        }
-    },
     CancelDetails: function () {
         Ext.getBody().unmask();
         var win = Ext.ComponentQuery.query('#userWindowItemId')[0];
-        win.destroy();
+        win.close();
     },
-    onClickMenu: function (obj, rowIndex, colIndex, item, e,
-        record) {
+    onClickMenu: function (obj, rowIndex, colIndex, item, e,record) {
         var me = this;
         var callout = Ext.widget(
                 'callout', {
@@ -130,51 +28,19 @@ Ext.define('AOC.view.users.manage.UserController', {
                     dismissDelay: 0,
                     listeners: {
                         afterrender: me.onAfterRenderEditCallout,
-                        edit: function (cmp) {
-                            currentRecord = e.record;
-                            var id = currentRecord.get('id');
-                            me.runTime.setWindowInEditMode(true);
-                            var mode = me.runTime.getWindowInEditMode();
-                            var win = Ext.ComponentQuery.query('#userWindowItemId')[0];
-                            var title = mode ? 'Edit User' : 'Add User';
-                            if (!win) {
-                                var data = {
-                                    "id": ""
-                                };
-                                win = Ext.create('AOC.view.base.NewBaseWindow', {
-                                        height: 450,
-                                        width: 950,
-                                        modal: true,
-                                        itemId: 'userWindowItemId',
-                                        title: '<b>' + title + '</b>',
-                                        editMode: true,
-                                        ID: id,
-                                        listeners:{
-                                        	'afterrender':function(){
-                                        		var me = this;
-                                        		me.mask(AOCLit.pleaseWaitTitle);
-                                        		var form = this.down('form'),
-                                        		roleName = form.lookupReference('roleName'),
-                                        		siteCombo = form.lookupReference('siteName'),
-                                        		siteStore = siteCombo.store,
-                                        		userInfo = AOCRuntime.getUser(),
-                                        		roleId = userInfo.role,
-                                        		siteId = userInfo.siteId,
-                                        		store = roleName.store;
-                                        		store.load({params:{role:roleId}});
-                                        		siteStore.load({params:{siteId:siteId}})
-                                        		this.unmask();
-                                        	}
-                                        },
-                                        items: [{
-                                            xtype: 'useredit',
-                                            hidefield: false
-                                        }]
-                                    });
-                                win.down('#useredititemid').getForm().setValues(currentRecord.data);
+                        edit: function(cmp) {
+                            var me = this,
+                            	currentRecord = e.record,
+                            	data = currentRecord.data;
+                                var id = data.id;
+                                win = Ext.create('AOC.view.users.myprofile.AddUserWindow', {
+                                    mode: 'edit',
+                                    ID: id,
+                                    title:'Edit User'
+                                });
+                                win.down('form').loadRecord(currentRecord);
                                 win.show();
-                            }
-                            callout.destroy();
+                                callout.destroy();
                         },
                         deleteuser: function (cmp) {
                             currentRecord = e.record;
@@ -187,9 +53,8 @@ Ext.define('AOC.view.users.manage.UserController', {
                                                 method: 'DELETE',
                                                 url: applicationContext + '/rest/users/' + ID,
                                                 success: function (response, opts) {
-
-                                                    Helper.showToast('Success', Msg);
-                                                    me.runTime.getActiveGrid().store.load();
+	                                                    Helper.showToast('Success', Msg);
+	                                                    me.runTime.getActiveGrid().store.load();
                                                 },
                                                 failure: function (response, opts) {}
                                             });
@@ -285,4 +150,4 @@ Ext.define('AOC.view.users.manage.UserController', {
         cmp.setValue('');
         cmp.orderedTriggers[0].hide();
     }
-        });
+});
