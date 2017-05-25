d83ae28f6bdbd7f7943100d9c7540a1e3cf49158
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-05-25 14:57:19 +0530

    adding lastmodifiedby and lastmodifiedbdate column in user grid

--- WebContent/avery/app/model/UserModel.js
+++ WebContent/avery/app/model/UserModel.js
@@ -8,7 +8,10 @@ Ext.define('AOC.model.UserModel',{
 		{name: 'email', mapping:'email',type: 'string'},
 		{name: 'role', mapping:'role',type: 'string'},
 		{name: 'password', mapping:'password',type: 'string'},
-		{name: 'jobTitle', mapping:'jobTitle',type: 'string'}
+		{name: 'jobTitle', mapping:'jobTitle',type: 'string'},
+		{name: 'lastModifiedBy', mapping:'lastModifiedBy',type: 'string'},
+		{name:'lastModifiedDate',mapping:'lastModifiedDate',type:'string'}
+		
 	    ]
 });
 
