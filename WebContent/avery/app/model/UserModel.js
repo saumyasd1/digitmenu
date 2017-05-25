98549b36ca655f5e5574577e84cd0a12d283ef7f
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-05-25 14:25:59 +0530

    Adding middle name in user grid

--- WebContent/avery/app/model/UserModel.js
+++ WebContent/avery/app/model/UserModel.js
@@ -3,6 +3,7 @@ Ext.define('AOC.model.UserModel',{
 	idProperty:null,
     fields: [
 	    {name: 'firstName', mapping:'firstName',type: 'string'},
+	    {name: 'middleName', mapping:'middleName',type: 'string'},
 		{name: 'lastName', mapping:'lastName',type: 'string'},
 		{name: 'email', mapping:'email',type: 'string'},
 		{name: 'role', mapping:'role',type: 'string'},
