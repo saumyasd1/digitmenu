c52049fd45ca526dcf44c166eca55bfaeb541fe9
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-05-25 16:58:30 +0530

    Adding code for disabling Delete option for Super admin

--- WebContent/avery/app/view/users/manage/UserController.js
+++ WebContent/avery/app/view/users/manage/UserController.js
@@ -1,86 +1,90 @@
 Ext.define('AOC.view.users.manage.UserController', {
-	extend: 'Ext.app.ViewController',
-	alias: 'controller.userMain',
-	runTime: AOC.config.Runtime,
-	helper: AOC.util.Helper,
-	createuser: function () {
-		var me = this,
-		    win = Ext.create('AOC.view.users.myprofile.AddUserWindow', {
-	    	   gridView :me.getView()
-	       });
-	   win.show();
-	},
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.userMain',
+    runTime: AOC.config.Runtime,
+    helper: AOC.util.Helper,
+    createuser: function () {
+        var me = this,
+            win = Ext.create('AOC.view.users.myprofile.AddUserWindow', {
+                gridView: me.getView()
+            });
+        win.show();
+    },
+    init: function () {
+        var me = this;
+        me.menuTpl = me.buildMenuTpl();
+    },
     CancelDetails: function () {
         Ext.getBody().unmask();
         var win = Ext.ComponentQuery.query('#userWindowItemId')[0];
         win.close();
     },
-    onClickMenu: function (obj, rowIndex, colIndex, item, e,record) {
+    onClickMenu: function (obj, rowIndex, colIndex, item, e, record) {
         var me = this;
         var callout = Ext.widget(
-                'callout', {
-                    cls: 'white more-menu-item-callout extra',
-                    html: me.buildMenuTpl.apply("{}"),
-                    target: e.target,
-                    calloutArrowLocation: 'top-left',
-                    relativePosition: 't-b',
-                    relativeOffsets: [52, 23],
-                    dismissDelay: 0,
-                    listeners: {
-                        afterrender: me.onAfterRenderEditCallout,
-                        edit: function(cmp) {
-                            var me = this,
-                            	currentRecord = e.record,
-                            	data = currentRecord.data;
-                                var id = data.id;
-                                win = Ext.create('AOC.view.users.myprofile.AddUserWindow', {
-                                    mode: 'edit',
-                                    ID: id,
-                                    title:'Edit User'
-                                });
-                                var refs = win.getReferences(),
-                                	profileImage = refs.profileImage,
-                                	userId = AOCRuntime.getUser().id;
-                                if(id == userId){
-                                	profileImage.setSrc(Helper.getFilePath());
-                                	if(id == 1){
-                                	 win.down('#site').setHidden(true);
-                                	}
-                                	win.down('#site').setDisabled(true);
-                                   	win.down('#role').setHidden(true);
-                                   	win.down('#roledisplayfield').setHidden(false);
-                                }
-                                win.down('form').loadRecord(currentRecord);
-                                win.show();
-                                callout.destroy();
-                        },
-                        deleteuser: function (cmp) {
-                            currentRecord = e.record;
-                            var ID = record.get('id');
-                            var Msg = AOCLit.deleteUserMsg;
-                            Ext.Msg.confirm('Alert', AOCLit.delUserMsg,
-                                    function (btn) {
-                                        if (btn == 'yes') {
-                                            Ext.Ajax.request({
-                                                method: 'DELETE',
-                                                url: applicationContext + '/rest/users/' + ID,
-                                                success: function (response, opts) {
-	                                                    Helper.showToast('Success', Msg);
-	                                                    me.runTime.getActiveGrid().store.load();
-                                                },
-                                                failure: function (response, opts) {
-                                                    msg = response.responseText;
-                                                    Helper.showToast('failure', msg);
-                                                    view.unmask();
-                                                    view.close();
-                                                }
-                                            });
+            'callout', {
+                cls: 'white more-menu-item-callout extra',
+                html: me.menuTpl.apply(record.data),
+                target: e.target,
+                calloutArrowLocation: 'top-left',
+                relativePosition: 't-b',
+                relativeOffsets: [52, 23],
+                dismissDelay: 0,
+                listeners: {
+                    afterrender: me.onAfterRenderEditCallout,
+                    edit: function (cmp) {
+                        var me = this,
+                            currentRecord = e.record,
+                            data = currentRecord.data;
+                        var id = data.id;
+                        win = Ext.create('AOC.view.users.myprofile.AddUserWindow', {
+                            mode: 'edit',
+                            ID: id,
+                            title: 'Edit User'
+                        });
+                        var refs = win.getReferences(),
+                            profileImage = refs.profileImage,
+                            userId = AOCRuntime.getUser().id;
+                        if (id == userId) {
+                            profileImage.setSrc(Helper.getFilePath());
+                            if (id == 1) {
+                                win.down('#site').setHidden(true);
+                            }
+                            win.down('#site').setDisabled(true);
+                            win.down('#role').setHidden(true);
+                            win.down('#roledisplayfield').setHidden(false);
+                        }
+                        win.down('form').loadRecord(currentRecord);
+                        win.show();
+                        callout.destroy();
+                    },
+                    deleteuser: function (cmp) {
+                        currentRecord = e.record;
+                        var ID = record.get('id');
+                        var Msg = AOCLit.deleteUserMsg;
+                        Ext.Msg.confirm('Alert', AOCLit.delUserMsg,
+                            function (btn) {
+                                if (btn == 'yes') {
+                                    Ext.Ajax.request({
+                                        method: 'DELETE',
+                                        url: applicationContext + '/rest/users/' + ID,
+                                        success: function (response, opts) {
+                                            Helper.showToast('Success', Msg);
+                                            me.runTime.getActiveGrid().store.load();
+                                        },
+                                        failure: function (response, opts) {
+                                            msg = response.responseText;
+                                            Helper.showToast('failure', msg);
+                                            view.unmask();
+                                            view.close();
                                         }
                                     });
-                            callout.destroy();
-                        }
+                                }
+                            });
+                        callout.destroy();
                     }
-                });
+                }
+            });
         callout.show();
         var heightAbove = e.getY() - Ext.getBody().getScroll().top,
             heightBelow = Ext.Element
@@ -113,13 +117,23 @@ Ext.define('AOC.view.users.manage.UserController', {
     },
     buildMenuTpl: function () {
         var me = this;
-        return Ext
-            .create(
-                'Ext.XTemplate',
-                '<div style="width: 140px !important;border-bottom: none !important;cursor:pointer;" class="user-profile-menu-callout user-profile-menu-item"  event="edit"">Edit</div>',
-                '</tpl>',
-                '<div style="width: 140px !important;cursor:pointer;" class="user-profile-menu-callout user-profile-menu-item"  event="deleteuser"">Delete</div>',
-                '</tpl>');
+        return Ext.create('Ext.XTemplate',
+            '<div style="width: 140px !important;border-bottom: none !important;cursor:pointer;" class="user-profile-menu-callout user-profile-menu-item"  event="edit">Edit</div>',
+            '<div style="width: 140px !important;border-bottom: none{[this.getDeleteUserStyle(values)]};" class="user-profile-menu-callout {[this.isDeleteEnable(values)]}"  event="deleteuser">Delete</div>', {
+                isDeleteEnable: function (v) {
+                    if (v.id == 1) {							//(Krishna)Check for Super Admin
+                        return 'order-profile-menu-item';
+                    }
+                    return 'user-profile-menu-item';
+                },
+                getDeleteUserStyle: function (v) {
+                    if (v.id == 1) {
+                        return Helper.getDisableMenuItemStyle();
+                    }
+                    return Helper.getEnableMenuItemStyle();
+                }
+            }
+        );
     },
     hideMandatoryMessage: function () {
         var obj = this.getView();
