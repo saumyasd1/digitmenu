394f0f97edff3d05287a75a603959723cc00db7d
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-06-01 11:16:08 +0530

    Changes related to timeZone issue(Anand Kumar)

--- src/com/avery/storage/entities/Partner.java
+++ src/com/avery/storage/entities/Partner.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.TimeZone;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
