62ac7aeadef7b433773a68db1fe12e1e04cfe774
Pratyoush <Pratyoush@ADLP116_Pratyoush>
2017-06-22 18:10:41 +0530

    Adding code for adding column emailId and password in site entity class and setter getter asper field is added in dataBase

--- src/com/avery/storage/entities/Site.java
+++ src/com/avery/storage/entities/Site.java
@@ -40,6 +40,13 @@ public class Site extends MainAbstractEntity {
 
 	@Column(name = "name", length = 50, unique = true)
 	private String name;
+	
+	@Column(name = "emailId", length = 100)
+	private String emailId;
+	
+	@Column(name = "password", length = 100)
+	private String password;
+	
 	@Column(name = "comment", length = 250)
 	private String comment;
 
@@ -57,6 +64,22 @@ public class Site extends MainAbstractEntity {
 
 	public void setComment(String comment) {
 		this.comment = comment;
+	}
+	
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	@OneToMany(mappedBy = "site", fetch = FetchType.LAZY)
