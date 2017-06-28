5fcbdb315356efbd71c21dd23eeebff4abfbe55b
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-06-28 17:19:06 +0530

    code for material and daily report enhancement

--- WebContent/avery/classic/src/view/home/ReportFormController.js
+++ WebContent/avery/classic/src/view/home/ReportFormController.js
@@ -16,14 +16,53 @@ Ext.define('AOC.view.home.ReportFormController', {
      		obj.toDate=this.lookupReference('toDate').getSubmitValue();
      	 } 
      	var rboCombo =this.lookupReference('rboName'),
-		rboStore = rboCombo.store,
-		rboName = [];
+     		rboStore = rboCombo.store,
+     		rboName = [],
+     		partnerCombo =  this.lookupReference('partner'),
+     		partnerStore = partnerCombo.store,
+     		partnerName = [],
+     		siteCombo = this.lookupReference('siteName'),
+     		siteStore = siteCombo.store,
+     		siteName = [],
+     		csrCombo = this.lookupReference('csrCombo'),
+     		csrStore = csrCombo.store,
+     		csrName = [];
 	
 	rboStore.each(function(rec){
 		if(rec.get('id') != 'all'){
 			rboName.push(rec.get('rboName'));
 		}
 	});
+	
+	if(partnerCombo.getValue() == 'all'){
+		partnerStore.each(function(rec){
+			if(rec.get('id') != 'all' ){
+				partnerName.push(rec.get('id'));
+			}
+		});
+	}else{
+		partnerName.push(partnerCombo.getValue());
+	}
+	
+	if(siteCombo.getValue() == 'all'){
+		siteStore.each(function(rec){
+			if(rec.get('id') != 'all' ){
+				siteName.push(rec.get('id'));
+			}
+		});
+	}else{
+		siteName.push(siteCombo.getValue());
+	}
+	
+	if(csrCombo.getValue() == 'all'){
+		csrStore.each(function(rec){
+			if(rec.get('id') != 'all' ){
+				csrName.push(rec.get('id'));
+			}
+		});
+	}else{
+		csrName.push(csrCombo.getValue());
+	}
 	
 	var statusCombo =this.lookupReference('status'),
 	statusStore = statusCombo.store,
@@ -38,11 +77,13 @@ Ext.define('AOC.view.home.ReportFormController', {
 		statusCode.push(statusCombo.getValue());
 	}
 	
-	obj.PartnerName=this.lookupReference('partner').getDisplayValue();
-	obj.RBOName=rboName.join(',');
-	obj.Status=statusCode.join(',');
+	obj.partnerId = partnerName.join(',');
+	obj.RBOName = rboName.join(',');
+	obj.siteId = siteName.join(',');
+	obj.csrId = csrName.join(',');
+	obj.Status = statusCode.join(',');
 	var values = reportForm.getValues();
-	query=Ext.JSON.encode(obj);
+		query = Ext.JSON.encode(obj);
 	
      	var form = Ext.create('Ext.form.Panel', { 
             standardSubmit: true,   
@@ -69,57 +110,113 @@ Ext.define('AOC.view.home.ReportFormController', {
     },
     
     onPartnerChange:function(obj,newValue){
-    	var me = this;
-    	var statusCombo =this.lookupReference('status');
+    	var me = this,
+    		view = me.getView(),
+    		statusCombo =this.lookupReference('status');
     	if(newValue != null){
 	    	var productLineCombo = me.lookupReference('productLineCombo'),
-	    		rboCombo = me.lookupReference('rboName');
-	    	
-	        var response = Ext.Ajax.request({
-	            async: false,
-	            url:applicationContext+'/rest/productLines/partner/'+newValue+'?partnerId='+newValue
-	        });
-	        
-	      	var jsonValue = Ext.decode(response.responseText).productlines,
-	      		serviceStoreData = [];
-	      	
-	    	if(jsonValue.length>0){
-	    		jsonValue.forEach(function(item){
-	    			var service = item;
-	    			serviceStoreData.push(service);
+	    		rboCombo = me.lookupReference('rboName'),
+	    		partnerCombo = me.lookupReference('partner'),
+	    		partnerStore = partnerCombo.store,
+		    	partnerId =[];
+
+	    	if(partnerCombo.getValue() == 'all'){
+	    		partnerStore.each(function(rec){
+	    			if(rec.get('id') != 'all'){
+	    				partnerId.push(rec.get('id'));
+	    			}
 	    		});
-	    		
-		    	var store =  Ext.create('Ext.data.Store',{
-		    		fields:['id'],
-		            data : serviceStoreData
-		    	});
-		    	
-		    	var uniqueValueArray1 = store.collect('rbo'),
-		    		serviceStoreData1= [];
-		    	
-		    	serviceStoreData1.push({id:'all', rboName:'Select All'});
-		    	if(uniqueValueArray1.length>0){
-		    		var rboStore = rboCombo.store;
-		    		
-		    		 uniqueValueArray1.forEach(function(item){
-			        	 serviceStoreData1.push(item);
-		    		 });
-		    		 rboStore.loadData(serviceStoreData1);
-		    	}
+	    	}else{
+	    		partnerId.push(partnerCombo.getValue());
 	    	}
+	    	
+	    	var response = Ext.Ajax.request({
+	    		url: applicationContext + '/rest/productLines/rboList',
+	    		method:'GET',
+	    		params:{partnerId:partnerId.join()},
+	    		success : function(response, opts) {
+	    				var jsonValue = Ext.decode(response.responseText).productlines;
+		    				serviceStoreData = [];
+		    	      	
+		    	    	if(jsonValue.length>0){
+		    	    		jsonValue.forEach(function(item){
+		    	    			var service = item;
+		    	    			serviceStoreData.push(service);
+		    	    		});
+		    	    		
+		    		    	var store =  Ext.create('Ext.data.Store',{
+		    		    		fields:['id'],
+		    		            data : serviceStoreData
+		    		    	});
+		    		    	var uniqueValueArray = [],
+		    		    		rboStoreData= [];
+		    		    	store.each(function(rec,index){
+		    		    		uniqueValueArray.push(rec.get('rbo'));
+		    		    	})
+		    		    	
+		    		    	rboStoreData.push({id:'all', rboName:'Select All'});
+		    		    	if(uniqueValueArray.length>0){
+		    		    		var rboStore = rboCombo.store;
+		    		    		
+		    		    		 uniqueValueArray.forEach(function(item){
+		    		    			 rboStoreData.push(item);
+		    		    		 });
+		    		    		 rboStore.loadData(rboStoreData);
+		    		    	}
+		    	    	}
+			        },
+		        failure: function(response, opts) {
+	                    msg = response.responseText;
+	                    Helper.showToast('failure', msg);
+	                    view.unmask();
+	                    view.close();
+	                }
+	    	});
+	    	
 	    	rboCombo.enable();
 	    	statusCombo.enable();
     	}
-    	if(!this.getView().isResubmit || !obj.isChangedForFirstTime){
-    		rboCombo.reset();
-    		statusCombo.reset();
-    	}
+		if(!this.getView().isResubmit || !obj.isChangedForFirstTime){
+			rboCombo.reset();
+			statusCombo.reset();
+		}
     },
     onPartnerAfterRender: function(obj){
 	    var userInfo = AOCRuntime.getUser(),
-	    	siteId = userInfo.siteId;
+	    	siteId = userInfo.siteId,
+	    	store = obj.store,
+	    	storeObj = {id:'all',partnerName:'Select All'};
+	    
 	    obj.getStore().proxy.extraParams = {
 		    siteId: siteId
 		};
+		store.on('load',function(){
+			store.insert(0,new Ext.data.Record(storeObj));
+		},store);	
+    },
+    onReportFormAfterRender: function(form){
+    	  var siteCombo = form.lookupReference('siteName'),
+	          siteStore = siteCombo.store,
+	          userInfo = AOCRuntime.getUser(),
+	          roleId = userInfo.role,
+	          siteId = userInfo.siteId;
+      
+	      siteStore.load({
+	          params: {
+	              siteId: siteId
+	          }
+	      });
+    },
+    onSiteSelect: function(siteCombo){
+    	var me = this,
+    		siteComboValue = siteCombo.getValue(),
+    		siteComboStore = siteCombo.store,
+    		refs = me.getReferences(),
+    		csrCombo = refs.csrCombo;
+    	csrCombo.enable();
+    	csrCombo.store.proxy.extraParams = {
+		    siteId: siteComboValue == 'all' ? 1 : siteComboValue
+		};
+    	csrCombo.store.load();
     }
 })
