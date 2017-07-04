1c11da957245f2ab09f8d3ae3e5920243f8039a4
Krishna Kumar Varshney <Krishna@ADLP109_Krishna>
2017-07-04 10:59:03 +0530

    adding site name for super admin only

--- WebContent/avery/app/model/UserModel.js
+++ WebContent/avery/app/model/UserModel.js
@@ -6,13 +6,13 @@ Ext.define('AOC.model.UserModel',{
 	    {name: 'middleName', mapping:'middleName',type: 'string'},
 		{name: 'lastName', mapping:'lastName',type: 'string'},
 		{name: 'email', mapping:'email',type: 'string'},
+		{name: 'siteName', mapping:'siteName',type: 'string'},
 		{name: 'role', mapping:'role',type: 'string'},
 		{name: 'password', mapping:'password',type: 'string'},
 		{name: 'confirmPassword', mapping:'password',type: 'string'},
 		{name: 'jobTitle', mapping:'jobTitle',type: 'string'},
 		{name: 'lastModifiedBy', mapping:'lastModifiedBy',type: 'string'},
 		{name:'lastModifiedDate',mapping:'lastModifiedDate',type:'string'}
-		
 	    ]
 });
 
